0fa593fa2419a55197790fe2a6c6f4f85c6314e1
manuelpuyol <manuelpuyol@github.com>
2020-08-03 18:59:31 -0500

    config to test with dummy app

--- Gemfile
+++ Gemfile
@@ -2,6 +2,9 @@ source "https://rubygems.org"
 
 # Specify your gem's dependencies in primer_view_components.gemspec
 gemspec
+rails_version = "#{ENV['RAILS_VERSION'] || '6.0.3.2'}"
 
+gem "capybara", "~> 3"
+gem "rails", rails_version == "master" ? { github: "rails/rails" } : rails_version
 gem "rake", "~> 12.0"
 gem "minitest", "~> 5.0"
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,11 @@
-# Configure Rails Envinronment
+require "pry"
+require "bundler/setup"
+
 ENV["RAILS_ENV"] = "test"
 
+require File.expand_path("../config/environment.rb", __FILE__)
+require "rails/test_help"
 require "view_component/test_helpers"
-require "minitest/autorun"
 
+require "minitest/autorun"
 require "primer/view_components"
