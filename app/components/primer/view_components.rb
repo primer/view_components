fd33cb14eeec09178d086036944b705446f984ba
manuelpuyol <manuelpuyol@github.com>
2020-08-03 18:59:41 -0500

    uncomment components

--- app/components/primer/view_components.rb
+++ app/components/primer/view_components.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
-# Base configurations
+require "active_support/core_ext"
 
-# require_relative "./component"
-# require_relative "./base_component"
+# ViewComponent
+
+require "view_component/engine"
+
+# Helpers
+
+require "primer/class_name_helper"
+require "primer/classify"
+require "primer/fetch_or_fallback_helper"
+
+# Base configurations
+require_relative "./component"
+require_relative "./base_component"
--- lib/primer/view_components.rb
+++ lib/primer/view_components.rb
@@ -1,11 +1,4 @@
-require "active_support/core_ext"
-require "view_component/engine"
-
-require "primer/class_name_helper"
-require "primer/classify"
-require "primer/fetch_or_fallback_helper"
 require "primer/view_components/version"
-
 require "primer/view_components/engine"
 
 module Primer
