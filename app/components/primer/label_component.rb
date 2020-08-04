4ee2618614dada04c1a942ad92f8073ff0d885fe
manuelpuyol <manuelpuyol@github.com>
2020-08-04 11:53:49 -0500

    add label component

---
+++ app/components/primer/label_component.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module Primer
+  class LabelComponent < Primer::Component
+    SCHEME_MAPPINGS = {
+      # gray
+      gray: "Label--gray",
+      dark_gray: "Label--gray-darker",
+
+      # colored
+      yellow: "Label--yellow",
+      orange: "Label--orange",
+      red: "Label--red",
+      green: "Label--green",
+      blue: "Label--blue",
+      purple: "Label--purple",
+      pink: "Label--pink",
+
+      # Deprecated
+      outline: "Label--outline",
+      green_outline: "Label--outline-green",
+    }.freeze
+    SCHEME_OPTIONS = SCHEME_MAPPINGS.keys << nil
+
+    VARIANT_MAPPINGS = {
+      large: "Label--large",
+      inline: "Label--inline",
+    }.freeze
+    VARIANT_OPTIONS = VARIANT_MAPPINGS.keys << nil
+
+    def initialize(title:, scheme: nil, variant: nil, **kwargs)
+      @kwargs = kwargs
+      @kwargs[:bg] = :blue if scheme.nil?
+      @kwargs[:tag] = :span
+      @kwargs[:title] = title
+      @kwargs[:classes] = class_names(
+        "Label",
+        kwargs[:classes],
+        SCHEME_MAPPINGS[fetch_or_fallback(SCHEME_OPTIONS, scheme)],
+        VARIANT_MAPPINGS[fetch_or_fallback(VARIANT_OPTIONS, variant)]
+      )
+    end
+
+    def call
+      render(Primer::BaseComponent.new(**@kwargs)) { content }
+    end
+  end
+end
---
+++ test/components/label_component_test.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class PrimerLabelComponentTest < Minitest::Test
+  include Primer::ComponentTestHelpers
+
+  def test_renders_content
+    render_inline(Primer::LabelComponent.new(title: "private-badge")) { "private" }
+
+    assert_text("private")
+  end
+
+  def test_defaults_to_bg_blue
+    render_inline(Primer::LabelComponent.new(title: "foo")) { "private" }
+
+    assert_selector(".bg-blue")
+  end
+
+  def test_falls_back_when_scheme_isn_t_valid
+    render_inline(Primer::LabelComponent.new(title: "title", scheme: :pink)) { "content" }
+
+    assert_text("content")
+  end
+
+  def test_renders_with_the_css_class_scheme_mapping_to_the_provided_scheme
+    render_inline(Primer::LabelComponent.new(title: "title", scheme: :gray)) { "private" }
+
+    assert_selector(".Label.Label--gray")
+  end
+
+  def test_renders_with_the_css_class_variant_mapping_to_the_provided_variant
+    render_inline(Primer::LabelComponent.new(title: "title", variant: :large)) { "private" }
+
+    assert_selector(".Label.Label--large")
+  end
+
+  def test_falls_back_when_variant_isn_t_valid
+    without_fetch_or_fallback_raises do
+      render_inline(Primer::LabelComponent.new(title: "title", variant: :xxl)) { "content" }
+    end
+
+    assert_text("content")
+  end
+end
