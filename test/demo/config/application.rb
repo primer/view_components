7d6bbda3b106e0faff38028057c6326ebea4b5df
manuelpuyol <manuelpuyol@github.com>
2020-08-04 17:37:23 -0500

    lint

--- test/demo/config/application.rb
+++ test/demo/config/application.rb
@@ -18,8 +18,8 @@ module Demo
     config.action_dispatch.default_headers.clear
 
     config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Origin' => '*',
-      'Access-Control-Request-Method' => %w{GET}.join(",")
+      "Access-Control-Allow-Origin" => "*",
+      "Access-Control-Request-Method" => %w{GET}.join(",")
     }
   end
 end
