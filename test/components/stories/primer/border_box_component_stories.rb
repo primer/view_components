24efbfb1d07df58c1fe62d7e1333892278b50919
manuelpuyol <manuelpuyol@github.com>
2020-08-05 11:29:40 -0500

    add stories

---
+++ test/components/stories/primer/border_box_component_stories.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+class Primer::BorderBoxComponentStories < ViewComponent::Storybook::Stories
+  layout "storybook_preview"
+
+  story(:full_box) do
+    content do |component|
+      component.slot(:header) { "Header" }
+      component.slot(:body) { "Body" }
+      component.slot(:row) { "Row one" }
+      component.slot(:row) { "Row two" }
+      component.slot(:row) { "Row three" }
+      component.slot(:footer) { "Footer" }
+    end
+  end
+
+  story(:header) do
+    content do |component|
+      component.slot(:header) { "Header" }
+    end
+  end
+
+  story(:body) do
+    content do |component|
+      component.slot(:body) { "Body" }
+    end
+  end
+
+  story(:footer) do
+    content do |component|
+      component.slot(:footer) { "Footer" }
+    end
+  end
+
+  story(:rows) do
+    content do |component|
+      component.slot(:row) { "Row one" }
+      component.slot(:row) { "Row two" }
+      component.slot(:row) { "Row three" }
+    end
+  end
+end
