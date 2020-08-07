40bb12a63d2b834d93755195a6daf2d2e7437852
manuelpuyol <manuelpuyol@github.com>
2020-08-07 16:17:25 -0500

    tmp

--- app/components/primer/view_components.rb
+++ app/components/primer/view_components.rb
@@ -10,36 +10,36 @@ require "view_component/engine"
 
 require "octicons_helper/helper"
 
-# Helpers
+Primer::ViewComponents::PATHS = [
+  "lib/primer/class_name_helper",
+  "lib/primer/classify",
+  "lib/primer/fetch_or_fallback_helper",
+  "app/components/primer/component",
+  "app/components/primer/base_component",
+  "app/components/primer/slot",
+  "app/components/primer/border_box_component",
+  "app/components/primer/box_component",
+  "app/components/primer/breadcrumb_component",
+  "app/components/primer/button_component",
+  "app/components/primer/counter_component",
+  "app/components/primer/details_component",
+  "app/components/primer/dropdown_menu_component",
+  "app/components/primer/flex_component",
+  "app/components/primer/flex_item_component",
+  "app/components/primer/heading_component",
+  "app/components/primer/label_component",
+  "app/components/primer/layout_component",
+  "app/components/primer/link_component",
+  "app/components/primer/progress_bar_component",
+  "app/components/primer/state_component",
+  "app/components/primer/subhead_component",
+  "app/components/primer/text_component",
+  "app/components/primer/timeline_item_component",
+  "app/components/primer/underline_nav_component"
+]
 
-require "primer/class_name_helper"
-require "primer/classify"
-require "primer/fetch_or_fallback_helper"
-
-# Base configurations
-
-require_relative "component"
-require_relative "base_component"
-require_relative "slot"
-
-# Components
-
-require_relative "border_box_component"
-require_relative "box_component"
-require_relative "breadcrumb_component"
-require_relative "button_component"
-require_relative "counter_component"
-require_relative "details_component"
-require_relative "dropdown_menu_component"
-require_relative "flex_component"
-require_relative "flex_item_component"
-require_relative "heading_component"
-require_relative "label_component"
-require_relative "layout_component"
-require_relative "link_component"
-require_relative "progress_bar_component"
-require_relative "state_component"
-require_relative "subhead_component"
-require_relative "text_component"
-require_relative "timeline_item_component"
-require_relative "underline_nav_component"
+unless Rails.env.development?
+  PATHS.each do |path|
+    require path
+  end
+end
--- lib/primer/view_components/engine.rb
+++ lib/primer/view_components/engine.rb
@@ -4,6 +4,22 @@ module Primer
   module ViewComponents
     class Engine < ::Rails::Engine
       isolate_namespace Primer::ViewComponents
+
+      if Rails.env.development?
+        config.to_prepare do
+          Rails.logger.debug "RELOADING COMPONENTS"
+          require_dependency Primer::ViewComponents::Engine.root.join('app', 'components', 'primer', 'view_components.rb')
+          Primer::ViewComponents::PATHS.each do |path|
+            require_dependency Primer::ViewComponents::Engine.root.join(path)
+          end
+        end
+
+        config.after_initialize do
+          # optional, without it will call `to_prepend` only when a file changes,
+          # not on every request
+          Rails.application.config.reload_classes_only_on_change = false
+        end
+      end
     end
   end
 end
