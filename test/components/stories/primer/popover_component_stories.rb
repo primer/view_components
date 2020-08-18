d62908d0fa0daabd31fb0f65824e9bb08f3a7109
Sarah Vessels <cheshire137@gmail.com>
2020-08-18 15:37:35 -0500

    Define different stories for popovers
    Like in test/components/stories/primer/border_box_component_stories.rb

--- test/components/stories/primer/popover_component_stories.rb
+++ test/components/stories/primer/popover_component_stories.rb
@@ -4,16 +4,28 @@ class Primer::PopoverComponentStories < ViewComponent::Storybook::Stories
   layout "storybook_preview"
 
   story(:popover) do
-    controls do
-      select(:caret,
-        Primer::PopoverComponent::Message::CARET_MAPPINGS.each_with_object({}) { |k, h| h[k] = k },
-        :primary)
-    end
-
     content do |component|
       component.slot(:heading) { "Popover heading" }
       component.slot(:message) { "Message about this particular piece of UI." }
       component.slot(:button) { "Got it!" }
+    end
+  end
+
+  story(:large_popover) do
+    content do |component|
+      component.slot(:heading) { "Popover heading" }
+      component.slot(:message, large: true) { "Message about this particular piece of UI." }
+      component.slot(:button) { "Got it!" }
+    end
+  end
+
+  Primer::PopoverComponent::Message::CARET_MAPPINGS.keys.each do |caret|
+    story("popover_#{caret}".to_sym) do
+      content do |component|
+        component.slot(:heading) { "Popover heading" }
+        component.slot(:message, caret: caret) { "Message about this particular piece of UI." }
+        component.slot(:button) { "Got it!" }
+      end
     end
   end
 end
