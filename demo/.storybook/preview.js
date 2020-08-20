dea3ca0105e5d9ac9a2d105ec087936d4922e02b
manuelpuyol <manuelpuyol@github.com>
2020-08-20 12:07:36 -0500

    build storybook

--- demo/.storybook/preview.js
+++ demo/.storybook/preview.js
@@ -1,5 +1,10 @@
+const isProd = process.env.NODE_ENV == 'production';
+const serverHost = isProd ? 'https://primer-view-components.herokuapp' : 'http://localhost:4000';
+
+console.log("serverHost", serverHost)
+
 export const parameters = {
   server: {
-    url: `http://localhost:4000/rails/stories`,
+    url: `${serverHost}/rails/stories`,
   }
 };
