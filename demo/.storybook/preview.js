541a78fed3f0c1b82f2b64d2cc489faa6c1d69e5
manuelpuyol <manuelpuyol@github.com>
2020-08-20 12:23:15 -0500

    update package json and remove log

--- demo/.storybook/preview.js
+++ demo/.storybook/preview.js
@@ -1,8 +1,6 @@
 const isProd = process.env.NODE_ENV == 'production';
 const serverHost = isProd ? 'https://primer-view-components.herokuapp' : 'http://localhost:4000';
 
-console.log("serverHost", serverHost)
-
 export const parameters = {
   server: {
     url: `${serverHost}/rails/stories`,
