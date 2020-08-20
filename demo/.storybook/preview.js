18c80ac950a630c814f412647bceb34b27478a59
manuelpuyol <manuelpuyol@github.com>
2020-08-20 16:34:46 -0500

    fix heroku logger and url

--- demo/.storybook/preview.js
+++ demo/.storybook/preview.js
@@ -1,5 +1,5 @@
 const isProd = process.env.NODE_ENV == 'production';
-const serverHost = isProd ? 'https://primer-view-components.herokuapp' : 'http://localhost:4000';
+const serverHost = isProd ? 'https://primer-view-components.herokuapp.com' : 'http://localhost:4000';
 
 export const parameters = {
   server: {
--- demo/config/environments/production.rb
+++ demo/config/environments/production.rb
@@ -78,9 +78,7 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.logger = Logger.new(STDOUT)
   end
 
   # Inserts middleware to perform automatic connection switching.
