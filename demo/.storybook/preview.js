0a0110cf3e94a629f9af3b75c9b6952962a806a5
Manuel Puyol <manuelpuyol@github.com>
2020-09-21 13:37:35 -0500

    change env var name

--- demo/.storybook/preview.js
+++ demo/.storybook/preview.js
@@ -1,10 +1,10 @@
 const isProd = process.env.NODE_ENV == 'production';
-const herokuAppName = process.env.HEROKU_APP_NAME;
+const appName = process.env.STORYBOOK_APP_NAME;
 
 console.log("======== TEST ==========");
 console.log(process.env);
 
-const serverHost = isProd ? `https://${herokuAppName}.herokuapp.com` : 'http://localhost:4000';
+const serverHost = isProd ? `https://${appName}.herokuapp.com` : 'http://localhost:4000';
 
 export const parameters = {
   server: {
