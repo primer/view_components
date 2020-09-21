bc4e26abb1e924d31e48b7b3931bc74389556e20
Manuel Puyol <manuelpuyol@github.com>
2020-09-21 13:42:18 -0500

    remove logging

--- demo/.storybook/preview.js
+++ demo/.storybook/preview.js
@@ -1,9 +1,6 @@
 const isProd = process.env.NODE_ENV == 'production';
 const appName = process.env.STORYBOOK_APP_NAME;
 
-console.log("======== TEST ==========");
-console.log(process.env);
-
 const serverHost = isProd ? `https://${appName}.herokuapp.com` : 'http://localhost:4000';
 
 export const parameters = {
