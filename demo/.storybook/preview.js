923d47ea508c1a7c130a735fb73ce90210bdf2be
Manuel Puyol <manuelpuyol@github.com>
2020-09-21 13:18:20 -0500

    test to check process.env

--- demo/.storybook/preview.js
+++ demo/.storybook/preview.js
@@ -1,6 +1,9 @@
 const isProd = process.env.NODE_ENV == 'production';
 const herokuAppName = process.env.HEROKU_APP_NAME;
 
+console.log("======== TEST ==========");
+console.log(process.env);
+
 const serverHost = isProd ? `https://${herokuAppName}.herokuapp.com` : 'http://localhost:4000';
 
 export const parameters = {
