3fb35dba3a949f56de18b3259193dbfa02b4c919
Natasha Umer <NatashaU@github.com>
2020-09-22 13:31:45 -0400

    add storybook for link component

---
+++ stories/primer/link_component_stories.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class Primer::LinkComponentStories < ViewComponent::Storybook::Stories
+  layout "storybook_preview"
+
+  story(:link) do
+    controls do
+      href "https://github.com/"
+      muted false
+    end
+
+    content do
+      "This is a link!"
+    end
+  end
+end
