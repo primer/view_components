91a1a37609e7c2749d023643543ac343ea5898ab
Cole Bemis <colebemis@github.com>
2020-10-16 10:55:41 -0700

    Add spinner tests

---
+++ test/components/spinner_component_test.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class PrimerSpinnerComponentTest < Minitest::Test
+  include Primer::ComponentTestHelpers
+
+  def test_renders_an_svg
+    render_inline(Primer::SpinnerComponent.new())
+
+    assert_selector("spinner")
+  end
+
+  def test_defaults_to_size_32
+    render_inline(Primer::SpinnerComponent.new())
+
+    assert_selector("svg[height=32][width=32]")
+  end
+
+  def test_sets_size_height_and_width
+    render_inline(Primer::SpinnerComponent.new(size: 16))
+
+    assert_selector("svg[height=16][width=16]")
+  end
+end
