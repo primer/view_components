d9a94f938d440effe555a57665c98ae8a83cfafc
Natasha Umer <NatashaU@github.com>
2020-10-19 17:15:52 +0200

    decrement storybook linter

--- test/components/component_test.rb
+++ test/components/component_test.rb
@@ -83,9 +83,9 @@ class PrimerComponentTest < Minitest::Test
 
   def test_components_storybook_count
 
+    # Should be Should be deprecated each time a new storybook is added to a component
     # Should be incremented if a new view component is added without a storybook
-    # Should be incremented if a new view component is added without a storybook
-    expected_missing_stories = 12
+    expected_missing_stories = 11
 
     expected_components_count = COMPONENTS_WITH_ARGS.length
 
