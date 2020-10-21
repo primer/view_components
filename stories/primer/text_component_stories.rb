fb4675a264bb2db8e2b4c8c585611d6eefc4cec3
Joel Hawksley <joelhawksley@github.com>
2020-10-21 08:21:42 -0600

    Update stories/primer/text_component_stories.rb

--- stories/primer/text_component_stories.rb
+++ stories/primer/text_component_stories.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Primer::TextComponentStories < ViewComponent::Storybook::Stories
-  layout layout "storybook_preview"
+  layout "storybook_preview"
 
   story(:text) do
     content do
