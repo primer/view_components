505917c4f7cdb89d45cc5acde24f28fe5f76a6af
Sarah Vessels <cheshire137@gmail.com>
2020-11-23 13:29:34 -0600

    Add some stories for select menu

---
+++ stories/primer/select_menu_component_stories.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+class Primer::SelectMenuComponentStories < ViewComponent::Storybook::Stories
+  layout "storybook_centered_preview"
+
+  story(:select_menu) do
+    content do |component|
+      component.slot(:modal) { "hello world" }
+    end
+  end
+
+  story(:select_menu_with_message) do
+    content do |component|
+      component.slot(:modal, message: "Goodness me") { "hello world" }
+    end
+  end
+
+  story(:select_menu_with_header) do
+    content do |component|
+      component.slot(:header) { "A nice title" }
+      component.slot(:modal) { "hello world" }
+    end
+  end
+
+  story(:select_menu_with_header_and_close_button) do
+    content do |component|
+      component.slot(:header) { "A nice title" }
+      component.slot(:close_button) { "close me" }
+      component.slot(:modal) { "hello world" }
+    end
+  end
+
+  story(:select_menu_with_filter) do
+    content do |component|
+      component.slot(:filter) { "filter description" }
+      component.slot(:modal) { "hello world" }
+    end
+  end
+
+  story(:select_menu_with_footer) do
+    content do |component|
+      component.slot(:modal) { "hello world" }
+      component.slot(:footer) { "the end" }
+    end
+  end
+end
