2d1d4fb94d0bd7d41833318ab5c358480e253806
Simon Taranto <simon.taranto@gmail.com>
2021-01-19 13:45:13 -0700

    Add docs example for BoxComponent

--- app/components/primer/box_component.rb
+++ app/components/primer/box_component.rb
@@ -3,6 +3,9 @@
 module Primer
   # A basic wrapper component for most layout related needs.
   class BoxComponent < Primer::Component
+    # @example
+    #   <%= render(Primer::BoxComponent.new) %>
+    #
     # @param system_arguments [Hash] <%= link_to_system_arguments_docs %>
     def initialize(**system_arguments)
       @system_arguments = system_arguments
