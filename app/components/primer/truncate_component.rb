e7588306fb8be147e958cc98b41e2cdf5d1bb048
Manuel Puyol <manuelpuyol@github.com>
2021-01-28 11:39:40 -0600

    add docs, only allow expandable on inline elements

--- app/components/primer/truncate_component.rb
+++ app/components/primer/truncate_component.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
 module Primer
+  # The Truncate component is a wrapper component that will shorten text with an ellipsis.
   class TruncateComponent < Primer::Component
-    ALIGN_DEFAULT = :left
-    ALIGN_OPTIONS = [ALIGN_DEFAULT, :right]
-
-    def initialize(align: ALIGN_DEFAULT, inline: false, expandable: false, **system_arguments)
+    # @example 25|Default
+    #   <%= render(Primer::TruncateComponent.new(tag: :p)) { "branch-name-that-is-really-long" } %>
+    #
+    # @example 25|Inline
+    #   <%= render(Primer::TruncateComponent.new(tag: :span, inline: true)) { "branch-name-that-is-really-long" } %>
+    #
+    # @example 25|Expandable
+    #   <%= render(Primer::TruncateComponent.new(tag: :span, inline: true, expandable: true)) { "branch-name-that-is-really-long" } %>
+    #
+    # @param inline [Boolean] Whether the element is inline (or inline-block).
+    # @param expandable [Boolean] Whether the entire string should be revealed on hover. Can only be used in conjuction with inline.
+    # @param system_arguments [Hash] <%= link_to_system_arguments_docs %>
+    def initialize(inline: false, expandable: false, **system_arguments)
       @align = fetch_or_fallback(ALIGN_OPTIONS, align, ALIGN_DEFAULT)
 
       @system_arguments = system_arguments
@@ -15,7 +25,7 @@ module Primer
         "css-truncate",
         "css-truncate-overflow" => !inline,
         "css-truncate-target" => inline,
-        "expandable" => expandable
+        "expandable" => inline && expandable
       )
     end
   end
