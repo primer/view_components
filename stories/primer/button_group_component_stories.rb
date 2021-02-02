7854cc0e7e5c148db75e873f21e17f20cefe947c
Manuel Puyol <manuelpuyol@github.com>
2021-02-02 16:42:03 -0600

    update stories to use custom buttons

--- stories/primer/button_group_component_stories.rb
+++ stories/primer/button_group_component_stories.rb
@@ -4,11 +4,12 @@ class Primer::ButtonGroupComponentStories < ViewComponent::Storybook::Stories
   layout "storybook_preview"
 
   story(:button_group) do
-    content do |component|
-      component.button { "Button" }
-      component.button { "Button" }
-      component.button { "Button" }
-      component.button { "Button" }
+    content do |c|
+      c.button { "Button" }
+      c.button(button_type: :primary) { "Primary" }
+      c.button(button_type: :danger) { "Danger" }
+      c.button(button_type: :outline) { "Outline" }
+      c.button(classes: "my-class") { "Custom class" }
     end
   end
 end
