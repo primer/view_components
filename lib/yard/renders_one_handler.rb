b919219f5e5391497b1c7c54016db967fde5365d
Manuel Puyol <manuelpuyol@github.com>
2021-02-09 12:30:08 -0600

    add custom handlers to YARD

---
+++ lib/yard/renders_one_handler.rb
@@ -0,0 +1,16 @@
+module YARD
+  class RendersOneHandler < YARD::Handlers::Ruby::Base
+    handles method_call(:renders_one)
+    namespace_only
+
+    process do
+      name = statement.parameters.first.jump(:tstring_content, :ident).source
+      object = YARD::CodeObjects::MethodObject.new(namespace, name)
+      register(object)
+      parse_block(statement.last, :owner => object)
+
+      object.dynamic = true
+      object[:renders_one] = true
+    end
+  end
+end
