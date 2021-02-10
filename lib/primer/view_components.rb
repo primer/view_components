243e9d266b0da30ad993edbd188fe1a57e9adb57
Simon Taranto <simon.taranto@gmail.com>
2021-02-10 10:52:30 -0700

    Add static dump of component statuses
    We'd like a way to inspect the statuses of components in projects where
    this library is used without having to load the entire library (which
    requires loading Rails and can be slow for things like linters).
    
    This PR adds some code and a rake task to dump the components' statuses
    as JSON into a file that can be read and parsed without loading the
    environment.
    
    We also add specs to make sure the file is up to date when developers
    change the status of a component. If they change a status and don't
    update this file, CI will fail and tell them to run the rake task and
    commit the diff.

--- lib/primer/view_components.rb
+++ lib/primer/view_components.rb
@@ -6,5 +6,38 @@ require "primer/view_components/engine"
 module Primer
   # :nodoc
   module ViewComponents
+    DEFAULT_STATUSES_PATH = File.expand_path("static")
+    DEFAULT_STATUS_FILE_NAME = "statuses.json"
+
+    # generate_statuses returns a hash mapping component name to
+    # the component's status
+    def self.generate_statuses
+      statuses = {}
+      components = Primer::Component.descendants
+
+      components.each do |component|
+        statuses[component.to_s] = component.status.to_s
+      end
+
+      statuses
+    end
+
+    # dump_statuses generates the status hash and then serializes
+    # it as json at the given path
+    def self.dump_statuses(path: DEFAULT_STATUSES_PATH)
+      require "json"
+
+      statuses = generate_statuses
+
+      File.open(File.join(path, DEFAULT_STATUS_FILE_NAME), "w") do |f|
+        f.write(statuses.to_json)
+      end
+    end
+
+    # read_statuses returns a JSON string matching the output of
+    # generate_statuses
+    def self.read_statuses(path: DEFAULT_STATUSES_PATH)
+      File.read(File.join(path, DEFAULT_STATUS_FILE_NAME))
+    end
   end
 end
