b16d81506e00f55e6e167515355d226a8d2f88d8
Manuel Puyol <manuelpuyol@github.com>
2021-02-12 16:09:32 -0600

    add to component_test

--- test/components/component_test.rb
+++ test/components/component_test.rb
@@ -8,6 +8,9 @@ class PrimerComponentTest < Minitest::Test
   # Components with any arguments necessary to make them render
   COMPONENTS_WITH_ARGS = [
     [Primer::AvatarComponent, { alt: "github", src: "https://github.com/github.png" }],
+    [Primer::AvatarStackComponent, {}, lambda do |component|
+      component.avatar(alt: "github", src: "https://github.com/github.png")
+    end],
     [Primer::BaseComponent, { tag: :div }],
     [Primer::BlankslateComponent, { title: "Foo" }],
     [Primer::BorderBoxComponent, {}, proc { |component| component.slot(:header) { "Foo" } }],
