1d8df0d7011726993e396a7f5e345668b5bc8d02
Joel Hawksley <joel@hawksley.org>
2021-02-23 09:43:43 -0700

    add with_preview helper

--- test/components/counter_component_integration_test.rb
+++ test/components/counter_component_integration_test.rb
@@ -4,8 +4,14 @@ require "application_system_test_case"
 
 class CounterComponentIntegrationTest < ApplicationSystemTestCase
   def test_integration
-    visit("/rails/view_components/counter_component/default")
+    with_preview(:default)
 
     assert_selector(".Counter", text: "2")
+  end
+
+  def with_preview(preview_name)
+    component_uri = self.class.name.split("Integration").first.underscore
+
+    visit("/rails/view_components/#{component_uri}/#{preview_name}")
   end
 end
