521b350d74a593b1568c23622070316e0d145c42
Manuel Puyol <manuelpuyol@github.com>
2021-02-23 16:51:30 -0600

    add stories for tab components

---
+++ stories/primer/tab_nav_component_stories.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class Primer::TabNavComponentStories < ViewComponent::Storybook::Stories
+  layout "storybook_preview"
+
+  story(:tab_nav) do
+    content do |c|
+      c.tab(selected: true, title: "Tab 1")
+      c.tab(title: "Tab 2")
+      c.tab(title: "Tab 3")
+    end
+  end
+
+  story(:with_panel) do
+    controls do
+      with_panel true
+    end
+
+    content do |c|
+      c.tab(selected: true, title: "Tab 1") { "Panel 1" }
+      c.tab(title: "Tab 2") { "Panel 2" }
+      c.tab(title: "Tab 3") { "Panel 3" }
+    end
+  end
+end
