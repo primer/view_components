0bdd38c5684355616485ea108dbc3f8cd8d8a3ea
Manuel Puyol <manuelpuyol@github.com>
2021-02-24 14:29:22 -0600

    add separate test for error message

--- test/primer/dsl/view_helper_test.rb
+++ test/primer/dsl/view_helper_test.rb
@@ -6,16 +6,24 @@ class TestComponent < ViewComponent::Base
   include Primer::ViewHelper::DSL
 end
 
+class OtherTestComponent < ViewComponent::Base
+  include Primer::ViewHelper::DSL
+end
+
 class Primer::ViewHelper::DSLTest < Minitest::Test
   def test_register_view_helpers
     assert_respond_to(TestComponent, :view_helper)
 
     TestComponent.view_helper :test
 
-    assert_raises Primer::ViewHelper::DSL::ViewHelperAlreadyDefined do
-      TestComponent.view_helper :test
-    end
+    assert_equal TestComponent, TestComponent.primer_helpers[:test]
+  end
 
-    assert_equal TestComponent, TestComponent.helpers[:test]
+  def test_raises_if_helper_already_defined
+    OtherTestComponent.view_helper :test
+
+    assert_raises Primer::ViewHelper::DSL::ViewHelperAlreadyDefined do
+      OtherTestComponent.view_helper :test
+    end
   end
 end
