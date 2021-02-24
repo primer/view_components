aceef12879e40a8385cf1c0fd4d727340f07c64b
Manuel Puyol <manuelpuyol@github.com>
2021-02-24 16:10:43 -0600

    update specs

--- test/components/tab_nav_component_test.rb
+++ test/components/tab_nav_component_test.rb
@@ -40,7 +40,7 @@ class PrimerTabNavComponentTest < Minitest::Test
   end
 
   def test_renders_tab_panels
-    render_inline(Primer::TabNavComponent.new) do |c|
+    render_inline(Primer::TabNavComponent.new(with_panel: true)) do |c|
       c.tab(tag: :button, selected: true, title: "Tab 1") do
         "Panel 1"
       end
@@ -60,7 +60,7 @@ class PrimerTabNavComponentTest < Minitest::Test
   end
 
   def test_only_renders_panels_for_tabs_with_content
-    render_inline(Primer::TabNavComponent.new) do |c|
+    render_inline(Primer::TabNavComponent.new(with_panel: true)) do |c|
       c.tab(tag: :button, selected: true, title: "Tab 1") do
         "Panel 1"
       end
@@ -74,5 +74,24 @@ class PrimerTabNavComponentTest < Minitest::Test
       end
     end
     assert_selector("div[role='tabpanel']", count: 1)
+  end
+
+  def test_does_not_render_panels_when_with_panel_is_false
+    render_inline(Primer::TabNavComponent.new(with_panel: false)) do |c|
+      c.tab(tag: :button, selected: true, title: "Tab 1") do
+        "Panel 1"
+      end
+      c.tab(tag: :button, title: "Tab 2") do
+        "Panel 2"
+      end
+    end
+
+    assert_selector(".tabnav") do
+      assert_selector("nav.tabnav-tabs[role='tablist']") do
+        assert_selector("button.tabnav-tab[aria-selected='true'][role='tab']", text: "Tab 1")
+        assert_selector("button.tabnav-tab[role='tab']", text: "Tab 2")
+      end
+    end
+    refute_selector("div[role='tabpanel']")
   end
 end
