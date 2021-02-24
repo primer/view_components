7712d1493abb93dfa7de98494621cae1eb00e82e
Manuel Puyol <manuelpuyol@github.com>
2021-02-24 14:22:01 -0600

    lint

--- lib/primer/view_components/engine.rb
+++ lib/primer/view_components/engine.rb
@@ -7,15 +7,13 @@ module Primer
     # :nodoc:
     class Engine < ::Rails::Engine
       isolate_namespace Primer::ViewComponents
-      config.autoload_once_paths = %W(
+      config.autoload_once_paths = %W[
         #{root}/app/components
         #{root}/app/lib
-      )
+      ]
 
       initializer "primer_view_components.assets" do |app|
-        if app.config.respond_to?(:assets)
-          app.config.assets.precompile += %w(primer_view_components)
-        end
+        app.config.assets.precompile += %w[primer_view_components] if app.config.respond_to?(:assets)
       end
     end
   end
