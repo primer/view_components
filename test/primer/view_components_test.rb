6a1423571a2ba93133f22a8de236fbd3906e3675
Simon Taranto <simon.taranto@gmail.com>
2021-02-25 13:29:53 -0700

    Remove statuses.json spec
    Now that we build the statuses file in CI we don't need to ask devs to
    do this.

--- test/primer/view_components_test.rb
+++ test/primer/view_components_test.rb
@@ -19,11 +19,7 @@ class Primer::ViewComponentsTest < Minitest::Test
       assert false, "Hi! statuses.json is missing. Please run `bundle exec rake statuses:dump` and commit the diff"
     end
 
-    # we make sure any changes to statuses are committed to the static file
-    assert_equal statuses, JSON.parse(read_statuses), "Hi! statuses.json is out of date. Please run `bundle exec rake statuses:dump` and commit the diff"
-
-    # Now that we know we're up to date, we make sure that we can
-    # dump to disk and then read back again
+    # we make sure that we can dump to disk and then read back again
     Primer::ViewComponents.dump_statuses
 
     assert_equal Primer::ViewComponents.generate_statuses.to_json, Primer::ViewComponents.read_statuses.chomp
