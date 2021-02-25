f1216eced642c0e9712b8c7f82033e485daedf40
Simon Taranto <simon.taranto@gmail.com>
2021-02-25 13:32:29 -0700

    Fix up the spec

--- test/primer/view_components_test.rb
+++ test/primer/view_components_test.rb
@@ -10,15 +10,7 @@ class Primer::ViewComponentsTest < Minitest::Test
     assert_equal String, statuses.values.first.class
   end
 
-  def test_written_statuses_are_up_to_date
-    statuses = Primer::ViewComponents.generate_statuses
-
-    begin
-      read_statuses = Primer::ViewComponents.read_statuses
-    rescue Errno::ENOENT
-      assert false, "Hi! statuses.json is missing. Please run `bundle exec rake statuses:dump` and commit the diff"
-    end
-
+  def test_writing_and_reading_statuses
     # we make sure that we can dump to disk and then read back again
     Primer::ViewComponents.dump_statuses
 
