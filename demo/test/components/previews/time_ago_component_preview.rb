1cdfe3eda5b29216e47504fa5fb94bb9995793af
Keith Cirkel <keithamus@users.noreply.github.com>
2021-03-02 16:44:44 +0000

    test: move TimeAgoComponentPreview into Primer subfolder

--- demo/test/components/previews/time_ago_component_preview.rb
+++
@@ -1,9 +0,0 @@
-class TimeAgoComponentPreview < ViewComponent::Preview
-  def default
-    render(Primer::TimeAgoComponent.new(time: Time.new))
-  end
-
-  def micro
-    render(Primer::TimeAgoComponent.new(time: Time.new, micro: true))
-  end
-end
