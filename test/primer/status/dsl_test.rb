12c954a85b47013c813ac517fb36bc3adabe8e7a
Manuel Puyol <manuelpuyol@github.com>
2021-03-03 15:09:02 -0600

    indepotent test

--- test/primer/status/dsl_test.rb
+++ test/primer/status/dsl_test.rb
@@ -11,16 +11,13 @@ class Primer::Status::DslTest < Minitest::Test
     assert_respond_to(TestComponent, :status)
   end
 
-  def test_defaults_to_alpha
+  def test_defaults_to_alpha_and_sets_status
     assert_equal :alpha, TestComponent.status
-  end
 
-  def test_register_status
     TestComponent.status :beta
 
     assert_equal :beta, TestComponent.status
   end
-
   def test_raises_if_status_does_not_exist
     err = assert_raises Primer::Status::Dsl::UnknownStatusError do
       TestComponent.status :foo
