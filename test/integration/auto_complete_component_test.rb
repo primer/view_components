088f1e177578ab5dafa0cb83b3e87bbb1a06ca99
Manuel Puyol <manuelpuyol@github.com>
2021-03-05 15:43:11 -0600

    add some integration testing

---
+++ test/integration/auto_complete_component_test.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "application_system_test_case"
+
+class IntegrationAutoCompleteComponentTest < ApplicationSystemTestCase
+  def test_renders_component
+    with_preview(:default)
+
+    assert_selector("auto-complete[for=\"test-id\"][src=\"/auto_complete\"]") do
+      assert_selector("input.form-control")
+      assert_selector("ul[id=\"test-id\"].autocomplete-results", visible: false)
+    end
+    refute_selector(".autocomplete-item")
+  end
+
+  def test_search_items
+    with_preview(:default)
+
+    fill_in 'input', with: 'a'
+
+    # results are now visible
+    assert_selector("ul[id=\"test-id\"].autocomplete-results", visible: true)
+    assert_selector(".autocomplete-item")
+  end
+end
