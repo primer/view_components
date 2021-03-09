50757268f05562b3ffad4a82cf3a99ae6817ef1f
Manuel Puyol <manuelpuyol@github.com>
2021-03-09 10:45:43 -0600

    update docs

--- app/lib/primer/fetch_or_fallback_helper.rb
+++ app/lib/primer/fetch_or_fallback_helper.rb
@@ -6,13 +6,18 @@
 # Use this helper to quietly ensure a value is
 # one that you expect:
 #
-# allowed_values  - allowed options for *value*
-# given_value     - input being coerced
-# fallback        - returned if *given_value* is not included in *allowed_values*
+# allowed_values    - allowed options for *value*
+# given_value       - input being coerced
+# fallback          - returned if *given_value* is not included in *allowed_values*
+# deprecated_values - deprecated options for *value*. Will warn of deprecation if not in production
 #
 # fetch_or_fallback([1,2,3], 5, 2) => 2
 # fetch_or_fallback([1,2,3], 1, 2) => 1
 # fetch_or_fallback([1,2,3], nil, 2) => 2
+#
+# With deprecations:
+# fetch_or_fallback([1,2], 3, 2, deprecated_values: [3]) => 3
+# fetch_or_fallback([1,2], nil, 2, deprecated_values: [3]) => 2
 module Primer
   # :nodoc:
   module FetchOrFallbackHelper
@@ -24,7 +29,7 @@ module Primer
       if allowed_values.include?(given_value)
         given_value
       elsif deprecated_values.include?(given_value)
-        ActiveSupport::Deprecation.warn("#{given_value} is deprecated and will be removed in a future version.")
+        ActiveSupport::Deprecation.warn("#{given_value} is deprecated and will be removed in a future version.") unless Rails.env.production?
 
         given_value
       else
