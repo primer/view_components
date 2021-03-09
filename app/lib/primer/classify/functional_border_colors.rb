d9ba89529d24c29c7f4d1bb2f32d9c0f5ce7de8f
Manuel Puyol <manuelpuyol@github.com>
2021-03-09 11:25:38 -0600

    extract functional border colors into a class

---
+++ app/lib/primer/classify/functional_border_colors.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+module Primer
+  class Classify
+    # Border specific functional colors
+    class FunctionalBorderColors < FunctionalColors
+      FUNCTIONAL_OPTIONS = {
+        primary: :primary,
+        secondary: :secondary,
+        tertiary: :tertiary,
+        info: :info,
+        success: :success,
+        warning: :warning,
+        danger: :danger,
+        inverse: :inverse,
+        overlay: :overlay
+      }.freeze
+
+      MAPPINGS = {
+        gray: FUNCTIONAL_OPTIONS[:primary],
+        gray_light: FUNCTIONAL_OPTIONS[:secondary],
+        gray_dark: FUNCTIONAL_OPTIONS[:tertiary],
+        blue: FUNCTIONAL_OPTIONS[:info],
+        green: FUNCTIONAL_OPTIONS[:success],
+        yellow: FUNCTIONAL_OPTIONS[:warning],
+        red: FUNCTIONAL_OPTIONS[:danger],
+        white: FUNCTIONAL_OPTIONS[:inverse],
+        # still unsure what will happen with these colors
+        gray_darker: nil,
+        blue_light: nil,
+        red_light: nil,
+        purple: nil,
+        black_fade: nil,
+        white_fade: nil
+      }.freeze
+
+      OPTIONS = FUNCTIONAL_OPTIONS.values.freeze
+      DEPRECATED_OPTIONS = MAPPINGS.keys.freeze
+
+      class << self
+        def call(val)
+          functional_color(
+            key: "border",
+            value: val,
+            mappings: MAPPINGS,
+            non_functional_prefix: "border",
+            functional_prefix: "border-",
+            functional_options: OPTIONS
+          )
+        end
+      end
+    end
+  end
+end
--- app/lib/primer/classify/functional_colors.rb
+++ app/lib/primer/classify/functional_colors.rb
@@ -4,51 +4,7 @@ module Primer
   class Classify
     # https://primer-css-git-mkt-color-modes-docs-primer.vercel.app/css/support/v16-migration
     class FunctionalColors
-      FUNCTIONAL_BORDER_OPTIONS = {
-        primary: :primary,
-        secondary: :secondary,
-        tertiary: :tertiary,
-        info: :info,
-        success: :success,
-        warning: :warning,
-        danger: :danger,
-        inverse: :inverse,
-        overlay: :overlay
-      }.freeze
-
-      BORDER_COLOR_MAPPINGS = {
-        gray: FUNCTIONAL_BORDER_OPTIONS[:primary],
-        gray_light: FUNCTIONAL_BORDER_OPTIONS[:secondary],
-        gray_dark: FUNCTIONAL_BORDER_OPTIONS[:tertiary],
-        blue: FUNCTIONAL_BORDER_OPTIONS[:info],
-        green: FUNCTIONAL_BORDER_OPTIONS[:success],
-        yellow: FUNCTIONAL_BORDER_OPTIONS[:warning],
-        red: FUNCTIONAL_BORDER_OPTIONS[:danger],
-        white: FUNCTIONAL_BORDER_OPTIONS[:inverse],
-        # still unsure what will happen with these colors
-        gray_darker: nil,
-        blue_light: nil,
-        red_light: nil,
-        purple: nil,
-        black_fade: nil,
-        white_fade: nil
-      }.freeze
-
-      BORDER_OPTIONS = FUNCTIONAL_BORDER_OPTIONS.values.freeze
-      DEPRECATED_BORDER_OPTIONS = BORDER_COLOR_MAPPINGS.keys.freeze
-
       class << self
-        def border_color(val)
-          functional_color(
-            key: "border",
-            value: val,
-            mappings: BORDER_COLOR_MAPPINGS,
-            non_functional_prefix: "border",
-            functional_prefix: "border-",
-            functional_options: BORDER_OPTIONS
-          )
-        end
-
         private
 
         def functional_color(key:, value:, mappings:, non_functional_prefix:, functional_options:, functional_prefix: "")
