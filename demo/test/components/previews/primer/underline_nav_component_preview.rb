5710c7821601bbadbc0e6ffcc8975d0487fb3770
Manuel Puyol <manuelpuyol@github.com>
2021-03-17 14:51:58 -0500

    add integration test to UnderlineNavComponent

---
+++ demo/test/components/previews/primer/underline_nav_component_preview.rb
@@ -0,0 +1,20 @@
+module Primer
+  class UnderlineNavComponentPreview < ViewComponent::Preview
+    def default
+      render(Primer::UnderlineNavComponent.new(with_panel: true)) do |c|
+        c.tab(selected: true) do |t|
+          t.panel { "Panel 1" }
+          "Tab 1"
+        end
+        c.tab do |t|
+          t.panel { "Panel 2" }
+          "Tab 2"
+        end
+        c.tab do |t|
+          t.panel { "Panel 3" }
+          "Tab 3"
+        end
+      end
+    end
+  end
+end
