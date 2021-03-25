150b37ad225e034f22d02c2f4b427d1453ea6c57
Manuel Puyol <manuelpuyol@github.com>
2021-03-25 15:33:17 -0500

    link to installation and add doc on how to import a single component

--- docs/src/@primer/gatsby-theme-doctocat/components/requires-js-flash.js
+++ docs/src/@primer/gatsby-theme-doctocat/components/requires-js-flash.js
@@ -4,7 +4,7 @@ import {Flash, Link} from '@primer/components'
 function RequiresJSFlash() {
   return (
     <Flash mb={3}>
-      This component requires JavaScript to function. Please refer to the <Link href="/javascript">JavaScript docs</Link>.
+      This component requires JavaScript to function. Please refer to the <Link href="/#installation">Installation section</Link> to set it up.
     </Flash>
   )
 }
