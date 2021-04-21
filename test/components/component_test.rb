9f19376263cf141e24a48a6fdc49f78d1a8f59df
Kristján Oddsson <koddsson@gmail.com>
2021-04-21 12:57:01 +0100

    Pass attributes to ClipboardCopy component in component test

--- test/components/component_test.rb
+++ test/components/component_test.rb
@@ -21,7 +21,7 @@ class PrimerComponentTest < Minitest::Test
     [Primer::ButtonComponent, {}],
     [Primer::ButtonGroupComponent, {}, proc { |component| component.button { "Button" } }],
     [Primer::ButtonMarketingComponent, {}],
-    [Primer::ClipboardCopy, {}],
+    [Primer::ClipboardCopy, { label: "Sting that will be read to screenreaders", value: "String that will be copied" }],
     [Primer::CloseButton, {}],
     [Primer::CounterComponent, { count: 1 }],
     [Primer::DetailsComponent, {}, lambda do |component|
