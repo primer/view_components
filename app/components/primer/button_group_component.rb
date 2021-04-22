07c8f4e8ae2de6edf2dfb638a10c68de2c21389c
Manuel Puyol <manuelpuyol@github.com>
2021-04-22 09:27:01 -0500

    Set Button variant on ButtonGroup

--- app/components/primer/button_group_component.rb
+++ app/components/primer/button_group_component.rb
@@ -6,19 +6,43 @@ module Primer
     # Required list of buttons to be rendered.
     #
     # @param kwargs [Hash] The same arguments as <%= link_to_component(Primer::ButtonComponent) %>.
-    renders_many :buttons, ->(**kwargs) { Primer::ButtonComponent.new(group_item: true, **kwargs) }
+    renders_many :buttons, lambda { |**kwargs|
+      kwargs[:group_item] = true
+      kwargs[:variant] = @variant
+
+      Primer::ButtonComponent.new(**kwargs)
+    }
 
     # @example Default
-    #   <%= render(Primer::ButtonGroupComponent.new) do |component|
-    #     component.button { "Default" }
-    #     component.button(scheme: :primary) { "Primary" }
-    #     component.button(scheme: :danger) { "Danger" }
-    #     component.button(scheme: :outline) { "Outline" }
-    #     component.button(classes: "my-class") { "Custom class" }
-    #   end %>
     #
+    #   <%= render(Primer::ButtonGroupComponent.new) do |component| %>
+    #     <% component.button { "Default" } %>
+    #     <% component.button(scheme: :primary) { "Primary" } %>
+    #     <% component.button(scheme: :danger) { "Danger" } %>
+    #     <% component.button(scheme: :outline) { "Outline" } %>
+    #     <% component.button(classes: "my-class") { "Custom class" } %>
+    #   <% end %>
+    #
+    # @example Variants
+    #
+    #   <%= render(Primer::ButtonGroupComponent.new(variant: :small)) do |component| %>
+    #     <% component.button { "Default" } %>
+    #     <% component.button(scheme: :primary) { "Primary" } %>
+    #     <% component.button(scheme: :danger) { "Danger" } %>
+    #     <% component.button(scheme: :outline) { "Outline" } %>
+    #   <% end %>
+    #
+    #   <%= render(Primer::ButtonGroupComponent.new(variant: :large)) do |component| %>
+    #     <% component.button { "Default" } %>
+    #     <% component.button(scheme: :primary) { "Primary" } %>
+    #     <% component.button(scheme: :danger) { "Danger" } %>
+    #     <% component.button(scheme: :outline) { "Outline" } %>
+    #   <% end %>
+    #
+    # @param variant [Symbol] <%= one_of(Primer::ButtonComponent::VARIANT_OPTIONS) %>
     # @param system_arguments [Hash] <%= link_to_system_arguments_docs %>
-    def initialize(**system_arguments)
+    def initialize(variant: Primer::ButtonComponent::DEFAULT_VARIANT, **system_arguments)
+      @variant = variant
       @system_arguments = system_arguments
       @system_arguments[:tag] ||= :div
 
