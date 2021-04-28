10d2079aa09839266faca55e78ec260a9c160709
Manuel Puyol <manuelpuyol@github.com>
2021-04-28 09:54:15 -0500

    warn of FlexItemComponent deprecation

--- app/components/primer/component.rb
+++ app/components/primer/component.rb
@@ -14,5 +14,23 @@ module Primer
     include JoinStyleArgumentsHelper
     include ViewHelper
     include Status::Dsl
+
+    private
+
+    def deprecated_component_warning(new_class: nil)
+      return if Rails.env.production? || silence_deprecations?
+
+      message = if new_class
+        "#{self.class.name} is deprecated, please use #{new_class.name} instead."
+      else
+        "#{self.class.name} is deprecated and should not be used."
+      end
+
+      ActiveSupport::Deprecation.warn(message)
+    end
+
+    def silence_deprecations?
+      Rails.application.config.primer_view_components.silence_deprecations
+    end
   end
 end
