f93086e1c8df79df34c1fcc4ca0b71857a6c9d97
Jon Rohan <yes@jonrohan.codes>
2021-04-28 11:05:40 -0700

    Adjust benchmark score

--- test/benchmarks/bench_octicons.rb
+++ test/benchmarks/bench_octicons.rb
@@ -12,7 +12,7 @@ class BenchOcticons < Minitest::Benchmark
 
   def bench_allocations_without_cache
     Primer::OcticonComponent::Cache.clear!
-    assert_allocations 43 do
+    assert_allocations 42 do
       Primer::OcticonComponent.new(**@options)
     end
   end
