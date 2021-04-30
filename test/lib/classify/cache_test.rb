1d7b2a221bac7d00f116fef08fae94eb58fcd823
Joel Hawksley <joel@hawksley.org>
2021-04-30 14:44:10 -0600

    add test to ensure classify doesn't reference nonexistent CSS classes

--- test/lib/classify/cache_test.rb
+++ test/lib/classify/cache_test.rb
@@ -10,4 +10,27 @@ class PrimerClassifyCacheTest < Minitest::Test
   ensure
     Primer::Classify::Cache.preload!
   end
+
+  def test_does_not_generate_primer_css_classes_that_do_not_exist
+    Primer::Classify::Cache.preload!
+
+    classes_from_classify_cache =
+      Primer::Classify::Cache::LOOKUP.
+        values.
+        flat_map { _1.values }.
+        flat_map { _1.values }.
+        map { ".#{_1}" }.
+        uniq
+
+    css_data =
+      JSON.parse(
+        File.read(
+          File.join(
+            __FILE__.split("test")[0], "/node_modules/@primer/css/dist/stats/primer.json"
+          )
+        )
+      )
+
+    assert_empty (classes_from_classify_cache - css_data["selectors"]["values"])
+  end
 end
