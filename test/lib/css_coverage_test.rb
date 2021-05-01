48fd54dcbf7d5da81c4f6b1aa64f6b106820499f
Jon Rohan <yes@jonrohan.codes>
2021-04-30 17:56:43 -0700

    Use var

--- test/lib/css_coverage_test.rb
+++ test/lib/css_coverage_test.rb
@@ -42,7 +42,13 @@ class CssCoverageTest < Minitest::Test
             __FILE__.split("test")[0], "/node_modules/@primer/css/dist/stats/primer.json"
           )
         )
-      )["selectors"]["values"].flat_map { _1.gsub(/(\w)\./, '\1 .').split(/[\s:\[\+>]+/) }.reject { !_1.starts_with?(".") }.uniq
+      )["selectors"]["values"]
+
+    # Cleanup the data to make sure it's only one selector per item
+    @css_data = @css_data
+                .flat_map { |c| c.gsub(/(\w)\./, '\1 .').split(/[\s:\[\+>]+/) }
+                .select { |c| c.starts_with?(".") }
+                .uniq
   end
 
   def test_does_not_generate_primer_css_classes_that_do_not_exist
