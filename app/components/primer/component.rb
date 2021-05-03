9ac7f7f1c83927a0495daffa47d36de485d145ae
Manuel Puyol <manuelpuyol@github.com>
2021-05-03 12:00:34 -0500

    Validate aria-label by checking aria-label and aria: { label }

--- app/components/primer/component.rb
+++ app/components/primer/component.rb
@@ -25,6 +25,11 @@ module Primer
       ActiveSupport::Deprecation.warn(message)
     end
 
+    def validate_aria_label
+      aria_label = @system_arguments[:"aria-label"] || @system_arguments.dig(:aria, :label)
+      raise ArgumentError, "`aria-label` is required." if aria_label.nil? && !Rails.env.production?
+    end
+
     def silence_deprecations?
       Rails.application.config.primer_view_components.silence_deprecations
     end
