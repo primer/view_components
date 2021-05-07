237c0c9910eca92dd6d96b5a24ce5561fb0f97fe
Kristján Oddsson <koddsson@gmail.com>
2021-05-07 13:22:32 +0100

    Update tests with the correct timezone

--- test/system/local_time_component_test.rb
+++ test/system/local_time_component_test.rb
@@ -6,18 +6,18 @@ class IntegrationLocalTimeComponentTest < ApplicationSystemTestCase
   def test_default
     with_preview(:default)
 
-    assert_selector("local-time", text: "Wed Apr 2, 2014 1:30:00 AM")
+    assert_selector("local-time", text: "Wed Apr 2, 2014 8:30:00 AM GMT+8")
   end
 
   def test_with_all_the_options
     with_preview(:with_all_the_options)
 
-    assert_selector("local-time", text: "Wednesday June 01, 16 02:05:07 PM British Summer Time")
+    assert_selector("local-time", text: "Wednesday June 01, 16 09:05:07 PM Taipei Standard Time")
   end
 
   def test_with_contents
     with_preview(:with_contents)
 
-    assert_selector("local-time", text: "Wed Apr 2, 2014 1:30:00 AM")
+    assert_selector("local-time", text: "Wed Apr 2, 2014 8:30:00 AM GMT+8")
   end
 end
