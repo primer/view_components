045adba86a02c0f5e93ffacdaf7b3c29eb4cbc86
Kristján Oddsson <koddsson@gmail.com>
2021-05-12 10:18:41 +0100

    WIP

--- stories/primer/task_list_stories.rb
+++ stories/primer/task_list_stories.rb
@@ -4,12 +4,6 @@ class Primer::TaskListStories < ViewComponent::Storybook::Stories
   layout "storybook_preview"
 
   story(:task_list) do
-    controls do
-      classes "custom-class"
-    end
-
-    content do
-      "Update your stories!"
-    end
+    content
   end
 end
--- test/components/previews/primer/task_list_preview.rb
+++ test/components/previews/primer/task_list_preview.rb
@@ -1,7 +1,15 @@
+# frozen_string_literal: true
+
 module Primer
   class TaskListPreview < ViewComponent::Preview
     def default
-      render(Primer::TaskList.new)
+      render(Primer::TaskList.new) do |component|
+        component.list do |list|
+          list.item do
+            "Foobar"
+          end
+        end
+      end
     end
   end
 end
