dff6610527ae20b44a9fdc549131304a2fe7262e
Manuel Puyol <manuelpuyol@github.com>
2021-05-13 20:31:20 +0000

    add stories

--- stories/primer/image_stories.rb
+++ stories/primer/image_stories.rb
@@ -6,6 +6,11 @@ class Primer::ImageStories < ViewComponent::Storybook::Stories
   story(:image) do
     controls do
       classes "custom-class"
+      src "https://github.com/github.png"
+      alt "image description"
+      height 100
+      width 100
+      select(:loading, Primer::Image::LOADING_OPTIONS, Primer::Image::DEFAULT_LOADING)
     end
 
     content do
