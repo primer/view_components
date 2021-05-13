5afb13d7f7879eca3f3c37f708bc7c318dc634e4
Manuel Puyol <manuelpuyol@github.com>
2021-05-13 20:30:01 +0000

    add tests

--- test/components/image_test.rb
+++ test/components/image_test.rb
@@ -5,7 +5,15 @@ require "test_helper"
 class PrimerImageTest < Minitest::Test
   include Primer::ComponentTestHelpers
 
-  def test_renders
-    assert_text("Add a test here")
+  def test_render_eager_loading_by_default
+    render_inline(Primer::Image.new(src: "src", alt: "alt"))
+
+    assert_selector("img[src='src'][alt='alt'][loading='eager']")
+  end
+
+  def test_lazy_loading
+    render_inline(Primer::Image.new(src: "src", alt: "alt", loading: :lazy))
+
+    assert_selector("img[src='src'][alt='alt'][loading='lazy']")
   end
 end
