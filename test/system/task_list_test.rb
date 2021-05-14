e8baa27fbd39b5562352823117842a0e77388c58
Kristján Oddsson <koddsson@gmail.com>
2021-05-14 14:49:32 +0100

    Fix task_list system tests

--- test/system/task_list_test.rb
+++ test/system/task_list_test.rb
@@ -6,6 +6,13 @@ class IntegrationTaskListTest < ApplicationSystemTestCase
   def test_renders_component
     with_preview(:default)
 
-    assert_selector("task-list")
+    assert_selector("task-lists[sortable]") do
+      assert_selector("ul.contains-task-list") do
+        assert_selector("li.task-list-item") do
+          assert_selector("input[type=checkbox].task-list-item-checkbox")
+          assert_text("Apple")
+        end
+      end
+    end
   end
 end
