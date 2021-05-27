88f4f06be6ce7faa5eeb294c889191d1a08a55a9
Manuel Puyol <manuelpuyol@github.com>
2021-05-27 17:38:07 +0000

    create custom erb linters

---
+++ lib/primer/view_components/linters/flash_component_migration_counter.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require_relative "helpers"
+
+module ERBLint
+  module Linters
+    # Counts the number of times a HTML flash is used instead of the component.
+    class FlashComponentMigrationCounter < Linter
+      include Helpers
+
+      TAGS = %w[div].freeze
+      CLASS = "flash"
+      MESSAGE = "We are migrating flashes to use [Primer::FlashComponent](https://primer.style/view-components/components/flash), please try to use that instead of raw HTML. Reach out to `@github/view-component-reviewers` or #view-component with any questions!"
+    end
+  end
+end
