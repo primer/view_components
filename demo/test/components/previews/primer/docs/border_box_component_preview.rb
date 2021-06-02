430c6ee10cc7df7b2f394881570181908d12370e
Manuel Puyol <manuelpuyol@github.com>
2021-06-02 18:29:34 +0000

    remove previews

--- demo/test/components/previews/primer/docs/border_box_component_preview.rb
+++
@@ -1,9 +0,0 @@
-module Primer
-  module Docs
-    class BorderBoxComponentPreview < ViewComponent::Preview
-      def header_body_rows_and_footer; end
-
-      def padding_density; end
-    end
-  end
-end
