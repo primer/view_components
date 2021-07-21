f20acaec62fbcce2b6d49ed571c9a2d42aa6cf42
Jon Rohan <yes@jonrohan.codes>
2021-07-21 12:55:26 -0700

    Adding more tests

--- test/components/alpha/truncate_test.rb
+++ test/components/alpha/truncate_test.rb
@@ -5,18 +5,40 @@ require "test_helper"
 class PrimerAlphaTruncateTest < Minitest::Test
   include Primer::ComponentTestHelpers
 
+  def test_truncate_supports_block
+    render_inline(Primer::Alpha::Truncate.new) { "content" }
+
+    assert_selector(".Truncate > .Truncate-text", text: "content")
+  end
+
+  def test_truncate_supports_block_with_arguments
+    render_inline(Primer::Alpha::Truncate.new(tag: :button, primary: true, expandable: true, max_width: 1337)) { "content" }
+
+    assert_selector(".Truncate > button.Truncate-text.Truncate-text--primary.Truncate-text--expandable[style='max-width: 1337px;']", text: "content")
+  end
+
+  def test_truncate_wrapper_and_text_tag
+    render_inline(Primer::Alpha::Truncate.new(tag: :button)) do |component|
+      component.item(tag: :strong) { "content" }
+    end
+
+    assert_selector("button.Truncate > strong.Truncate-text", text: "content")
+  end
+
   def test_truncate_with_default_options
     render_inline(Primer::Alpha::Truncate.new) do |component|
-      component.text { "content" }
+      component.item { "content" }
     end
+
+    binding.irb
 
     assert_selector(".Truncate > .Truncate-text", text: "content")
   end
 
   def test_truncate_with_primary
     render_inline(Primer::Alpha::Truncate.new) do |component|
-      component.text { "content" }
-      component.text(primary: true) { "primary content" }
+      component.item { "content" }
+      component.item(primary: true) { "primary content" }
     end
 
     assert_selector(".Truncate > .Truncate-text.Truncate-text--primary", text: "primary content")
@@ -24,8 +46,8 @@ class PrimerAlphaTruncateTest < Minitest::Test
 
   def test_truncate_with_expandable
     render_inline(Primer::Alpha::Truncate.new) do |component|
-      component.text { "content" }
-      component.text(tag: :button, expandable: true) { "expandable content" }
+      component.item { "content" }
+      component.item(tag: :button, expandable: true) { "expandable content" }
     end
 
     assert_selector(".Truncate > button.Truncate-text.Truncate-text--expandable", text: "expandable content")
@@ -33,7 +55,7 @@ class PrimerAlphaTruncateTest < Minitest::Test
 
   def test_truncate_with_max_width
     render_inline(Primer::Alpha::Truncate.new) do |component|
-      component.text(max_width: 1337) { "content" }
+      component.item(max_width: 1337) { "content" }
     end
 
     assert_selector(".Truncate > .Truncate-text[style='max-width: 1337px;']", text: "content")
