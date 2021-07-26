90b49702f45f8c2c0113ba4b8802c8402985a297
Jon Rohan <yes@jonrohan.codes>
2021-07-26 11:30:13 -0700

    Fix story content

--- stories/primer/beta/truncate/truncate_text_stories.rb
+++ stories/primer/beta/truncate/truncate_text_stories.rb
@@ -9,11 +9,11 @@ class Primer::Beta::Truncate::TruncateTextStories < ViewComponent::Storybook::St
     controls do
       priority false
       expandable false
-      max_width nil
+      max_width 125
     end
 
     content do
-      "Item"
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     end
   end
 end
