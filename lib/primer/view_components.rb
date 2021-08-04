73d839d718ca65de1be459c243e2eb73b2299817
Manuel Puyol <manuelpuyol@github.com>
2021-08-04 20:41:46 +0000

    move view_helper to lib/

--- lib/primer/view_components.rb
+++ lib/primer/view_components.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "primer/classify"
+require "primer/view_helper"
 require "primer/view_components/version"
 require "primer/view_components/engine"
 
---
+++ lib/primer/view_helper.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Primer
+  # Module to allow shorthand calls for Primer components
+  module ViewHelper
+    class ViewHelperNotFound < StandardError; end
+
+    HELPERS = {
+      octicon: "Primer::OcticonComponent",
+      heading: "Primer::HeadingComponent",
+      time_ago: "Primer::TimeAgoComponent",
+      image: "Primer::Image"
+    }.freeze
+
+    HELPERS.each do |name, component|
+      define_method "primer_#{name}" do |*args, **kwargs, &block|
+        render component.constantize.new(*args, **kwargs), &block
+      end
+    end
+  end
+end
