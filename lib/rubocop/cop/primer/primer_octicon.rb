db45ac48fc4aa0dce9a783916bf18ff8ebd48011
Manuel Puyol <manuelpuyol@github.com>
2021-08-05 16:06:35 +0000

    somewhat working

--- lib/rubocop/cop/primer/primer_octicon.rb
+++ lib/rubocop/cop/primer/primer_octicon.rb
@@ -18,7 +18,9 @@ module RuboCop
           Replace the octicon helper with primer_octicon. See https://primer.style/view-components/components/octicon for details.
         STR
 
-        VALID_ATTRIBUTES = %i[height width size class].freeze
+        SIZE_ATTRIBUTES = %i[height width size].freeze
+        VALID_ATTRIBUTES = [*SIZE_ATTRIBUTES, :class].freeze
+        INVALID_CLASSES = -1
 
         def on_send(node)
           return unless node.method_name == :octicon
@@ -29,16 +31,16 @@ module RuboCop
 
           # Don't convert unknown attributes
           return unless (attributes - VALID_ATTRIBUTES).empty?
+          # Can't convert size
+          return if (SIZE_ATTRIBUTES & attributes).any? && octicon_size(kwargs).nil?
 
           # find class pair
-          class_arg = kwargs.pairs.find { |kwarg| kwarg.key.value == :class }
+          classes = classes(kwargs)
 
-          if class_arg.present?
-            return unless class_arg.value.type == :str
+          return if classes == INVALID_CLASSES
 
-            # get actual classes
-            classes = class_arg.value.value
-
+          # check if classes are convertible
+          if classes.present?
             system_arguments = ::Primer::Classify::Utilities.classes_to_hash(classes)
 
             # no classes are fixable
@@ -48,28 +50,64 @@ module RuboCop
           add_offense(node, message: INVALID_MESSAGE)
         end
 
-        # def autocorrect(node)
-        #   lambda do |corrector|
-        #     system_arguments = ::Primer::Classify::Utilities.classes_to_hash(node.value.value)
-        #     corrector.replace(node.loc.expression, arguments_as_string(system_arguments))
-        #   end
-        # end
+        def autocorrect(node)
+          lambda do |corrector|
+            icon = node.arguments.first.source
+            kwargs = node.arguments.last
+
+            # Converting arguments for the component
+            classes = classes(kwargs)
+            size = transform_size(kwargs)
+            args = arguments_as_string(icon, size, classes)
+
+            corrector.replace(node.loc.expression, "primer_octicon(#{args})")
+          end
+        end
 
         private
 
-        def arguments_as_string(system_arguments)
-          system_arguments.map do |key, value|
-            val = case value
-                  when Symbol
-                    ":#{value}"
-                  when String
-                    value.to_json
-                  else
-                    value
-                  end
+        def transform_size(kwargs)
+          size = octicon_size(kwargs)
 
-            "#{key}: #{val}"
-          end.join(", ")
+          return "" if size.between?(10, 16)
+          return ":medium" if size.between?(22, 26)
+
+          size
+        end
+
+        def octicon_size(kwargs)
+          size = nil
+
+          kwargs.pairs.each do |pair|
+            if SIZE_ATTRIBUTES.include?(pair.key.value) && (pair.value.type == :str || pair.value.type == :int)
+              size = pair.value.source.to_i
+              break
+            end
+          end
+
+          size
+        end
+
+        def classes(kwargs)
+          # find class pair
+          class_arg = kwargs.pairs.find { |kwarg| kwarg.key.value == :class }
+
+          return if class_arg.blank?
+          return INVALID_CLASSES unless class_arg.value.type == :str
+
+          class_arg.value.value
+        end
+
+        def arguments_as_string(icon, size, classes)
+          args = icon
+
+          args += ", size: #{size}" if size.present?
+
+          return args if classes.blank?
+
+          system_args = ::Primer::Classify::Utilities.classes_to_args(classes)
+
+          "#{args}, #{system_args}"
         end
       end
     end
