7651f2658b1bfb979ec6c4e17490930c3af9d92c
Manuel Puyol <manuelpuyol@github.com>
2021-08-11 15:22:46 -0500

    ERB interpolation fixes (#737)
    A string like `string-<%= interpolation %>-<% if condition %>Yes<% else %>No<% end %>` was being converted to `string-#{ interpolation }-<% if condition %>Yes<% else %>No<% end %>`.
    So, for now, I'm only converting an ERB block if it only contains assignments `<%=`
    
    Another issue was the use of quotes inside the interpolation:
    `string-<%= condition ? "Yes" : "No" %>` converted to `string-%{ condition ? \"Yes\" : \"No\" }`. This is being fixed by dropping the `to_json` at the conversion and wrapping the result in quotes

--- test/linters/argument_mappers/helpers/erb_block_test.rb
+++ test/linters/argument_mappers/helpers/erb_block_test.rb
@@ -3,6 +3,15 @@
 require "linter_test_case"
 
 class ArgumentMappersButtonTest < LinterTestCase
+  def test_does_not_convert_interpolation_with_if
+    @file = '<div attribute="<% if condition %>Yes<% else %>No<% end %>">'
+    err = assert_raises ERBLint::Linters::ArgumentMappers::ConversionError do
+      ERBLint::Linters::ArgumentMappers::Helpers::ErbBlock.new.convert(tags.first.attributes["attribute"])
+    end
+
+    assert_equal("Cannot convert attribute \"attribute\" because its value contains an erb block", err.message)
+  end
+
   def test_converts_basic_interpolation
     @file = '<div attribute="<%= some_call %>">'
     args = ERBLint::Linters::ArgumentMappers::Helpers::ErbBlock.new.convert(tags.first.attributes["attribute"])
@@ -22,6 +31,13 @@ class ArgumentMappersButtonTest < LinterTestCase
     args = ERBLint::Linters::ArgumentMappers::Helpers::ErbBlock.new.convert(tags.first.attributes["attribute"])
 
     assert_equal("\"string-\#{ some_call }\#{ other_call }-more-\#{ another_call }\"", args)
+  end
+
+  def test_converts_ternary_interpolations
+    @file = '<div attribute="string-<%= condition ? "Yes" : "No" %>">'
+    args = ERBLint::Linters::ArgumentMappers::Helpers::ErbBlock.new.convert(tags.first.attributes["attribute"])
+
+    assert_equal("\"string-\#{ condition ? \"Yes\" : \"No\" }\"", args)
   end
 
   def test_converts_to_json_if_no_interpolation
