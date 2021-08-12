541ab8b3731ed0ab05195875217ff1a0451af92d
Cameron Dutro <camertron@gmail.com>
2021-08-12 15:34:34 -0700

    Remove range logic from benchmark tests

--- test/test_helpers/assert_allocations_helper.rb
+++ test/test_helpers/assert_allocations_helper.rb
@@ -2,14 +2,7 @@
 
 module Primer
   module AssertAllocationsHelper
-    def assert_allocations(range_or_count)
-      range = case range_or_count
-              when Integer
-                range_or_count..range_or_count
-              else
-                range_or_count
-              end
-
+    def assert_allocations(count)
       GC.disable
       total_start = GC.stat[:total_allocated_objects]
       yield
@@ -18,13 +11,7 @@ module Primer
 
       total = total_end - total_start
 
-      msg = if range.size > 1
-              "Expected between #{range.first} and #{range.last} allocations, got #{total} allocations"
-            else
-              "Expected #{range.first} allocations, got #{total} allocations"
-            end
-
-      assert_includes range, total, msg
+      assert_equal count, total, "Expected #{count} allocations, got #{total} allocations"
     end
   end
 end
