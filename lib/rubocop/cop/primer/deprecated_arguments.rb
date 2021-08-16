40dc7d82b757f8c9b47dbdce925d89d164f93096
Jon Rohan <yes@jonrohan.codes>
2021-08-16 14:49:55 -0700

    Creating rubocop for deprecated arguments

---
+++ lib/rubocop/cop/primer/deprecated_arguments.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+require "rubocop"
+require "primer/classify/utilities"
+require "primer/view_components/statuses"
+require_relative "../../../../app/lib/primer/view_helper"
+
+# :nocov:
+module RuboCop
+  module Cop
+    module Primer
+      # This cop ensures that components don't use deprecated arguments
+      #
+      # bad
+      # Component.new(foo: :deprecated)
+      #
+      # good
+      # Component.new(foo: :bar)
+      class DeprecatedArguments < RuboCop::Cop::Cop
+        INVALID_MESSAGE = <<~STR
+          Avoid using deprecated arguments: https://primer.style/view-components/deprecated.
+        STR
+
+        def on_send(node)
+          return unless valid_node?(node)
+          return unless node.arguments?
+
+          # we are looking for hash arguments and they are always last
+          kwargs = node.arguments.last
+
+          return unless kwargs.type == :hash
+
+          @deprecated_arguments = cop_config["Deprecated"] || {}
+
+          kwargs.pairs.each do |pair|
+            key = pair.key.value
+            value = pair.value.value
+            next unless @deprecated_arguments.key?(key) && @deprecated_arguments[key].key?(value)
+
+            add_offense(pair, message: INVALID_MESSAGE)
+          end
+        end
+
+        def autocorrect(node)
+          lambda do |corrector|
+            replacement = @deprecated_arguments[node.key.value][node.value.value]
+            corrector.replace(node, "#{node.key.value}: #{replacement}") if replacement.present?
+          end
+        end
+
+        private
+
+        # We only verify SystemArguments if it's a `.new` call on a component or
+        # a ViewHleper call.
+        def valid_node?(node)
+          view_helpers.include?(node.method_name) || (node.method_name == :new && ::Primer::ViewComponents::STATUSES.key?(node.receiver.const_name))
+        end
+
+        def view_helpers
+          ::Primer::ViewHelper::HELPERS.keys.map { |key| "primer_#{key}".to_sym }
+        end
+      end
+    end
+  end
+end
---
+++ test/rubocop/deprecated_arguments_test.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require "cop_test"
+
+class RubocopDeprecatedArgumentsTest < CopTest
+  def cop_class
+    RuboCop::Cop::Primer::DeprecatedArguments
+  end
+
+  def setup
+    config = RuboCop::Config.new({ "Primer/DeprecatedArguments" => { "Enabled" => true, "Deprecated" => { foo: { deprecated: ":new_argument" }, bar: { deprecated: nil } } } })
+    @cop = cop_class.new(config)
+  end
+
+  def test_deprecated_argument
+    investigate(cop, <<-RUBY)
+      Primer::BaseComponent.new(foo: :deprecated)
+    RUBY
+
+    assert_equal 1, cop.offenses.count
+  end
+
+  def test_multiple_deprecated_argument
+    investigate(cop, <<-RUBY)
+      Primer::BaseComponent.new(foo: :deprecated, bar: :deprecated, baz: :bin)
+    RUBY
+
+    assert_equal 2, cop.offenses.count
+  end
+
+  def test_deprecated_argument_autocorrected
+    investigate(cop, <<-RUBY)
+      Primer::BaseComponent.new(foo: :deprecated)
+    RUBY
+
+    assert_equal 1, cop.offenses.count
+    assert_equal "Primer::BaseComponent.new(foo: :new_argument)", cop.offenses.first.corrector.rewrite.strip
+  end
+
+  def test_deprecated_argument_with_nil_not_autocorrected
+    investigate(cop, <<-RUBY)
+      Primer::BaseComponent.new(bar: :deprecated)
+    RUBY
+
+    assert_equal 1, cop.offenses.count
+    refute cop.offenses.first.correctable?
+  end
+end
