642328c76db71a86888b577038082489d29281e9
Jon Rohan <yes@jonrohan.codes>
2021-08-17 14:15:11 -0700

    Don't need the utilities in this

--- lib/rubocop/cop/primer/deprecated_arguments.rb
+++ lib/rubocop/cop/primer/deprecated_arguments.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require "rubocop"
-require "primer/classify/utilities"
 require "primer/view_components/statuses"
 require_relative "../../../../app/lib/primer/view_helper"
 
