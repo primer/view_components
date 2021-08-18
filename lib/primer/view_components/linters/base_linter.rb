de1d7bcfdcfc54aca764b5cde4ecfd02ba98d757
Manuel Puyol <manuelpuyol@github.com>
2021-08-18 18:38:23 +0000

    test override config

--- lib/primer/view_components/linters/base_linter.rb
+++ lib/primer/view_components/linters/base_linter.rb
@@ -179,15 +179,15 @@ module ERBLint
           end
         end
 
-        if @offenses_not_corrected.zero?
-          # have to adjust to get `\n` so we delete the whole line
-          add_offense(processed_source.to_source_range(comment_node.loc.adjust(end_pos: 1)), "Unused erblint:count comment for #{rule_name}", "") if comment_node
-          return
-        end
-
         # Unless explicitly set, we don't want to mark correctable offenses if the counter is correct.
         if !@config.override_ignores_if_correctable? && expected_count == @total_offenses
           clear_offenses
+          return
+        end
+
+        if @offenses_not_corrected.zero?
+          # have to adjust to get `\n` so we delete the whole line
+          add_offense(processed_source.to_source_range(comment_node.loc.adjust(end_pos: 1)), "Unused erblint:count comment for #{rule_name}", "") if comment_node
           return
         end
 
--- test/linters/button_component_migration_counter_test.rb
+++ test/linters/button_component_migration_counter_test.rb
@@ -150,24 +150,22 @@ class ButtonComponentMigrationCounterTest < LinterTestCase
     assert_equal expected, result
   end
 
-  def test_autocorrects_removing_unnecessary_ignores
+  def test_does_not_autocorrects_when_ignores_are_correct
     @file = <<~HTML
-      <%# erblint:counter ButtonComponentMigrationCounter 1 %>
+      <%# erblint:counter ButtonComponentMigrationCounter 2 %>
       <button class="btn btn-primary">
+        button 1
+      </button>
+      <button invalid-attr class="btn">
         button 1
       </button>
     HTML
 
-    expected = <<~HTML
-      <%= render Primer::ButtonComponent.new(scheme: :primary) do %>
-        button 1
-      <% end %>
-    HTML
-
-    assert_equal expected, corrected_content
+    assert_equal @file, corrected_content
   end
 
-  def test_autocorrects_ignore_counts
+  def test_autocorrects_ignore_counts_if_override_enabled
+    @linter = linter_with_override
     @file = <<~HTML
       <%# erblint:counter ButtonComponentMigrationCounter 2 %>
       <button class="btn btn-primary">
