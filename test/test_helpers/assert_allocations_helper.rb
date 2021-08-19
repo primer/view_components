5f9475d8b9165ef133243ddf6b94c16c9b7cffd2
Cameron Dutro <camertron@gmail.com>
2021-08-19 15:20:41 -0700

    Stabilize allocation counts (#764)

--- test/test_helpers/assert_allocations_helper.rb
+++ test/test_helpers/assert_allocations_helper.rb
@@ -2,42 +2,25 @@
 
 module Primer
   module AssertAllocationsHelper
-    def assert_allocations(range_or_count)
-      range = case range_or_count
-              when Integer
-                range_or_count..range_or_count
-              else
-                range_or_count
-              end
-
+    def assert_allocations(count_map)
       GC.disable
+      GC.start
+      GC.compact if GC.respond_to?(:compact)
       total_start = GC.stat[:total_allocated_objects]
       yield
       total_end = GC.stat[:total_allocated_objects]
       GC.enable
 
       total = total_end - total_start
+      count = count_map[ruby_version]
 
-      msg = if range.size > 1
-              "Expected between #{range.first} and #{range.last} allocations, got #{total} allocations"
-            else
-              "Expected #{range.first} allocations, got #{total} allocations"
-            end
-
-      assert_includes range, total, msg
+      assert_equal count, total, "Expected #{count} allocations, got #{total} allocations"
     end
 
-    def expectations_for(type)
-      self.class::EXPECTATIONS[ruby_version][type]
-    end
+    private
 
     def ruby_version
-      return @ruby_version if defined? @ruby_version
-
-      @ruby_version = ENV["RUBY_VERSION"].dup
-      @ruby_version.gsub!("ruby-", "")
-      @ruby_version[-1] = "x"
-      @ruby_version
+      @ruby_version ||= RUBY_VERSION.split(".").take(2).join(".")
     end
   end
 end
