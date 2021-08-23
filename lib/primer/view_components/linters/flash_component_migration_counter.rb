ce525e7e92bebb8cb05023777aa96afb6676ca21
Manuel Puyol <manuelpuyol@github.com>
2021-08-23 13:27:43 -0500

    Add basic autocorrection for `Flash` linter (#761)
    Adds autocorrection for Flashes that have simple text content:
    
    ```html
    <!-- corrects -->
    <div class="flash">some text</div>
    <!-- doesn't correct -->
    <div class="flash">
      <span>some text</span>
    </div>
    ```

--- lib/primer/view_components/linters/flash_component_migration_counter.rb
+++ lib/primer/view_components/linters/flash_component_migration_counter.rb
@@ -1,14 +1,29 @@
 # frozen_string_literal: true
 
 require_relative "base_linter"
+require_relative "autocorrectable"
+require_relative "argument_mappers/flash"
 
 module ERBLint
   module Linters
     # Counts the number of times a HTML flash is used instead of the component.
     class FlashComponentMigrationCounter < BaseLinter
+      include Autocorrectable
+
       TAGS = %w[div].freeze
       CLASSES = %w[flash].freeze
       MESSAGE = "We are migrating flashes to use [Primer::FlashComponent](https://primer.style/view-components/components/flash), please try to use that instead of raw HTML."
+      ARGUMENT_MAPPER = ArgumentMappers::Flash
+      COMPONENT = "Primer::FlashComponent"
+
+      def map_arguments(tag, tag_tree)
+        # We can only autocorrect elements with simple text as content.
+        return nil if tag_tree[:children].any?
+
+        ARGUMENT_MAPPER.new(tag).to_s
+      rescue ArgumentMappers::ConversionError
+        nil
+      end
     end
   end
 end
