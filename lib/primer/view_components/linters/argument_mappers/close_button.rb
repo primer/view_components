4936dab374de0da8f2d00d9cdeba17067d0a953f
Manuel Puyol <manuelpuyol@github.com>
2021-08-23 19:41:23 +0000

    add closebutton argument mapper

---
+++ lib/primer/view_components/linters/argument_mappers/close_button.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require_relative "base"
+
+module ERBLint
+  module Linters
+    module ArgumentMappers
+      # Maps classes in a flash element to arguments for the Flash component.
+      class CloseButton < Base
+        ATTRIBUTES = %w[type].freeze
+
+        TYPE_OPTIONS = Primer::ViewComponents::Constants.get(
+          component: "Primer::CloseButton",
+          constant: "TYPE_OPTIONS"
+        ).freeze
+
+        DEFAULT_TYPE = Primer::ViewComponents::Constants.get(
+          component: "Primer::CloseButton",
+          constant: "DEFAULT_TYPE"
+        ).freeze
+
+        def attribute_to_args(attribute)
+          # button is the default type, so we don't need to do anything.
+          return {} if attribute.value == DEFAULT_TYPE
+
+          raise ConversionError, "CloseButton component does not support type \"#{attribute.value}\"" unless TYPE_OPTIONS.include?(attribute.value)
+
+          { type: ":#{attribute.value}" }
+        end
+
+        def classes_to_args(classes)
+          classes.each_with_object({ classes: [] }) do |class_name, acc|
+            next if class_name == "close-button"
+
+            acc[:classes] << class_name
+          end
+        end
+      end
+    end
+  end
+end
