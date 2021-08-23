a2daf1070557b66f67c1dcf0c437c050de0ad47d
Manuel Puyol <manuelpuyol@github.com>
2021-08-23 20:41:04 +0000

    udpate flash and baselinter to use new tag tree

--- test/linters/base_linter_test.rb
+++ test/linters/base_linter_test.rb
@@ -12,10 +12,14 @@ class BaseLinterTest < LinterTestCase
   end
 
   def build_tag_tree(tags)
-    @linter.send(:build_tag_tree, tags)
+    @linter.send(:build_tag_tree, processed_source, tags)
   end
 
-  def test_text_is_not_a_tag_children
+  def tag_children(tree)
+    tree[:children].select { |x| x.is_a?(Hash) }
+  end
+
+  def test_text_is_a_children
     @file = <<~HTML
       <div>
         some text
@@ -27,7 +31,7 @@ class BaseLinterTest < LinterTestCase
 
     assert_equal [processed_tags.first], tree.keys
     assert_equal processed_tags.last, tree[processed_tags.first][:closing]
-    assert_empty tree[processed_tags.first][:children]
+    assert_equal tree[processed_tags.first][:children].size, 1
   end
 
   def test_tree_with_children
@@ -44,7 +48,7 @@ class BaseLinterTest < LinterTestCase
     assert_equal [processed_tags.first, processed_tags.second], tree.keys
     assert_equal processed_tags.last, tree[processed_tags.first][:closing]
     assert_equal processed_tags.third, tree[processed_tags.second][:closing]
-    assert_equal [tree[processed_tags.second]], tree[processed_tags.first][:children]
+    assert_equal [tree[processed_tags.second]], tag_children(tree[processed_tags.first])
   end
 
   def test_tree_with_children_between_text
@@ -60,7 +64,7 @@ class BaseLinterTest < LinterTestCase
     assert_equal [processed_tags.first, processed_tags.second], tree.keys
     assert_equal processed_tags.last, tree[processed_tags.first][:closing]
     assert_equal processed_tags.third, tree[processed_tags.second][:closing]
-    assert_equal [tree[processed_tags.second]], tree[processed_tags.first][:children]
+    assert_equal [tree[processed_tags.second]], tag_children(tree[processed_tags.first])
   end
 
   def test_tree_with_multiple_children
@@ -75,28 +79,6 @@ class BaseLinterTest < LinterTestCase
     processed_tags = tags
     tree = build_tag_tree(processed_tags)
 
-    assert_equal 3, tree[processed_tags.first][:children].size
-  end
-
-  def test_tree_with_nested_children
-    @file = <<~HTML
-      <div>
-        <div>
-          <div>text</div>
-          <div>text</div>
-        </div>
-        <div>
-          <div>text</div>
-          <div>text</div>
-          <div>text</div>
-        </div>
-      </div>
-    HTML
-
-    processed_tags = tags
-    tree = build_tag_tree(processed_tags)
-
-    assert_equal 2, tree[processed_tags.first][:children].first[:children].size
-    assert_equal 3, tree[processed_tags.first][:children].last[:children].size
+    assert_equal 3, tag_children(tree[processed_tags.first]).size
   end
 end
