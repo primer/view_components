9fc40e43d26bf0c2f9b272dfc4cf73520cf2e6f9
Joel Hawksley <joel@hawksley.org>
2021-08-25 15:52:27 -0600

    rename

--- stories/primer/beta/breadcrumbs/item_stories.rb
+++ stories/primer/beta/breadcrumbs/item_stories.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Primer::Beta::Breadcrumb::ItemStories < ViewComponent::Storybook::Stories
+class Primer::Beta::Breadcrumbs::ItemStories < ViewComponent::Storybook::Stories
   layout "storybook_centered_preview"
 
   story(:item) do
