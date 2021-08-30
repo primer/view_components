c188c44199db325582a776026482e6eec2bb058e
Joel Hawksley <joel@hawksley.org>
2021-08-30 14:31:27 -0600

    fix stories

--- stories/primer/beta/breadcrumbs_stories.rb
+++ stories/primer/beta/breadcrumbs_stories.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
+require "primer/beta/breadcrumbs"
+
 class Primer::Beta::BreadcrumbsStories < ViewComponent::Storybook::Stories
   layout "storybook_preview"
 
-  story(:breadcrumb) do
+  story(:breadcrumbs) do
     content do |component|
-      component.item(selected: false) { "Breadcrumb Item one" }
-      component.item(href: "https://github.com/") { "Breadcrumb Item two" }
+      component.item(href: "https://github.com/") { "Breadcrumb Item one" }
+      component.item(selected: false) { "Breadcrumb Item two" }
     end
   end
 end
