1dd9f9b7b1e4f0c8936e25b97ef99b6a81054ae2
Cameron Dutro <camertron@gmail.com>
2021-09-03 14:41:57 -0700

    Adjust allocation counts in octicon benchmarks

--- test/benchmarks/bench_octicons.rb
+++ test/benchmarks/bench_octicons.rb
@@ -15,7 +15,7 @@ class BenchOcticons < Minitest::Benchmark
   def bench_allocations_without_cache
     Primer::OcticonComponent.new(**@options)
     Primer::Octicon::Cache.clear!
-    assert_allocations "3.0" => 38, "2.7" => 40, "2.6" => 45, "2.5" => 47 do
+    assert_allocations "3.0" => 31, "2.7" => 34, "2.6" => 38, "2.5" => 40 do
       Primer::OcticonComponent.new(**@options)
     end
   ensure
@@ -24,7 +24,7 @@ class BenchOcticons < Minitest::Benchmark
 
   def bench_allocations_with_cache
     Primer::Octicon::Cache.preload!
-    assert_allocations "3.0" => 17, "2.7" => 19, "2.6" => 23, "2.5" => 25 do
+    assert_allocations "3.0" => 10, "2.7" => 14, "2.6" => 17, "2.5" => 19 do
       Primer::OcticonComponent.new(**@options)
     end
   end
