4424eff6fee4aece3f1c0bc8d754842fbced2f4c
Manuel Puyol <manuelpuyol@github.com>
2021-10-05 21:07:46 +0000

    Add stats class

---
+++ lib/primer/view_components/stats.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Primer
+  # :nodoc:
+  module ViewComponents
+    class Stats
+      class << self
+        def accessibility_tags_count
+          all_components.count do |f|
+            File.read(f).include?("@accessibility")
+          end
+        end
+
+        def components_count
+          all_components.size
+        end
+
+        private
+
+        def all_components
+          @all_components ||= Dir[File.join(__dir__, "../../../app/components/**/*.rb")]
+        end
+      end
+    end
+  end
+end
---
+++ test/primer/stats_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require "test_helper"
+require "primer/view_components/stats"
+
+class Primer::ViewComponents::StatsTest < Minitest::Test
+  def test_accessibility_tags_count
+    assert_equal 15, Primer::ViewComponents::Stats.accessibility_tags_count
+  end
+end
