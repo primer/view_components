4861460a0167378e7030884d2373f9b4e02ddf9c
Manuel Puyol <manuelpuyol@github.com>
2021-10-15 18:10:17 +0000

    working state

--- demo/.storybook/color-modes/components/color-mode-selector.js
+++ demo/.storybook/color-modes/components/color-mode-selector.js
@@ -1,29 +1,53 @@
-import React, { memo, useCallback } from 'react';
+import React, { memo, useState } from 'react';
 import { useGlobals } from '@storybook/api';
-import { Icons, IconButton } from '@storybook/components';
-import { PARAM_KEY } from '../constants';
+import { Icons, IconButton, WithTooltip, TooltipLinkList, TooltipMessage, Trigger } from '@storybook/components';
+import { THEMES } from '../constants';
+
+const getColorList = (active, set) => [
+  THEMES.map((theme) => ({
+    id: theme,
+    title: theme,
+    onClick: () => {
+      set(theme);
+    },
+    right: <div>{theme}</div>,
+    active: active === theme,
+  })),
+];
 
 export const ColorModeSelector = memo(() => {
-  const [globals, updateGlobals] = useGlobals();
+  const [colorMode, setColorMode] = useState('light');
+  const [expanded, setExpanded] = useState(false);
+  const iframe = document.getElementById('storybook-preview-iframe')
 
-  const isActive = globals[PARAM_KEY] || false;
+  const updateTheme = (theme) => {
+    const root = iframe.contentWindow.document.children[0]
+    root.setAttribute('data-color-mode', 'light')
+    root.setAttribute('data-dark-theme', theme)
+    root.setAttribute('data-light-theme', theme)
+  }
 
-  const toggleOutline = useCallback(
-    () =>
-      updateGlobals({
-        [PARAM_KEY]: !isActive,
-      }),
-    [isActive]
-  );
+  const themeList = THEMES.map(theme => ({
+    id: theme,
+    title: theme,
+    onClick: () => {
+      setColorMode(theme)
+      updateTheme(theme)
+    },
+  }));
 
   return (
-    <IconButton
-      key="outline"
-      active={isActive}
-      title="Apply outlines to the preview"
-      onClick={toggleOutline}
+    <WithTooltip
+      placement="bottom-start"
+      trigger="click"
+      tooltipShown={expanded}
+      onVisibilityChange={s => setExpanded(s)}
+      tooltip={<TooltipLinkList links={themeList} />}
+      closeOnClick
     >
-      <Icons icon="paintbrush" />
-    </IconButton>
+      <IconButton key="colorMode" active={!!colorMode} title="Color modes">
+        <Icons icon="paintbrush" /> &nbsp;{colorMode}
+      </IconButton>
+    </WithTooltip>
   );
 });
--- demo/.storybook/color-modes/constants.js
+++ demo/.storybook/color-modes/constants.js
@@ -1,2 +1,11 @@
 export const ADDON_ID = 'primer/colorModes';
 export const PARAM_KEY = 'colorModes';
+export const THEMES = [
+  'light',
+  'light_protanopia',
+  'dark',
+  'dark_dimmed',
+  'dark_high_contrast',
+  'dark_protanopia',
+  // 'all'
+]
