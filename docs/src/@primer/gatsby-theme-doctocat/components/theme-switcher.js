997bb9e2bb3675062bd0dbaa7bd4986070d32577
Manuel Puyol <manuelpuyol@github.com>
2021-10-15 18:45:28 +0000

    add color mode selector to docs

--- docs/src/@primer/gatsby-theme-doctocat/components/theme-switcher.js
+++ docs/src/@primer/gatsby-theme-doctocat/components/theme-switcher.js
@@ -1,21 +1,39 @@
 import React from 'react'
-import {Flex} from '@primer/components'
-import {MoonIcon, SunIcon} from '@primer/octicons-react'
+import {Box, DropdownMenu, DropdownButton} from '@primer/components'
+
+const THEMES = [
+  'light',
+  'light_colorblind',
+  'dark',
+  'dark_dimmed',
+  'dark_high_contrast',
+  'dark_colorblind',
+]
 
 function ThemeSwitcher({children}) {
-  const [colorMode, setColorMode] = React.useState('light')
+  const items = React.useMemo(
+    () => THEMES.map((theme) => ({
+      text: theme,
+      key: theme
+    })),
+    []
+  )
+  const [colorMode, setColorMode] = React.useState(items[0])
 
   return (
-    <div className="Box" data-color-mode={colorMode} data-dark-theme="dark" data-light-theme="light">
-      <Flex pt={2} px={2} justifyContent="flex-end">
-        <button
-          className="btn"
-          aria-label={colorMode === 'light' ? 'Activate dark mode' : 'Activate light mode'}
-          onClick={() => setColorMode(colorMode === 'light' ? 'dark' : 'light')}
-        >
-          {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
-        </button>
-      </Flex>
+    <div className="Box" data-color-mode="light" data-dark-theme={colorMode.text} data-light-theme={colorMode.text}>
+      <Box pt={2} px={2} justifyContent="flex-end" display="flex">
+      <DropdownMenu
+        renderAnchor={({children, ...anchorProps}) => (
+          <DropdownButton variant="small" {...anchorProps}>
+            {children}
+          </DropdownButton>
+        )}
+        items={items}
+        selectedItem={colorMode}
+        onChange={setColorMode}
+      />
+      </Box>
       <div className="frame-example p-3">{children}</div>
     </div>
   )
