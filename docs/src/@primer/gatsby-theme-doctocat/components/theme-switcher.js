1417dd121a91943b4b6926390cfe681e576bff36
Manuel Puyol <manuelpuyol@github.com>
2021-10-19 17:44:09 +0000

    Use primitives to get the theme list

--- docs/src/@primer/gatsby-theme-doctocat/components/theme-switcher.js
+++ docs/src/@primer/gatsby-theme-doctocat/components/theme-switcher.js
@@ -1,14 +1,8 @@
 import React from 'react'
-import {Box, DropdownMenu, DropdownButton} from '@primer/components'
+import {Box, DropdownMenu, DropdownButton, ThemeProvider} from '@primer/components'
+import primitives from '@primer/primitives'
 
-const THEMES = [
-  'light',
-  'light_colorblind',
-  'dark',
-  'dark_dimmed',
-  'dark_high_contrast',
-  'dark_colorblind',
-]
+const THEMES = Object.keys(primitives.colors)
 
 function ThemeSwitcher({children}) {
   const items = React.useMemo(
@@ -23,16 +17,18 @@ function ThemeSwitcher({children}) {
   return (
     <div className="Box" data-color-mode="light" data-dark-theme={colorMode.text} data-light-theme={colorMode.text}>
       <Box pt={2} px={2} justifyContent="flex-end" display="flex">
-      <DropdownMenu
-        renderAnchor={({children, ...anchorProps}) => (
-          <DropdownButton variant="small" {...anchorProps}>
-            {children}
-          </DropdownButton>
-        )}
-        items={items}
-        selectedItem={colorMode}
-        onChange={setColorMode}
-      />
+      <ThemeProvider dayScheme={colorMode.text}>
+        <DropdownMenu
+          renderAnchor={({children, ...anchorProps}) => (
+            <DropdownButton variant="small" {...anchorProps}>
+              {children}
+            </DropdownButton>
+          )}
+          items={items}
+          selectedItem={colorMode}
+          onChange={setColorMode}
+        />
+      </ThemeProvider>
       </Box>
       <div className="frame-example p-3">{children}</div>
     </div>
