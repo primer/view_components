d3d24da0992472dc5b91e23c1e616da995945789
Katie Foster <inkblotty@github.com>
2021-10-19 19:52:17 +0000

    basic test

--- test/components/primer/alpha/border_box_header_test.rb
+++ test/components/primer/alpha/border_box_header_test.rb
@@ -5,9 +5,17 @@ require "test_helper"
 class PrimerAlphaBorderBoxHeaderTest < Minitest::Test
   include Primer::ComponentTestHelpers
 
-  def test_renders
-    render_inline(Primer::BorderBoxHeader.new)
+  def test_renders_content
+    render_inline(Primer::BorderBoxHeader.new) { "Header" }
 
-    assert_text("Add a test here")
+    assert_text("Header")
+  end
+
+  def test_renders_title
+    render_inline(Primer::BorderBoxHeader.new) do |h|
+      h.title { "Title" }
+    end
+
+    assert_text("Title")
   end
 end
