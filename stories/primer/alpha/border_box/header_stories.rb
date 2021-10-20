224f495c650ed1c7963cd4fac863eaf33b0208b1
Katie Foster <inkblotty@github.com>
2021-10-20 17:58:58 +0000

    fix story

--- stories/primer/alpha/border_box/header_stories.rb
+++ stories/primer/alpha/border_box/header_stories.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "primer/alpha/border_box_header"
+require "primer/alpha/border_box/header"
 
 class Primer::Alpha::BorderBox::HeaderStories < ViewComponent::Storybook::Stories
   layout "storybook_preview"
