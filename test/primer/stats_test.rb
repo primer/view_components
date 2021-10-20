640820e37c2df68b0f6fa4beb6420456d4e01712
Katie Foster <inkblotty@github.com>
2021-10-20 19:57:18 +0000

    bump stats_test number

--- test/primer/stats_test.rb
+++ test/primer/stats_test.rb
@@ -5,6 +5,7 @@ require "primer/view_components/stats"
 
 class Primer::ViewComponents::StatsTest < Minitest::Test
   def test_accessibility_tags_count
-    assert_equal 15, Primer::ViewComponents::Stats.accessibility_tags_count
+    # reference: https://github.com/primer/view_components/pull/833
+    assert_equal 17, Primer::ViewComponents::Stats.accessibility_tags_count
   end
 end
