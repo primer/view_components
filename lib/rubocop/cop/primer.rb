fcff00db0082095706a37d5c219ebc455c3b5052
Cameron Dutro <camertron@gmail.com>
2021-10-25 15:31:47 -0700

    Fix linting

--- lib/rubocop/cop/primer.rb
+++ lib/rubocop/cop/primer.rb
@@ -1,3 +1,13 @@
 # frozen_string_literal: true
 
+require "primer/classify/utilities"
+
+Primer::Classify::Utilities.singleton_class.prepend(Module.new do
+  # The original method retrieves a config option from Rails.application, which isn't
+  # defined when RuboCop is running.
+  def validate_class_names?
+    true
+  end
+end)
+
 Dir[File.join(__dir__, "primer", "*.rb")].sort.each { |file| require file }
