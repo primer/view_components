a7de136af571e589546c4b023e937cd144e5950e
Cameron Dutro <camertron@gmail.com>
2021-10-29 13:57:44 -0700

    Add test for cache hit/miss notifications

--- test/lib/classify/cache_test.rb
+++ test/lib/classify/cache_test.rb
@@ -38,4 +38,27 @@ class PrimerClassifyCacheTest < Minitest::Test
     cache.max_size = Rails.application.config.primer_view_components.max_classify_cache_size
     # rubocop:enable Style/RedundantFetchBlock
   end
+
+  def test_notifies_on_cache_hits_and_misses
+    hits = 0
+    misses = 0
+
+    hit_sub = ActiveSupport::Notifications.subscribe("primer_view_components.classify_cache.hit") do
+      hits += 1
+    end
+
+    miss_sub = ActiveSupport::Notifications.subscribe("primer_view_components.classify_cache.miss") do
+      misses += 1
+    end
+
+    Primer::Classify::Cache.instance.clear!
+    Primer::Classify.call(classes: "m-1")
+    Primer::Classify.call(classes: "m-1")
+
+    assert_equal hits, 1
+    assert_equal misses, 1
+  ensure
+    ActiveSupport::Notifications.unsubscribe(hit_sub) if hit_sub
+    ActiveSupport::Notifications.unsubscribe(miss_sub) if miss_sub
+  end
 end
