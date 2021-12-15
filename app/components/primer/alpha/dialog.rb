a06e539e958f5b715cc401f21bc12e775417e7d1
Owen Niblock <owenniblock@github.com>
2021-12-15 17:40:21 +0000

    Adds further mark-up for dialog component

--- app/components/primer/alpha/dialog.rb
+++ app/components/primer/alpha/dialog.rb
@@ -4,6 +4,13 @@ module Primer
   module Alpha
     # Use `Dialog` for an overlayed dialog window.
     class Dialog < Primer::Component
+      # Optional list of buttons to be rendered.
+      #
+      # @param kwargs [Hash] The same arguments as <%= link_to_component(Primer::ButtonComponent) %> except for `size` and `group_item`.
+      renders_many :buttons, lambda { |**system_arguments|
+        Primer::ButtonComponent.new(**system_arguments)
+      }
+
       # @example Default
       #   <%= render(Alpha::Primer::Dialog.new) { "Your content here" } %>
       #
@@ -11,8 +18,8 @@ module Primer
       #   <%= render(Primer::BoxComponent.new(bg: :subtle, p: 3)) { "Hello world" } %>
       #
       # @param system_arguments [Hash] <%= link_to_system_arguments_docs %>
-      def initialize(**system_arguments)
-        @system_arguments = deny_tag_argument(title:, description:, **system_arguments)
+      def initialize(title:, description:, **system_arguments)
+        @system_arguments = deny_tag_argument(**system_arguments)
 
         @system_arguments[:tag] = :div
         @system_arguments[:role] = :dialog
