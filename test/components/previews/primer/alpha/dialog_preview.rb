36bbdc53e497f8f3b636daf076d696fd12850950
Owen Niblock <owenniblock@github.com>
2022-01-05 10:43:23 +0000

    Adds the behaviours lib to dialog component

---
+++ test/components/previews/primer/alpha/dialog_preview.rb
@@ -0,0 +1,9 @@
+module Primer
+  module Alpha
+    class DialogPreview < ViewComponent::Preview
+      def default
+        render(Primer::Alpha::Dialog.new)
+      end
+    end
+  end
+end
---
+++ test/system/alpha/dialog_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "application_system_test_case"
+
+class IntegrationAlphaDialogTest < ApplicationSystemTestCase
+  def test_renders_component
+    with_preview(:default)
+
+    assert_selector("dialog")
+  end
+end
