804c137d3b0f21933e05505db790f630ac1565c9
Josh Klina <joshua.klina@gmail.com>
2022-01-13 14:44:15 +0000

    Restore classify validation

--- lib/primer/classify/validation.rb
+++ lib/primer/classify/validation.rb
@@ -10,8 +10,7 @@ module Primer
 
       class << self
         def invalid?(class_name)
-          class_name.start_with?(INVALID_CLASS_NAME_PREFIXES) ||
-            Primer::Classify::Utilities.supported_selector?(class_name)
+          class_name.start_with?(INVALID_CLASS_NAME_PREFIXES) || Primer::Classify::Utilities.supported_selector?(class_name)
         end
       end
     end
