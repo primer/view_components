ef3a3c77c768bb6ab5c8e0fcd5f6037d36433213
Owen Niblock <owenniblock@github.com>
2022-01-31 16:18:34 +0000

    Add code from WIP behaviors changes

--- app/components/primer/alpha/dialog.ts
+++ app/components/primer/alpha/dialog.ts
@@ -1,16 +1,1 @@
-// @ts-ignore
-import { ModalDialogElement } from '@primer/behaviors/modal-dialog'
-
-declare global {
-  interface Window {
-    ModalDialogElement: typeof ModalDialogElement
-  }
-  interface HTMLElementTagNameMap {
-    'modal-dialog': ModalDialogElement
-  }
-}
-
-if (!window.customElements.get('modal-dialog')) {
-  window.ModalDialogElement = ModalDialogElement
-  window.customElements.define('modal-dialog', ModalDialogElement)
-}
+import '@primer/behaviors/components/modal-dialog'
