363f9979b9085764f703074681d9aba25e49fb20
Cameron Dutro <camertron@gmail.com>
2022-02-11 10:20:48 -0800

    Fix docs build

--- demo/config/initializers/kuby.rb
+++ demo/config/initializers/kuby.rb
@@ -1,2 +1,9 @@
-require 'kuby'
-Kuby.load!
+begin
+  require 'kuby'
+  Kuby.load!
+rescue LoadError
+  # Happens during docs:build, which runs in the context of the
+  # view_component gem's bundle (i.e. not the demo app's bundle)
+  # and therefore doesn't have any of the kuby-* gems loaded. It
+  # should be no problem to skip loading Kuby in this case.
+end
