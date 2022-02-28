dda639802fc2f41556c54af3d9a98c89c2afd28b
Kate Higa <khiga8@github.com>
2022-02-28 14:57:49 -0800

    update stories

--- stories/primer/alpha/tooltip_stories.rb
+++ stories/primer/alpha/tooltip_stories.rb
@@ -6,5 +6,11 @@ class Primer::Alpha::TooltipStories < ViewComponent::Storybook::Stories
   layout "storybook_preview"
 
   story(:tooltip_default) do
+    controls do
+      select(:direction, Primer::Alpha::Tooltip::DIRECTION_OPTIONS, :s)
+      select(:type, Primer::Alpha::Tooltip::TYPE_OPTIONS, :description)
+      text(:for_id, "someButton")
+      text(:text, "This is tooltip text")
+    end
   end
 end
