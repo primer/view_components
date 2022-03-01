aefb7f013146f5ed8fed55a28f5d3a5f1bd3274a
Kate Higa <khiga8@github.com>
2022-02-28 17:06:07 -0800

    update integration tests

--- test/system/tooltip_test.rb
+++ test/system/tooltip_test.rb
@@ -4,14 +4,18 @@ require "application_system_test_case"
 
 class IntegrationTooltipTest < ApplicationSystemTestCase
   def test_renders_with_aria_described_by
-    with_preview(:default)
+    with_preview(:description)
 
-    assert_selector("tool-tip[data-view-component][aria-describedby]")
+    assert_selector("button[id='dislike-button']")
+    assert_selector("tool-tip[for='dislike-button'][data-view-component][role='tooltip']", visible: false, text: 'This means you dislike this comment')
+    assert_equal(find('button')['aria-describedby'], find('tool-tip', visible: false)['id'])
   end
 
   def test_renders_with_aria_labelled_by
     with_preview(:label)
-
-    assert_selector("tool-tip[data-view-component][aria-labelledby]")
+    
+    assert_selector("button[id='like-button']")
+    assert_selector("tool-tip[for='like-button'][data-view-component]", visible: false, text: 'Like')
+    assert_equal(find('button')['aria-labelledby'], find('tool-tip', visible: false)['id'])
   end
 end
