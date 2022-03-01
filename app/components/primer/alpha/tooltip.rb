b7817b237ce0da2d002ed440d1989a97bf906277
Kate Higa <khiga8@github.com>
2022-02-28 17:53:12 -0800

    update examples

--- app/components/primer/alpha/tooltip.rb
+++ app/components/primer/alpha/tooltip.rb
@@ -43,6 +43,26 @@ module Primer
       #   @code
       #     <%= render(Primer::ButtonComponent.new(id: "save-button", scheme: :primary)) { "Save" } %>
       #     <%= render(Primer::Alpha::Tooltip.new(for_id: "save-button", type: :description, text: "This will immediately impact all organization members", direction: :ne)) %>
+      # @example With direction
+      #   @description
+      #     Set direction of tooltip with `direction`. The tooltip is responsive and will automatically adjust direction to avoid cutting off.
+      #   @code
+      #     <%= render(Primer::ButtonComponent.new(id: "North", m: 2)) { "North" } %>
+      #     <%= render(Primer::Alpha::Tooltip.new(for_id: "North", type: :label, text: "This is North-facing and responsive.", direction: :n)) %>
+      #     <%= render(Primer::ButtonComponent.new(id: "South", m: 2)) { "South" } %>
+      #     <%= render(Primer::Alpha::Tooltip.new(for_id: "South", type: :label, text: "This is South-facing and responsive.", direction: :s)) %>
+      #     <%= render(Primer::ButtonComponent.new(id: "East", m: 2)) { "East" } %>
+      #     <%= render(Primer::Alpha::Tooltip.new(for_id: "East", type: :label, text: "This is East-facing and responsive.", direction: :e)) %>
+      #     <%= render(Primer::ButtonComponent.new(id: "West", m: 2)) { "West" } %>
+      #     <%= render(Primer::Alpha::Tooltip.new(for_id: "West", type: :label, text: "This is West-facing and responsive.", direction: :w)) %>
+      #     <%= render(Primer::ButtonComponent.new(id: "Northeast", m: 2)) { "Northeast" } %>
+      #     <%= render(Primer::Alpha::Tooltip.new(for_id: "Northeast", type: :label, text: "This is Northeast-facing and responsive.", direction: :ne)) %>
+      #     <%= render(Primer::ButtonComponent.new(id: "Southeast", m: 2)) { "Southeast" } %>
+      #     <%= render(Primer::Alpha::Tooltip.new(for_id: "Southeast", type: :label, text: "This is Southeast-facing and responsive.", direction: :se)) %>
+      #     <%= render(Primer::ButtonComponent.new(id: "Northwest", m: 2)) { "Northwest" } %>
+      #     <%= render(Primer::Alpha::Tooltip.new(for_id: "Northwest", type: :label, text: "This is Northwest-facing and responsive.", direction: :nw)) %>
+      #     <%= render(Primer::ButtonComponent.new(id: "Southwest", m: 2)) { "Southwest" } %>
+      #     <%= render(Primer::Alpha::Tooltip.new(for_id: "Southwest", type: :label, text: "This is Southwest-facing and responsive.", direction: :sw)) %>
       # @param for_id [String] The ID of the element that the tooltip should be attached to.
       # @param type [Symbol] <%= one_of(Primer::Alpha::Tooltip::TYPE_OPTIONS) %>.
       # @param direction [Symbol] <%= one_of(Primer::Alpha::Tooltip::DIRECTION_OPTIONS) %>
--- test/system/tooltip_test.rb
+++ test/system/tooltip_test.rb
@@ -7,15 +7,15 @@ class IntegrationTooltipTest < ApplicationSystemTestCase
     with_preview(:description)
 
     assert_selector("button[id='dislike-button']")
-    assert_selector("tool-tip[for='dislike-button'][data-view-component][role='tooltip']", visible: false, text: 'This means you dislike this comment')
-    assert_equal(find('button')['aria-describedby'], find('tool-tip', visible: false)['id'])
+    assert_selector("tool-tip[for='dislike-button'][data-view-component][role='tooltip']", visible: false, text: "This means you dislike this comment")
+    assert_equal(find("button")["aria-describedby"], find("tool-tip", visible: false)["id"])
   end
 
   def test_renders_with_aria_labelled_by
     with_preview(:label)
-    
+
     assert_selector("button[id='like-button']")
-    assert_selector("tool-tip[for='like-button'][data-view-component]", visible: false, text: 'Like')
-    assert_equal(find('button')['aria-labelledby'], find('tool-tip', visible: false)['id'])
+    assert_selector("tool-tip[for='like-button'][data-view-component]", visible: false, text: "Like")
+    assert_equal(find("button")["aria-labelledby"], find("tool-tip", visible: false)["id"])
   end
 end
