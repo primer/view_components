6f3d487ef475acfe7f2d83f74a761ceec9b83227
Kate Higa <khiga8@github.com>
2022-02-28 17:06:07 -0800

    update integration tests

--- demo/test/components/previews/primer/tooltip_preview.rb
+++ demo/test/components/previews/primer/tooltip_preview.rb
@@ -1,13 +1,9 @@
 module Primer
   class TooltipPreview < ViewComponent::Preview
-    def default
-      render(Primer::ButtonComponent.new(id: "dislike-button", "aria-label": "Dislike")) { "👍" }
-      render(Primer::Alpha::Tooltip.new(for_id: "dislike-button", type: :description, text: "This means you dislike this comment"))
+    def description
     end
 
     def label
-      render(Primer::ButtonComponent.new(id: "like-button")) { "👍" }
-      render(Primer::Alpha::Tooltip.new(for_id: "like-button", type: :label, text: "Like"))
     end
   end
 end
