5a3e1207c94bffe74735baa6d83c0ace83465aa8
Hector Garcia <hectahertz@github.com>
2022-03-18 15:30:57 +0100

    Update storybook

--- stories/primer/icon_button_stories.rb
+++ stories/primer/icon_button_stories.rb
@@ -11,22 +11,7 @@ class Primer::IconButtonStories < ViewComponent::Storybook::Stories
       select(:tag, Primer::BaseButton::TAG_OPTIONS, Primer::BaseButton::DEFAULT_TAG)
       select(:type, Primer::BaseButton::TYPE_OPTIONS, Primer::BaseButton::DEFAULT_TYPE)
       select(:scheme, Primer::IconButton::SCHEME_OPTIONS, Primer::IconButton::DEFAULT_SCHEME)
-    end
-  end
-
-  story(:with_tooltip) do
-    controls do
-      aria(label: "Button label")
-      icon "star"
-      box false
-      select(:tag, Primer::BaseButton::TAG_OPTIONS, Primer::BaseButton::DEFAULT_TAG)
-      select(:type, Primer::BaseButton::TYPE_OPTIONS, Primer::BaseButton::DEFAULT_TYPE)
-      select(:scheme, Primer::IconButton::SCHEME_OPTIONS, Primer::IconButton::DEFAULT_SCHEME)
-      text(:id, "button-with-tooltip")
-    end
-
-    content do |c|
-      c.tooltip(text: "Tooltip text")
+      select(:tooltip_direction, Primer::Alpha::Tooltip::DIRECTION_OPTIONS, :s)
     end
   end
 end
