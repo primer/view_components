a4a2b0cc033ee0095881a0b05bfae1baaba7a95a
Hector Garcia <hectahertz@github.com>
2022-03-30 18:22:39 +0200

    Configure .devcontainer to download primer/css on post-create

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -2,3 +2,13 @@
 
 sudo cp --force .devcontainer/welcome-message.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
 script/setup
+
+script_folder="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
+workspaces_folder="$(cd "${script_folder}/.." && pwd)"
+
+cd "${workspaces_folder}"
+if [ ! -d "$1" ]; then
+    git clone "https://github.com/primer/css"
+else
+    echo "Already cloned $1"
+fi
