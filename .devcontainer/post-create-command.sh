6faafe98dd062eee02e6bce889b9fdd7e191cffa
Hector Garcia <hectahertz@github.com>
2022-03-31 14:43:23 +0000

    Fix post-create-command.sh css folder

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -4,7 +4,7 @@ sudo cp --force .devcontainer/welcome-message.txt /usr/local/etc/vscode-dev-cont
 script/setup
 
 script_folder="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
-workspaces_folder="$(cd "${script_folder}/.." && pwd)"
+workspaces_folder="$(cd "${script_folder}/../.." && pwd)"
 
 cd "${workspaces_folder}"
 git clone "https://github.com/primer/css"
