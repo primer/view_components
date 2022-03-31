1d55aa9ebd6aaff55fbb4be3609f6bca06f8c01f
Hector Garcia <hectahertz@github.com>
2022-03-31 14:18:08 +0000

    Fix post-create-command

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -7,8 +7,4 @@ script_folder="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
 workspaces_folder="$(cd "${script_folder}/.." && pwd)"
 
 cd "${workspaces_folder}"
-if [ ! -d "$1" ]; then
-    git clone "https://github.com/primer/css"
-else
-    echo "Already cloned $1"
-fi
+git clone "https://github.com/primer/css"
