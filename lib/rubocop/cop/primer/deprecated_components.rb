6238669b9d020793b99741de9b63443af1ac164e
Kate Higa <khiga8@github.com>
2022-04-09 11:39:57 -0700

    all tests pass

--- lib/rubocop/cop/primer/deprecated_components.rb
+++ lib/rubocop/cop/primer/deprecated_components.rb
@@ -9,7 +9,7 @@ module RuboCop
     module Primer
       # This cop ensures that components marked as "deprecated" in `static/statuses.json` are not used.
       class DeprecatedComponents < BaseCop
-        # Converts a string to a pattern accepted by rubocop-ast
+        # Converts a string to acceptable rubocop-ast pattern syntax
         def pattern(component)
           Parser::CurrentRuby.parse(component)
             .to_s
@@ -17,10 +17,6 @@ module RuboCop
             .gsub("\n", "")
             .gsub("  ", " ")
         end
-
-        # def_node_matcher :legacy_component?, <<~PATTERN
-        #   (send %pattern :new ...)
-        # PATTERN
 
         # If there is no alternative, set the value to nil.
         ALTERNATIVE_COMPONENTS = {
@@ -33,21 +29,16 @@ module RuboCop
 
         def on_send(node)
           load_deprecated_components
-          # @deprecated_components
-          # ["Primer::Tooltip"].each do |component|
-          #   # add_offense(node, message: message(component)) 
-          #   binding.irb if legacy_component?(node, pattern: pattern(component))
-          # end
-          ["Primer::Tooltip"].each do |component|
+          @deprecated_components.each do |component|
             pattern = "(send #{pattern(component)} :new ...)"
-            if NodePattern.new(pattern).match(node)
+            add_offense(node, message: message(component))  if NodePattern.new(pattern).match(node)
           end
         end
 
         def message(component)
           message = "#{component} has been deprecated and should not be used."
           if ALTERNATIVE_COMPONENTS.fetch(component).present?
-            message += " Please try #{ALTERNATIVE_COMPONENTS[component]} instead." 
+            message += " Please use #{ALTERNATIVE_COMPONENTS[component]} instead." 
           end
           message
         end
@@ -61,7 +52,7 @@ module RuboCop
           @deprecated_components = json.select { |_, value| value == "deprecated" }.keys
           @deprecated_components.each do |deprecated|
             unless ALTERNATIVE_COMPONENTS.keys.include?(deprecated)
-              raise "Please provide an alternative component for #{deprecated} in ALTERNATIVE_COMPONENTS. If none exist, set the value to nil."
+              raise "Please add an alternative component for #{deprecated} to ALTERNATIVE_COMPONENTS. If none exist, set the value to nil."
             end
           end
         end
--- test/rubocop/deprecated_components_test.rb
+++ test/rubocop/deprecated_components_test.rb
@@ -7,22 +7,22 @@ class RubocopDeprecatedComponentsTest < CopTest
     RuboCop::Cop::Primer::DeprecatedComponents
   end
 
-  # def test_raises_offense_if_calling_legacy_component
-  #   investigate(cop, <<-RUBY)
-  #     Primer::Tooltip.new
-  #     Primer::BlankslateComponent.new
-  #     Primer::FlexComponent.new
-  #   RUBY
+  def test_raises_offense_if_calling_legacy_component
+    investigate(cop, <<-RUBY)
+      Primer::Tooltip.new
+      Primer::BlankslateComponent.new
+      Primer::FlexComponent.new
+    RUBY
 
-  #   assert_equal 3, cop.offenses.count
-  # end
+    assert_equal 3, cop.offenses.count
+  end
 
-  # def test_raises_offense_if_calling_legacy_component_with_args
-  #   investigate(cop, <<-RUBY)
-  #     Primer::FlexComponent.new(:foo)
-  #   RUBY
-  #   assert_equal 1, cop.offenses.count
-  # end
+  def test_raises_offense_if_calling_legacy_component_with_args
+    investigate(cop, <<-RUBY)
+      Primer::FlexComponent.new(:foo)
+    RUBY
+    assert_equal 1, cop.offenses.count
+  end
 
   def test_raises_offense_if_calling_legacy_component_in_render
     investigate(cop, <<-RUBY)
@@ -32,21 +32,20 @@ class RubocopDeprecatedComponentsTest < CopTest
     assert_equal 1, cop.offenses.count
   end
 
-  # def test_raises_offense_if_calling_legacy_component_in_render_with_args
-  #   investigate(cop, <<-RUBY)
-  #     render(Primer::FlexComponent.new(foo: "bar"))
-  #   RUBY
+  def test_raises_offense_if_calling_legacy_component_in_render_with_args
+    investigate(cop, <<-RUBY)
+      render(Primer::FlexComponent.new(foo: "bar"))
+    RUBY
 
-  #   assert_equal 1, cop.offenses.count
-  # end
+    assert_equal 1, cop.offenses.count
+  end
 
-  # def test_suggests_alternative_component_if_available
-  #   investigate(cop, <<-RUBY)
-  #     render(Primer::Tooltip.new(foo: "bar"))
-  #   RUBY
+  def test_suggests_alternative_component_if_available
+    investigate(cop, <<-RUBY)
+      render(Primer::Tooltip.new(foo: "bar"))
+    RUBY
 
-  #   binding.irb
-  #   assert_equal 1, cop.offenses.count
-  #   assert_equal "Please try Primer::Alpha::Tooltip instead.", cop.offenses.first.message
-  # end
+    assert_equal 1, cop.offenses.count
+    assert_equal "Primer::Tooltip has been deprecated and should not be used. Please use Primer::Alpha::Tooltip instead.", cop.offenses.first.message
+  end
 end
