f09c402be72ba1c85ca2ac146cb3f80974ef0a5f
Kate Higa <khiga8@github.com>
2022-04-09 11:43:56 -0700

    add examples and clean

--- lib/rubocop/cop/primer/deprecated_components.rb
+++ lib/rubocop/cop/primer/deprecated_components.rb
@@ -2,22 +2,25 @@
 
 require "rubocop"
 require "json"
-require 'parser/current'
+require "parser/current"
 
 module RuboCop
   module Cop
     module Primer
       # This cop ensures that components marked as "deprecated" in `static/statuses.json` are not used.
+      #
+      # bad
+      # Primer::BlankslateComponent.new(:foo)
+      #
+      # good
+      # Primer::Beta::Blankslate.new(:foo)
+      #
+      # bad
+      # Primer::Tooltip.new(:foo)
+      #
+      # good
+      # Primer::Alpha::Tooltip.new(:foo)
       class DeprecatedComponents < BaseCop
-        # Converts a string to acceptable rubocop-ast pattern syntax
-        def pattern(component)
-          Parser::CurrentRuby.parse(component)
-            .to_s
-            .gsub('nil', 'nil?')
-            .gsub("\n", "")
-            .gsub("  ", " ")
-        end
-
         # If there is no alternative, set the value to nil.
         ALTERNATIVE_COMPONENTS = {
           "Primer::BlankslateComponent" => "Primer::Beta::Blankslate",
@@ -28,33 +31,40 @@ module RuboCop
         }.freeze
 
         def on_send(node)
-          load_deprecated_components
-          @deprecated_components.each do |component|
+          deprecated_components.each do |component|
             pattern = "(send #{pattern(component)} :new ...)"
-            add_offense(node, message: message(component))  if NodePattern.new(pattern).match(node)
+            add_offense(node, message: message(component)) if NodePattern.new(pattern).match(node)
           end
+        end
+
+        private
+
+        # Converts a string to acceptable rubocop-ast pattern syntax
+        def pattern(component)
+          Parser::CurrentRuby.parse(component)
+                             .to_s
+                             .gsub("nil", "nil?")
+                             .delete("\n")
+                             .gsub("  ", " ")
         end
 
         def message(component)
           message = "#{component} has been deprecated and should not be used."
-          if ALTERNATIVE_COMPONENTS.fetch(component).present?
-            message += " Please use #{ALTERNATIVE_COMPONENTS[component]} instead." 
-          end
+          message += " Please use #{ALTERNATIVE_COMPONENTS[component]} instead." if ALTERNATIVE_COMPONENTS.fetch(component).present?
           message
         end
 
-        def load_deprecated_components
+        def deprecated_components
           json = JSON.parse(
             File.read(
               File.join(File.dirname(__FILE__), "../../../../static/statuses.json")
             )
           ).freeze
-          @deprecated_components = json.select { |_, value| value == "deprecated" }.keys
-          @deprecated_components.each do |deprecated|
-            unless ALTERNATIVE_COMPONENTS.keys.include?(deprecated)
-              raise "Please add an alternative component for #{deprecated} to ALTERNATIVE_COMPONENTS. If none exist, set the value to nil."
-            end
+          deprecated_components = json.select { |_, value| value == "deprecated" }.keys
+          deprecated_components.each do |deprecated|
+            raise "Please add an alternative component for #{deprecated} to ALTERNATIVE_COMPONENTS. If none exist, set the value to nil." unless ALTERNATIVE_COMPONENTS.key?(deprecated)
           end
+          deprecated_components
         end
       end
     end
