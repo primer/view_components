6b743406924ac4102a256863c313f5d72bd84c63
Kate Higa <16447748+khiga8@users.noreply.github.com>
2022-04-11 11:51:09 -0700

    Add performance improvement (#1112)

--- lib/rubocop/cop/primer/deprecated_components.rb
+++ lib/rubocop/cop/primer/deprecated_components.rb
@@ -65,14 +65,14 @@ module RuboCop
         end
 
         def deprecated_components
-          deprecated_components = statuses_json.select { |_, value| value == "deprecated" }.keys
-          deprecated_components.each do |deprecated|
-            unless COMPONENT_TO_USE_INSTEAD.key?(deprecated)
-              raise "Please provide a component that should be used in place of #{deprecated} in COMPONENT_TO_USE_INSTEAD. "\
-                    "If there is no alternative, set the value to nil."
+          @deprecated_components ||= statuses_json.select { |_, value| value == "deprecated" }.keys.tap do |deprecated_components|
+            deprecated_components.each do |deprecated|
+              unless COMPONENT_TO_USE_INSTEAD.key?(deprecated)
+                raise "Please provide a component that should be used in place of #{deprecated} in COMPONENT_TO_USE_INSTEAD. "\
+                      "If there is no alternative, set the value to nil."
+              end
             end
           end
-          deprecated_components
         end
       end
     end
