33c99ad84920eb7c950d5e84b7a3f2ef2943aa8a
Kate Higa <khiga8@github.com>
2022-04-22 11:26:13 -0700

    Rubocop linter

--- test/components/primer/alpha/action_menu/item_test.rb
+++ test/components/primer/alpha/action_menu/item_test.rb
@@ -9,7 +9,7 @@ class PrimerActionMenuItemTest < Minitest::Test
     render_inline(Primer::Alpha::ActionMenu::Item.new) { "Default" }
 
     assert_selector("li.ActionList-item[role='none']") do
-      assert_selector("span.ActionList-content[role='menuitem']") do 
+      assert_selector("span.ActionList-content[role='menuitem']") do
         assert_selector("span.ActionList-item-label", text: "Default")
       end
     end
@@ -21,7 +21,7 @@ class PrimerActionMenuItemTest < Minitest::Test
     end
 
     assert_selector("li.ActionList-item[role='none']") do
-      assert_selector("span.ActionList-content[role='menuitem']") do 
+      assert_selector("span.ActionList-content[role='menuitem']") do
         assert_selector("span.ActionList-item-label", text: "Default")
       end
     end
@@ -30,7 +30,7 @@ class PrimerActionMenuItemTest < Minitest::Test
   def test_allows_some_interactive_elements_as_menu_item
     render_inline(Primer::Alpha::ActionMenu::Item.new(tag: :a, href: "/")) { "Link" }
     assert_selector("li.ActionList-item[role='none']") do
-      assert_selector("a[role='menuitem'][href='/']") do 
+      assert_selector("a[role='menuitem'][href='/']") do
         assert_selector("span.ActionList-item-label", text: "Link")
       end
     end
@@ -44,7 +44,7 @@ class PrimerActionMenuItemTest < Minitest::Test
   def test_renders_item_as_dangerous
     render_inline(Primer::Alpha::ActionMenu::Item.new(is_dangerous: true)) { "Dangerous" }
     assert_selector("li.ActionList-item.ActionList-item--danger[role='none']") do
-      assert_selector("span[role='menuitem']") do 
+      assert_selector("span[role='menuitem']") do
         assert_selector("span.ActionList-item-label", text: "Dangerous")
       end
     end
