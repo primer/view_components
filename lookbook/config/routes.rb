75b3f99e6627a80adc998f956dad3fecb557c730
Jon Rohan <yes@jonrohan.codes>
2022-04-27 00:08:32 +0000

    Adding setup scripts

--- lookbook/config/routes.rb
+++ lookbook/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   mount Lookbook::Engine, at: "/lookbook"
-  # mount ActionCable.server => "/"
-
+  get "/", to: redirect("/lookbook")
   get "/auto_complete", to: "auto_complete_test#index"
 end
