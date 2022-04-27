6bb9e98108a37cf71d8e16d0813e3cdc4e5799d4
Jon Rohan <yes@jonrohan.codes>
2022-04-27 01:55:18 +0000

    Fixing autocomplete

--- lookbook/app/controllers/preview_controller.rb
+++ lookbook/app/controllers/preview_controller.rb
@@ -1,3 +1,4 @@
 class PreviewController < ViewComponentsController
   helper Lookbook::PreviewHelper
+  helper Primer::ViewHelper
 end
---
+++ lookbook/test/components/previews/auto_complete_preview.rb
@@ -0,0 +1,17 @@
+class AutoCompletePreview < ViewComponent::Preview
+  def default
+    render(Primer::Beta::AutoComplete.new(label_text: "Select a fruit", input_id: "input-id", list_id: "test-id", src: "/auto_complete"))
+  end
+
+  def with_non_visible_label
+    render(Primer::Beta::AutoComplete.new(label_text: "Select a fruit", input_id: "input-id", list_id: "test-id", src: "/auto_complete", is_label_visible: false))
+  end
+
+  def with_icon
+    render(Primer::Beta::AutoComplete.new(label_text: "Select a fruit", input_id: "input-id", list_id: "test-id", src: "/auto_complete", with_icon: true))
+  end
+
+  def with_clear_button
+    render(Primer::Beta::AutoComplete.new(label_text: "Select a fruit", input_id: "input-id", list_id: "test-id", src: "/auto_complete", is_clearable: true))
+  end
+end
