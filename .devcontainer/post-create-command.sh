0d919fb9e67bbe26ce8e08b8df55d5232901c79a
Katie Langerman <langermank@github.com>
2022-05-04 20:43:17 +0000

    test remove setup script

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sudo cp --force .devcontainer/welcome-message.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
-script/setup
 
 script_folder="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
 workspaces_folder="$(cd "${script_folder}/../.." && pwd)"
