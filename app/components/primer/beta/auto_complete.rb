d517f3104ab981dfc9cd6f46aa9a17d04307fdbe
Katie Langerman <langermank@github.com>
2022-05-22 14:34:19 +0000

    update docs

--- app/components/primer/beta/auto_complete.rb
+++ app/components/primer/beta/auto_complete.rb
@@ -39,6 +39,12 @@ module Primer
         Primer::BaseComponent.new(**system_arguments)
       }
 
+      #
+      # Leading visual.
+      #
+      # - `leading_visual_icon` for a <%= link_to_component(Primer::OcticonComponent) %>.
+      #
+      # @param system_arguments [Hash] Same arguments as <%= link_to_component(Primer::OcticonComponent) %>.
       renders_one :leading_visual, types: {
         icon: lambda { |**system_arguments|
           system_arguments[:classes] = class_names("FormControl--input-leadingVisual")
@@ -87,35 +93,68 @@ module Primer
         Primer::BaseComponent.new(**sanitized_args)
       }
 
-      # @example Default
+      # @example Leading visual
       #   @description
-      #     Labels are stacked by default.
+      #     Display any Octicon aaSsas a leading visual within the field
       #   @code
-      #     <%= render(Primer::Beta::AutoComplete.new(label_text: "Fruits", src: "/auto_complete", input_id: "fruits-input--default", list_id: "fruits-popup--default")) %>
+      # <%= render(Primer::Beta::AutoComplete.new(label_text: label_text, input_id: input_id, list_id: list_id, src: "/auto_complete", show_clear_button: show_clear_button, visually_hide_label: visually_hide_label, placeholder: placeholder, size: size, full_width: full_width, disabled: disabled, invalid: invalid, input_name: input_name)) do |c| %>
+      #   <% c.leading_visual_icon(icon: :search)
+      #   <% c.results do
+      #     <% render(Primer::Beta::AutoComplete::Item.new(selected: true, value: "apple")) do |_c| %>
+      #       Apple
+      #     <% end %>
+      #     <% render(Primer::Beta::AutoComplete::Item.new(value: "orange")) do |_c| %>
+      #       Orange
+      #     <% end %>
+      #   <% end %>
+      # <% end %>
       #
-      # @example With inline label
+      # @example Trailing action
       #   @description
       #     Labels can be inline by setting `is_label_inline: true`. However, labels will always become stacked on smaller screen sizes.
       #   @code
-      #     <%= render(Primer::Beta::AutoComplete.new(label_text: "Fruits", src: "/auto_complete", is_label_inline: true, input_id: "fruits-input--inline-label", list_id: "fruits-popup--inline-label")) %>
+      #     <%= render(Primer::Beta::AutoComplete.new(label_text: label_text, input_id: input_id, list_id: list_id, src: "/auto_complete", show_clear_button: show_clear_button, visually_hide_label: visually_hide_label, placeholder: placeholder, size: size, full_width: full_width, disabled: disabled, invalid: invalid, input_name: input_name)) do |c| %>
+      #   <% c.results do
+      #     <% render(Primer::Beta::AutoComplete::Item.new(selected: true, value: "apple")) do |_c| %>
+      #       Apple
+      #     <% end %>
+      #     <% render(Primer::Beta::AutoComplete::Item.new(value: "orange")) do |_c| %>
+      #       Orange
+      #     <% end %>
+      #   <% end %>
+      # <% end %>
       #
-      # @example With non-visible label
+      # @example Visually hidden label
       #   @description
       #     A non-visible label may be rendered with `visually_hide_label: true`, but it is highly discouraged. See <%= link_to_accessibility %>.
       #   @code
-      #     <%= render(Primer::Beta::AutoComplete.new(label_text: "Fruits", src: "/auto_complete", input_id: "fruits-input--non-visible-label", list_id: "fruits-popup--non-visible-label", visually_hide_label: true)) %>
+      #     <%= render(Primer::Beta::AutoComplete.new(label_text: label_text, input_id: input_id, list_id: list_id, src: "/auto_complete", show_clear_button: show_clear_button, visually_hide_label: visually_hide_label, placeholder: placeholder, size: size, full_width: full_width, disabled: disabled, invalid: invalid, input_name: input_name)) do |c| %>
+      #   <% c.leading_visual_icon(icon: :search)
+      #   <% c.results do
+      #     <% render(Primer::Beta::AutoComplete::Item.new(selected: true, value: "apple")) do |_c| %>
+      #       Apple
+      #     <% end %>
+      #     <% render(Primer::Beta::AutoComplete::Item.new(value: "orange")) do |_c| %>
+      #       Orange
+      #     <% end %>
+      #   <% end %>
+      # <% end %>
       #
-      # @example With icon
+      # @example Full width field
       #   @description
-      #     To display a search icon, set `with_icon` to `true`.
+      #     To allow field to span width of its container, set `full_width` to `true`.
       #   @code
-      #     <%= render(Primer::Beta::AutoComplete.new(label_text: "Fruits", src: "/auto_complete", list_id: "fruits-popup--icon", input_id: "fruits-input--icon", with_icon: true)) %>
-      #
-      # @example With icon and non-visible label
-      #   <%= render(Primer::Beta::AutoComplete.new(label_text: "Fruits", src: "/auto_complete", list_id: "fruits-popup--icon-no-label", input_id: "fruits-input--icon-no-label", with_icon: true, visually_hide_label: true)) %>
-      #
-      # @example With clear button
-      #   <%= render(Primer::Beta::AutoComplete.new(label_text: "Fruits", src: "/auto_complete", input_id: "fruits-input--clear", list_id: "fruits-popup--clear", show_clear_button: true)) %>
+      #     <%= render(Primer::Beta::AutoComplete.new(label_text: label_text, input_id: input_id, list_id: list_id, src: "/auto_complete", show_clear_button: show_clear_button, visually_hide_label: visually_hide_label, placeholder: placeholder, size: size, full_width: full_width, disabled: disabled, invalid: invalid, input_name: input_name)) do |c| %>
+      #   <% c.leading_visual_icon(icon: :search)
+      #   <% c.results do
+      #     <% render(Primer::Beta::AutoComplete::Item.new(selected: true, value: "apple")) do |_c| %>
+      #       Apple
+      #     <% end %>
+      #     <% render(Primer::Beta::AutoComplete::Item.new(value: "orange")) do |_c| %>
+      #       Orange
+      #     <% end %>
+      #   <% end %>
+      # <% end %>
       #
       # @example With custom classes for the input
       #   <%= render(Primer::Beta::AutoComplete.new(label_text: "Fruits", src: "/auto_complete", input_id: "fruits-input--custom-input", list_id: "fruits-popup--custom-input")) do |c| %>
@@ -142,13 +181,18 @@ module Primer
       # @param visually_hide_label [Boolean] Controls if the label is visible. If `true`, screen reader only text will be added.
       # @param show_clear_button [Boolean] Adds optional clear button.
       # @param system_arguments [Hash] <%= link_to_system_arguments_docs %>
+      # @param size [Hash] Input size can be small, medium (default), or large
+      # @param full_width [Boolean] Input can be full-width or fit to content
+      # @param disabled [Boolean] Disabled input
+      # @param invalid [Boolean] Invalid input
+      # @param placeholder [String] The placeholder text displayed within the input
       def initialize(label_text:, src:, list_id:, input_id:, input_name: nil, placeholder: nil, show_clear_button: false, visually_hide_label: false, size: DEFAULT_SIZE, full_width: false, disabled: false, invalid: false, **system_arguments)
         @label_text = label_text
         @list_id = list_id
         @input_id = input_id
         @input_name = input_name || input_id
         @placeholder = placeholder
-        @visually_hide_label = visually_hide_label && "sr-only"
+        @visually_hide_label = visually_hide_label
         @show_clear_button = show_clear_button
         @system_arguments = deny_tag_argument(**system_arguments)
         @system_arguments[:tag] = "auto-complete"
--- lookbook/test/components/previews/beta/auto_complete_preview.rb
+++ lookbook/test/components/previews/beta/auto_complete_preview.rb
@@ -12,9 +12,12 @@ module Beta
     # @param full_width toggle
     # @param disabled toggle
     # @param invalid toggle
-    def playground(label_text: "Select a fruit", show_clear_button: false, visually_hide_label: false, placeholder: "Placeholder text", size: :medium, full_width: false, disabled: false, invalid: false)
-      render(Primer::Beta::AutoComplete.new(label_text: label_text, input_id: "input-id", list_id: "test-id", src: "/auto_complete", show_clear_button: show_clear_button, visually_hide_label: visually_hide_label, placeholder: placeholder, size: size, full_width: full_width, disabled: disabled, invalid: invalid)) do |c|
-        # c.leading_visual_icon(icon: :search)
+    # @param input_id text
+    # @param list_id text
+    # @param input_name text
+    def playground(label_text: "Select a fruit", show_clear_button: false, visually_hide_label: false, placeholder: "Placeholder text", size: :medium, full_width: false, disabled: false, invalid: false, input_id: "input-id", list_id: "list-id", input_name: "input-id")
+      render(Primer::Beta::AutoComplete.new(label_text: label_text, input_id: input_id, list_id: list_id, src: "/auto_complete", show_clear_button: show_clear_button, visually_hide_label: visually_hide_label, placeholder: placeholder, size: size, full_width: full_width, disabled: disabled, invalid: invalid, input_name: input_name)) do |c|
+        c.leading_visual_icon(icon: :search)
         c.results do
           render(Primer::Beta::AutoComplete::Item.new(selected: true, value: "apple")) do |_c|
             Apple
@@ -25,5 +28,187 @@ module Beta
         end
       end
     end
+
+    # @label Leading visual
+    # @param label_text text
+    # @param show_clear_button toggle
+    # @param visually_hide_label toggle
+    # @param placeholder text
+    # @param size select [small, medium, large]
+    # @param full_width toggle
+    # @param disabled toggle
+    # @param invalid toggle
+    # @param input_id text
+    # @param list_id text
+    # @param input_name text
+    def leading_visual(label_text: "Select a fruit", show_clear_button: false, visually_hide_label: false, placeholder: "Placeholder text", size: :medium, full_width: false, disabled: false, invalid: false, input_id: "input-id", list_id: "list-id", input_name: "input-id")
+      render(Primer::Beta::AutoComplete.new(label_text: label_text, input_id: input_id, list_id: list_id, src: "/auto_complete", show_clear_button: show_clear_button, visually_hide_label: visually_hide_label, placeholder: placeholder, size: size, full_width: full_width, disabled: disabled, invalid: invalid, input_name: input_name)) do |c|
+        c.leading_visual_icon(icon: :search)
+        c.results do
+          render(Primer::Beta::AutoComplete::Item.new(selected: true, value: "apple")) do |_c|
+            Apple
+          end
+          render(Primer::Beta::AutoComplete::Item.new(value: "orange")) do |_c|
+            Orange
+          end
+        end
+      end
+    end
+
+    # @label Trailing action
+    # @param label_text text
+    # @param show_clear_button toggle
+    # @param visually_hide_label toggle
+    # @param placeholder text
+    # @param size select [small, medium, large]
+    # @param full_width toggle
+    # @param disabled toggle
+    # @param invalid toggle
+    # @param input_id text
+    # @param list_id text
+    # @param input_name text
+    def trailing_action(label_text: "Select a fruit", show_clear_button: true, visually_hide_label: false, placeholder: "Placeholder text", size: :medium, full_width: false, disabled: false, invalid: false, input_id: "input-id", list_id: "list-id", input_name: "input-id")
+      render(Primer::Beta::AutoComplete.new(label_text: label_text, input_id: input_id, list_id: list_id, src: "/auto_complete", show_clear_button: show_clear_button, visually_hide_label: visually_hide_label, placeholder: placeholder, size: size, full_width: full_width, disabled: disabled, invalid: invalid, input_name: input_name)) do |c|
+        c.results do
+          render(Primer::Beta::AutoComplete::Item.new(selected: true, value: "apple")) do |_c|
+            Apple
+          end
+          render(Primer::Beta::AutoComplete::Item.new(value: "orange")) do |_c|
+            Orange
+          end
+        end
+      end
+    end
+
+    # @label Full width
+    # @param label_text text
+    # @param show_clear_button toggle
+    # @param visually_hide_label toggle
+    # @param placeholder text
+    # @param size select [small, medium, large]
+    # @param full_width toggle
+    # @param disabled toggle
+    # @param invalid toggle
+    # @param input_id text
+    # @param list_id text
+    # @param input_name text
+    def full_width(label_text: "Select a fruit", show_clear_button: false, visually_hide_label: false, placeholder: "Placeholder text", size: :medium, full_width: true, disabled: false, invalid: false, input_id: "input-id", list_id: "list-id", input_name: "input-id")
+      render(Primer::Beta::AutoComplete.new(label_text: label_text, input_id: input_id, list_id: list_id, src: "/auto_complete", show_clear_button: show_clear_button, visually_hide_label: visually_hide_label, placeholder: placeholder, size: size, full_width: full_width, disabled: disabled, invalid: invalid, input_name: input_name)) do |c|
+        c.leading_visual_icon(icon: :search)
+        c.results do
+          render(Primer::Beta::AutoComplete::Item.new(selected: true, value: "apple")) do |_c|
+            Apple
+          end
+          render(Primer::Beta::AutoComplete::Item.new(value: "orange")) do |_c|
+            Orange
+          end
+        end
+      end
+    end
+
+    # @label Visually hide label
+    # @param label_text text
+    # @param show_clear_button toggle
+    # @param visually_hide_label toggle
+    # @param placeholder text
+    # @param size select [small, medium, large]
+    # @param full_width toggle
+    # @param disabled toggle
+    # @param invalid toggle
+    # @param input_id text
+    # @param list_id text
+    # @param input_name text
+    def visually_hide_label(label_text: "Select a fruit", show_clear_button: false, visually_hide_label: true, placeholder: "Placeholder text", size: :medium, full_width: false, disabled: false, invalid: false, input_id: "input-id", list_id: "list-id", input_name: "input-id")
+      render(Primer::Beta::AutoComplete.new(label_text: label_text, input_id: input_id, list_id: list_id, src: "/auto_complete", show_clear_button: show_clear_button, visually_hide_label: visually_hide_label, placeholder: placeholder, size: size, full_width: full_width, disabled: disabled, invalid: invalid, input_name: input_name)) do |c|
+        c.leading_visual_icon(icon: :search)
+        c.results do
+          render(Primer::Beta::AutoComplete::Item.new(selected: true, value: "apple")) do |_c|
+            Apple
+          end
+          render(Primer::Beta::AutoComplete::Item.new(value: "orange")) do |_c|
+            Orange
+          end
+        end
+      end
+    end
+
+    # @!group Sizes
+
+    # @param label_text text
+    # @param show_clear_button toggle
+    # @param visually_hide_label toggle
+    # @param placeholder text
+    # @param size select [small, medium, large]
+    # @param full_width toggle
+    # @param disabled toggle
+    # @param invalid toggle
+    # @param input_id text
+    # @param list_id text
+    # @param input_name text
+    def small(label_text: "Select a fruit", show_clear_button: false, visually_hide_label: false, placeholder: "Placeholder text", size: :small, full_width: false, disabled: false, invalid: false, input_id: "input-id-1", list_id: "list-id-1", input_name: "input-id-1")
+      render(Primer::Beta::AutoComplete.new(label_text: label_text, input_id: input_id, list_id: list_id, src: "/auto_complete", show_clear_button: show_clear_button, visually_hide_label: visually_hide_label, placeholder: placeholder, size: size, full_width: full_width, disabled: disabled, invalid: invalid, input_name: input_name)) do |c|
+        c.leading_visual_icon(icon: :search)
+        c.results do
+          render(Primer::Beta::AutoComplete::Item.new(selected: true, value: "apple")) do |_c|
+            Apple
+          end
+          render(Primer::Beta::AutoComplete::Item.new(value: "orange")) do |_c|
+            Orange
+          end
+        end
+      end
+    end
+
+    # @param label_text text
+    # @param show_clear_button toggle
+    # @param visually_hide_label toggle
+    # @param placeholder text
+    # @param size select [small, medium, large]
+    # @param full_width toggle
+    # @param disabled toggle
+    # @param invalid toggle
+    # @param input_id text
+    # @param list_id text
+    # @param input_name text
+    def medium(label_text: "Select a fruit", show_clear_button: false, visually_hide_label: false, placeholder: "Placeholder text", size: :medium, full_width: false, disabled: false, invalid: false, input_id: "input-id-2", list_id: "list-id-2", input_name: "input-id-2")
+      render(Primer::Beta::AutoComplete.new(label_text: label_text, input_id: input_id, list_id: list_id, src: "/auto_complete", show_clear_button: show_clear_button, visually_hide_label: visually_hide_label, placeholder: placeholder, size: size, full_width: full_width, disabled: disabled, invalid: invalid, input_name: input_name)) do |c|
+        c.leading_visual_icon(icon: :search)
+        c.results do
+          render(Primer::Beta::AutoComplete::Item.new(selected: true, value: "apple")) do |_c|
+            Apple
+          end
+          render(Primer::Beta::AutoComplete::Item.new(value: "orange")) do |_c|
+            Orange
+          end
+        end
+      end
+    end
+
+    # @param label_text text
+    # @param show_clear_button toggle
+    # @param visually_hide_label toggle
+    # @param placeholder text
+    # @param size select [small, medium, large]
+    # @param full_width toggle
+    # @param disabled toggle
+    # @param invalid toggle
+    # @param input_id text
+    # @param list_id text
+    # @param input_name text
+    def large(label_text: "Select a fruit", show_clear_button: false, visually_hide_label: false, placeholder: "Placeholder text", size: :large, full_width: false, disabled: false, invalid: false, input_id: "input-id-3", list_id: "list-id-3", input_name: "input-id-3")
+      render(Primer::Beta::AutoComplete.new(label_text: label_text, input_id: input_id, list_id: list_id, src: "/auto_complete", show_clear_button: show_clear_button, visually_hide_label: visually_hide_label, placeholder: placeholder, size: size, full_width: full_width, disabled: disabled, invalid: invalid, input_name: input_name)) do |c|
+        c.leading_visual_icon(icon: :search)
+        c.results do
+          render(Primer::Beta::AutoComplete::Item.new(selected: true, value: "apple")) do |_c|
+            Apple
+          end
+          render(Primer::Beta::AutoComplete::Item.new(value: "orange")) do |_c|
+            Orange
+          end
+        end
+      end
+    end
+
+    # @!endgroup
   end
 end
