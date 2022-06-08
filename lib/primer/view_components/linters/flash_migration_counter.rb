a600eddb2a7df211e03dc749bfc763091d1d115e
Joel Hawksley <joel@hawksley.org>
2022-06-07 18:03:38 -0600

    rename FlashComponent to Flash

--- lib/primer/view_components/linters/flash_migration_counter.rb
+++ lib/primer/view_components/linters/flash_migration_counter.rb
@@ -7,14 +7,14 @@ require_relative "argument_mappers/flash"
 module ERBLint
   module Linters
     # Counts the number of times a HTML flash is used instead of the component.
-    class FlashComponentMigrationCounter < BaseLinter
+    class FlashMigrationCounter < BaseLinter
       include Autocorrectable
 
       TAGS = %w[div].freeze
       CLASSES = %w[flash].freeze
-      MESSAGE = "We are migrating flashes to use [Primer::FlashComponent](https://primer.style/view-components/components/flash), please try to use that instead of raw HTML."
+      MESSAGE = "We are migrating flashes to use [Primer::Beta::Flash](https://primer.style/view-components/components/flash), please try to use that instead of raw HTML."
       ARGUMENT_MAPPER = ArgumentMappers::Flash
-      COMPONENT = "Primer::FlashComponent"
+      COMPONENT = "Primer::Beta::Flash"
 
       def map_arguments(tag, tag_tree)
         # We can only autocorrect elements with simple text as content.
--- stories/primer/beta/flash_stories.rb
+++ stories/primer/beta/flash_stories.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-class Primer::FlashComponentStories < ViewComponent::Storybook::Stories
+class Primer::Beta::FlashStories < ViewComponent::Storybook::Stories
   layout "storybook_preview"
 
   story(:flash) do
     controls do
       text(:icon, "people")
-      select(:scheme, Primer::FlashComponent::SCHEME_MAPPINGS.keys, :default)
+      select(:scheme, Primer::Beta::Flash::SCHEME_MAPPINGS.keys, :default)
       full false
       spacious false
       dismissible false
