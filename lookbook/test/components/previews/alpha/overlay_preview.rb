584b80ba4e9062127e9c5b13d28e27f60a4ccdc1
Katie Langerman <langermank@github.com>
2022-06-10 18:20:39 +0000

    configure multi level preview

--- lookbook/test/components/previews/alpha/overlay_preview.rb
+++ lookbook/test/components/previews/alpha/overlay_preview.rb
@@ -29,5 +29,50 @@ module Alpha
         c.footer_button { "Dismiss" }
       end
     end
+
+    # @label Multi layered
+    #
+    # @param title [String] text
+    # @param description [String] text
+    # @param overlay_id [String] text
+    # @param show_header_divider [Boolean] toggle
+    # @param show_footer_divider [Boolean] toggle
+    # @param show_close_button [Boolean] toggle
+    # @param footer_content_align select [start, center, end]
+    # @param header_variant select [medium, large]
+    # @param body_padding_variant select [normal, condensed, none]
+    # @param motion select [scale_fade, none]
+    # @param variant_narrow select [center, anchor, side, full]
+    # @param variant_regular select [center, anchor, side, full]
+    # @param placement_narrow [Symbol] select [[Left, left], [Right, right], [Top, top], [Bottom, bottom], [Unset, unset]]
+    # @param placement_regular [Symbol] select [[Left, left], [Right, right], [Top, top], [Bottom, bottom], [Unset, unset]]
+    # @param open toggle
+    def multi_layered(
+      title: "Overlay title",
+      description: "This is the description of the dialog",
+      overlay_id: "dialog-without-buttons",
+      show_header_divider: false,
+      show_footer_divider: false,
+      show_close_button: true,
+      footer_content_align: :end,
+      header_variant: :medium,
+      body_padding_variant: :normal,
+      motion: :scale_fade,
+      open: true
+    )
+      render_with_template(locals: {
+                             open: open,
+                             title: title,
+                             description: description,
+                             overlay_id: overlay_id,
+                             show_header_divider: show_header_divider,
+                             show_footer_divider: show_footer_divider,
+                             show_close_button: show_close_button,
+                             footer_content_align: footer_content_align,
+                             header_variant: header_variant,
+                             body_padding_variant: body_padding_variant,
+                             motion: motion
+                           })
+    end
   end
 end
