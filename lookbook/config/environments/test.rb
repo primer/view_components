f5585dd57a23fdcc6a51cffecf6a5f6972f7e0dd
Jon Rohan <yes@jonrohan.codes>
2022-07-22 22:59:48 +0000

    Moving lookbook previews to main test folder

--- lookbook/config/environments/test.rb
+++ lookbook/config/environments/test.rb
@@ -54,6 +54,8 @@ Rails.application.configure do
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
 
+  config.view_component.preview_paths << Rails.root.join("../test/components/previews")
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
