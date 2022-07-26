73d0b8179d214568c822c972161e58a76d793629
Jon Rohan <yes@jonrohan.codes>
2022-07-26 23:43:10 +0000

    Filling out test

--- test/components/primer/alpha/segmented_control_test.rb
+++ test/components/primer/alpha/segmented_control_test.rb
@@ -2,12 +2,74 @@
 
 require "test_helper"
 
-class PrimerAlphaSegmentedControlTest < Minitest::Test
-  include Primer::ComponentTestHelpers
+module Primer
+  module Alpha
+    class SegmentedControlTest < Minitest::Test
+      include Primer::ComponentTestHelpers
+      include ViewComponent::RenderPreviewHelper
 
-  def test_renders
-    render_inline(Primer::SegmentedControl.new)
+      def test_renders
+        render_preview(:default)
 
-    assert_text("Add a test here")
+        assert_selector("segmented-control.SegmentedControl") do
+          assert_selector("button.SegmentedControl-button", count: Primer::Alpha::SegmentedControlPreview::NUMBER_OF_BUTTONS_DEFAULT) do
+            assert_selector(".SegmentedControl-content") do
+              refute_selector(".SegmentedControl-leadingVisual")
+              assert_selector(".SegmentedControl-text")
+            end
+          end
+        end
+      end
+
+      def test_selected_button_has_aria_attribute
+        render_preview(:default)
+
+        assert_selector("button.SegmentedControl-button.SegmentedControl-button--selected[aria-current=\"true\"]", count: 1)
+      end
+
+      def test_text_has_data_content_attribute
+        render_preview(:default)
+
+        assert_selector(".SegmentedControl-text[data-content]")
+      end
+
+      def test_renders_icons_and_text
+        render_preview(:icons_and_text)
+
+        assert_selector("segmented-control.SegmentedControl") do
+          assert_selector("button.SegmentedControl-button", count: Primer::Alpha::SegmentedControlPreview::NUMBER_OF_BUTTONS_DEFAULT) do
+            assert_selector(".SegmentedControl-content") do
+              assert_selector(".SegmentedControl-leadingVisual")
+              assert_selector(".SegmentedControl-text")
+            end
+          end
+        end
+      end
+
+      def test_renders_icons_only
+        render_preview(:icons_only)
+
+        assert_selector("segmented-control.SegmentedControl") do
+          assert_selector("button.SegmentedControl-button[aria-label]", count: Primer::Alpha::SegmentedControlPreview::NUMBER_OF_BUTTONS_DEFAULT) do
+            assert_selector(".SegmentedControl-content") do
+              assert_selector(".SegmentedControl-leadingVisual")
+              refute_selector(".SegmentedControl-text")
+            end
+          end
+        end
+      end
+
+      def test_renders_full_width
+        render_preview(:full_width)
+
+        assert_selector("segmented-control.SegmentedControl.SegmentedControl--fullWidth")
+      end
+
+      def test_renders_icons_only_when_narrow
+        render_preview(:icons_only_when_narrow)
+
+        assert_selector("segmented-control.SegmentedControl.SegmentedControl--iconOnly-whenNarrow")
+      end
+    end
   end
 end
