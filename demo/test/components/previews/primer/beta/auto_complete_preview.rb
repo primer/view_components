711e589f2ad9f18636592ec1b6022228e7988a17
Katie Langerman <langermank@github.com>
2022-07-26 23:16:23 +0000

    add more tests

--- demo/test/components/previews/primer/beta/auto_complete_preview.rb
+++ demo/test/components/previews/primer/beta/auto_complete_preview.rb
@@ -18,6 +18,16 @@ module Primer
       def show_clear_button
         render(Primer::Beta::AutoComplete.new(label_text: "Select a fruit", input_id: "input-id", list_id: "test-id", src: "/auto_complete", show_clear_button: true))
       end
+
+      def leading_visual
+        render(Primer::Beta::AutoComplete.new(label_text: "Select a fruit", show_clear_button: false, visually_hide_label: false, placeholder: "Placeholder text", size: :medium, full_width: false, disabled: false, invalid: false, input_id: "input-id", list_id: "list-id", input_name: "input-id", src: "/auto_complete")) do |c|
+          c.leading_visual_icon(icon: :search)
+        end
+      end
+
+      def full_width
+        render(Primer::Beta::AutoComplete.new(label_text: "Select a fruit", input_id: "input-id", list_id: "test-id", src: "/auto_complete", full_width: true))
+      end
     end
   end
 end
