741501211243376f6586163f43b8a82693d94ada
Katie Langerman <langermank@github.com>
2022-07-26 23:55:19 +0000

    try adding test

--- test/components/beta/auto_complete/item_test.rb
+++ test/components/beta/auto_complete/item_test.rb
@@ -22,4 +22,14 @@ class PrimerAutoCompleteItemTest < Minitest::Test
 
     assert_selector("li[role=\"option\"][data-autocomplete-value=\"foo\"][aria-disabled=\"true\"].ActionList-item", text: "Item")
   end
+
+  def test_renders_with_icon
+    render_inline Primer::Beta::AutoComplete::Item.new(value: "foo", disabled: true)  do |component|
+      component.leading_visual_icon(icon: :search)
+    end
+
+    assert_selector("li[role=\"option\"][data-autocomplete-value=\"foo\"][aria-disabled=\"true\"].ActionList-item", text: "Item") do
+      assert_selector("span.ActionList-item-visual--leading")
+    end
+  end
 end
