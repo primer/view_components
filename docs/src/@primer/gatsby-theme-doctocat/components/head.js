795639a0f1af49e7169bb7d5b103fe39edec582f
Kate Higa <16447748+khiga8@users.noreply.github.com>
2022-08-01 11:15:11 -0700

    copy JS to static docs directory and use that instead (#1248)

--- docs/src/@primer/gatsby-theme-doctocat/components/head.js
+++ docs/src/@primer/gatsby-theme-doctocat/components/head.js
@@ -18,15 +18,7 @@ function Head(props) {
     : siteMetadata.title
   const description = props.description || siteMetadata.description
 
-  let primerViewComponentsSrc
-
-  if (process.env.NODE_ENV === 'development') {
-    primerViewComponentsSrc =
-      'http://localhost:4000/assets/primer_view_components.js'
-  } else {
-    primerViewComponentsSrc =
-      'https://unpkg.com/@primer/view-components@latest/app/assets/javascripts/primer_view_components.js'
-  }
+  const primerViewComponentsSrc = "/primer_view_components.js"
 
   return (
     <Helmet>
