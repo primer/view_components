ea9635d58c82ba565ee14ae31e8cf5c318671907
Jon Rohan <yes@jonrohan.codes>
2022-08-01 20:27:12 +0000

    Updating tooltip preview labels

--- test/previews/primer/alpha/tooltip_preview.rb
+++ test/previews/primer/alpha/tooltip_preview.rb
@@ -15,6 +15,7 @@ module Primer
       end
 
       # @!group Tooltip enabled elements
+      # @label Tooltip with Primer::ButtonComponent
       def tooltip_with_button(type: :description, direction: :s, tooltip_text: "Tooltip text")
         render(Primer::ButtonComponent.new(id: "button-with-tooltip")) do |c|
           c.tooltip(text: tooltip_text, type: type, direction: direction)
@@ -22,6 +23,7 @@ module Primer
         end
       end
 
+      # @label Tooltip with Primer::LinkComponent
       def tooltip_with_link(type: :description, direction: :s, tooltip_text: "Tooltip text")
         render(Primer::LinkComponent.new(href: "#link-with-tooltip", id: "link-with-tooltip")) do |c|
           c.tooltip(text: tooltip_text, type: type, direction: direction)
@@ -29,6 +31,7 @@ module Primer
         end
       end
 
+      # @label Tooltip with Primer::IconButton
       def tooltip_with_icon_button(direction: :s, tooltip_text: "Tooltip text")
         render(Primer::IconButton.new(icon: :search, "aria-label": tooltip_text, tooltip_direction: direction))
       end
