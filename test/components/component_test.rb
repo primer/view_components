1b425f9f6f136d92a39d3d12d2fad75f90aabfdb
River Lynn Parrhesia Bailey <river.bailey@testdouble.com>
2022-08-02 15:16:21 -0500

    fixing missing comma from bad merge

--- test/components/component_test.rb
+++ test/components/component_test.rb
@@ -89,7 +89,7 @@ class PrimerComponentTest < Minitest::Test
       "Primer::Component",
       "Primer::OcticonsSymbolComponent",
       "Primer::Content",
-      "Primer::BlankslateComponent"
+      "Primer::BlankslateComponent",
       "Primer::BorderBoxComponent"
     ]
 
