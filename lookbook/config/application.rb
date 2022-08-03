9b6d6de0aa7bc540070c0ca29bed901ac73761fc
Katie Langerman <langermank@github.com>
2022-08-03 01:32:25 +0000

    add additional params

--- lookbook/config/application.rb
+++ lookbook/config/application.rb
@@ -16,6 +16,8 @@ module Lookbook
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.lookbook.experimental_features = [:pages]
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
