ea1c29d7fa0d917a729cba684bf5f8ae039bb228
Jon Rohan <yes@jonrohan.codes>
2022-08-08 13:40:07 -0700

    Updating label component to not always have large (#1283)
    * Updating label component to not always have large
    
    * Create thin-countries-happen.md
    
    * Update label_component_test.rb

---
+++ test/previews/primer/label_component_preview.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Primer
+  # @label LabelComponent
+  class LabelComponentPreview < ViewComponent::Preview
+    # @label Default Options
+    #
+    # @param size [Symbol] select [medium, large]
+    # @param tag [Symbol] select [span, summary, a, div]
+    # @param inline [Boolean] toggle
+    def default(size: :medium, tag: :span, inline: false)
+      render(Primer::LabelComponent.new(tag: tag, size: size, inline: inline)) { "Label" }
+    end
+  end
+end
