880d10616aefbd3f9ece4c21cff37c848b792b33
River Lynn Parrhesia Bailey <river.bailey@testdouble.com>
2022-08-10 09:46:21 -0500

    added missing status :beta to header

--- app/components/primer/beta/border_box/header.rb
+++ app/components/primer/beta/border_box/header.rb
@@ -7,6 +7,8 @@ module Primer
       #
       # @accessibility When using `header.with_title`, set `tag` to one of `h1`, `h2`, `h3`, etc. based on what is appropriate for the page context. <%= link_to_heading_practices %>
       class Header < Primer::Component
+        status :beta
+
         TITLE_TAG_FALLBACK = :h2
         TITLE_TAG_OPTIONS = [:h1, TITLE_TAG_FALLBACK, :h3, :h4, :h5, :h6].freeze
 
