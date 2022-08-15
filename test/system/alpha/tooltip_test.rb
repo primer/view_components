16243bccc9beaddd8384e315261131e38e348908
Kate Higa <16447748+khiga8@users.noreply.github.com>
2022-08-15 14:39:32 +0000

    Add comprehensive system tests

--- test/system/alpha/tooltip_test.rb
+++ test/system/alpha/tooltip_test.rb
@@ -8,23 +8,82 @@ module Alpha
       visit_preview(:default)
 
       assert_selector("button[id='button-with-tooltip']")
-      assert_selector("tool-tip[for='button-with-tooltip'][data-view-component][role='tooltip']", visible: false, text: "Tooltip text")
-      assert_equal(find("button")["aria-describedby"], find("tool-tip", visible: false)["id"])
-      assert_equal("position-absolute", find("tool-tip", visible: false)["class"])
+      assert_selector("tool-tip[for='button-with-tooltip'][data-view-component][role='tooltip']", text: "Tooltip text", visible: :hidden)
+      assert_equal(find("button")["aria-describedby"], find("tool-tip", visible: :hidden)["id"])
     end
 
-    def test_retains_existing_aria_labelledby
+    def test_appears_and_positions_on_focus_of_button
+      visit_preview(:default)
+
+      assert_selector("tool-tip.position-absolute.sr-only", visible: :hidden)
+      assert_equal(nil, find("tool-tip", visible: :hidden)["style"]) # position not set on initial load
+
+      find("button").send_keys("") # sends focus to button
+
+      assert_selector("tool-tip.position-absolute", visible: true)
+      refute_selector("tool-tip.position-absolute.sr-only", visible: true)
+      style = find("tool-tip", visible: true)["style"]
+      assert_equal(true, style.include?("top"))
+      assert_equal(true, style.include?("left"))
+    end
+
+    def test_appears_and_positions_on_hover_of_button
+      visit_preview(:default)
+
+      assert_equal(nil, find("tool-tip", visible: :hidden)["style"])
+      assert_selector("tool-tip.position-absolute.sr-only", visible: :hidden)
+
+      find("button").hover
+
+      assert_selector("tool-tip.position-absolute", visible: true)
+      refute_selector("tool-tip.position-absolute.sr-only", visible: true)
+      style = find("tool-tip", visible: true)["style"]
+      assert_equal(true, style.include?("top"))
+      assert_equal(true, style.include?("left"))
+    end
+
+    def test_hides_tooltip_on_escape
+      visit_preview(:default)
+
+      find("button").send_keys("") # focus
+      assert_selector("tool-tip", visible: true)
+
+      find("button").send_keys(:escape)
+      assert_selector("tool-tip", visible: :hidden)
+    end
+
+    def test_appends_to_existing_aria_labelledby
       visit_preview(:label_tooltip_on_button_with_existing_labelledby)
 
-      tooltip_id = find("tool-tip", visible: false)["id"]
+      tooltip_id = find("tool-tip", visible: :hidden)["id"]
       assert_equal("existing-label-id #{tooltip_id}", find("button")["aria-labelledby"])
     end
 
-    def test_retains_existing_aria_describedby
+    def test_appends_to_existing_aria_describedby
       visit_preview(:description_tooltip_on_button_with_existing_describedby)
 
-      tooltip_id = find("tool-tip", visible: false)["id"]
+      tooltip_id = find("tool-tip", visible: :hidden)["id"]
       assert_equal("existing-description-id #{tooltip_id}", find("button")["aria-describedby"])
+    end
+
+    def test_always_aria_hidden_when_tooltip_is_label
+      visit_preview(:label_tooltip_on_button_with_existing_labelledby)
+
+      assert_selector("tool-tip[aria-hidden='true']", visible: :hidden)
+      find("button").send_keys("") # focus
+      assert_selector("tool-tip[aria-hidden='true']", visible: :visible)
+    end
+
+    def test_never_aria_hidden_when_tooltip_is_description
+      visit_preview(:default)
+
+      assert_selector("tool-tip", visible: :hidden)
+      refute_selector("tool-tip[aria-hidden]", visible: :hidden)
+
+      find("button").send_keys("") # focus
+
+      assert_selector("tool-tip", visible: :hidden)
+      refute_selector("tool-tip[aria-hidden]", visible: :visible)
     end
   end
 end
