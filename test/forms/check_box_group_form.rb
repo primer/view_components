f8eececd3ef84ddbb88e56720014ee66fbaa00ad
Cameron Dutro <camertron@gmail.com>
2022-08-19 20:20:49 +0000

    Allow receiving results from a check box group as an array of values

--- test/forms/check_box_group_form.rb
+++ test/forms/check_box_group_form.rb
@@ -7,5 +7,11 @@ class CheckBoxGroupForm < ApplicationForm
       check_group.check_box(name: "long_i", value: "long_i", label: "Eye-ples and ba-nainais", caption: 'Long "I" sound')
       check_group.check_box(name: "long_o", value: "long_o", label: "Oh-ples and ba-nonos", caption: 'Long "O" sound')
     end
+
+    check_form.check_box_group(name: "places", label: "Cool places") do |check_group|
+      check_group.check_box(value: "lopez", label: "Lopez Island", caption: "Vacation getaway")
+      check_group.check_box(value: "bellevue", label: "Bellevue", caption: "Beautiful view")
+      check_group.check_box(value: "seattle", label: "Seattle", caption: "The emerald city")
+    end
   end
 end
