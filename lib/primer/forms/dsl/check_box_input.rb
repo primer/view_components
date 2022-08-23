41425ccf530bf189ac465a38ceb5028bf0c01cd3
Cameron Dutro <camertron@gmail.com>
2022-08-23 09:28:50 -0700

    Support submitting a check box group as an array (#1323)
    * Allow receiving results from a check box group as an array of values
    
    * Support submitting a check box group as an array
    
    * Add changeset

--- lib/primer/forms/dsl/check_box_input.rb
+++ lib/primer/forms/dsl/check_box_input.rb
@@ -5,11 +5,18 @@ module Primer
     module Dsl
       # :nodoc:
       class CheckBoxInput < Input
-        attr_reader :name, :label
+        DEFAULT_SCHEME = :boolean
+        SCHEMES = [DEFAULT_SCHEME, :array].freeze
 
-        def initialize(name:, label:, **system_arguments)
+        attr_reader :name, :label, :value, :scheme
+
+        def initialize(name:, label:, value: nil, scheme: DEFAULT_SCHEME, **system_arguments)
+          raise ArgumentError, "Check box scheme must be one of #{SCHEMES.join(', ')}" unless SCHEMES.include?(scheme)
+
           @name = name
           @label = label
+          @value = value
+          @scheme = scheme
 
           super(**system_arguments)
         end
