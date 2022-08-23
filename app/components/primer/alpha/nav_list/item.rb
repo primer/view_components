afa8b5d92172782cb2ac357dbb4b9597850685e3
Joel Hawksley <joel@hawksley.org>
2022-08-23 15:28:27 -0600

    remove reference to undefined functionality

--- app/components/primer/alpha/nav_list/item.rb
+++ app/components/primer/alpha/nav_list/item.rb
@@ -5,10 +5,6 @@ module Primer
     class NavList
       # :nodoc:
       class Item < Primer::Alpha::ActionList::Item
-        include ViewComponent::AliasedSlots
-
-        alias_renders_many :items, as: :subitems
-
         def initialize(selected_item_id: nil, selected_by_ids: [], **system_arguments)
           @selected_item_id = selected_item_id
           @selected_by_ids = Array(selected_by_ids)
