630f71c477be9626dcbd1648a7a7e1dfeb02c743
simurai <simurai@github.com>
2022-08-30 02:48:59 +0000

    Remove direction

--- app/components/primer/experimental/action_bar.rb
+++ app/components/primer/experimental/action_bar.rb
@@ -24,13 +24,6 @@ module Primer
       }.freeze
       DENSITY_OPTIONS = DENSITY_MAPPINGS.keys.freeze
 
-      DIRECTION_DEFAULT = :horizontal
-      DIRECTION_MAPPINGS = {
-        DIRECTION_DEFAULT => nil,
-        :vertical => "ActionBar--vertical"
-      }.freeze
-      DIRECTION_OPTIONS = DIRECTION_MAPPINGS.keys.freeze
-
       renders_many :items, types: {
         icon_button: lambda { |**system_arguments|
           Primer::Experimental::ActionBar::Item.new(item_type: :icon_button, **system_arguments)
@@ -45,21 +38,19 @@ module Primer
       #   <%= render(Primer::Experimental::ActionBar.new) { "Example" } %>
       #
       # @param system_arguments [Hash] <%= link_to_system_arguments_docs %>
-      def initialize(size: SIZE_DEFAULT, density: DENSITY_DEFAULT, direction: DIRECTION_DEFAULT, **system_arguments)
+      def initialize(size: SIZE_DEFAULT, density: DENSITY_DEFAULT, **system_arguments)
         @menu_id = "action-bar-overflow-menu-#{SecureRandom.hex(4)}"
         @system_arguments = system_arguments
         @system_arguments[:tag] = :"action-bar"
 
         @size = fetch_or_fallback(SIZE_OPTIONS, size, SIZE_DEFAULT)
         @density = fetch_or_fallback(DENSITY_OPTIONS, density, DENSITY_DEFAULT)
-        @direction = fetch_or_fallback(DIRECTION_OPTIONS, direction, DIRECTION_DEFAULT)
 
         @system_arguments[:classes] = class_names(
           system_arguments[:classes],
           "ActionBar",
           SIZE_MAPPINGS[@size],
-          DENSITY_MAPPINGS[@density],
-          DIRECTION_MAPPINGS[@direction]
+          DENSITY_MAPPINGS[@density]
         )
         @system_arguments[:role] = "toolbar"
       end
--- test/previews/primer/experimental/action_bar_preview.rb
+++ test/previews/primer/experimental/action_bar_preview.rb
@@ -9,10 +9,9 @@ module Primer
     class ActionBarPreview < ViewComponent::Preview
       # @param size [Symbol] select [[Small, small], [Medium, medium], [Large, large]]
       # @param density [Symbol] select [[Condensed, condensed], [Normal, normal], [Spacious, spacious]]
-      # @param direction [Symbol] select [[Horizontal, horizontal], [Vertical, vertical]]
       # @param aria_label [String]
-      def playground(size: :medium, density: :normal, direction: :horizontal)
-        render(Primer::Experimental::ActionBar.new(size: size, density: density, direction: direction)) do |component|
+      def playground(size: :medium, density: :normal)
+        render(Primer::Experimental::ActionBar.new(size: size, density: density)) do |component|
           component.with_item_icon_button(icon: :heading, "aria-label": "Heading")
           component.with_item_icon_button(icon: :bold, "aria-label": "Bold")
           component.with_item_icon_button(icon: :italic, "aria-label": "Italic")
