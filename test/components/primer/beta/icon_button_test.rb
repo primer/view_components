c02d68b163ab89ff5783b0a5d87ec13cf2188a3b
Jon Rohan <yes@jonrohan.codes>
2022-08-30 22:48:53 +0000

    Basic render test for IconButton

--- test/components/primer/beta/icon_button_test.rb
+++ test/components/primer/beta/icon_button_test.rb
@@ -6,8 +6,11 @@ class PrimerBetaIconButtonTest < Minitest::Test
   include Primer::ComponentTestHelpers
 
   def test_renders
-    render_inline(Primer::Beta::IconButton.new)
+    render_inline(Primer::Beta::IconButton.new(icon: :star, "aria-label": "Star"))
 
-    assert_text("Add a test here")
+    assert_selector(".Button.Button--iconOnly") do
+      assert_selector(".Button-visual")
+    end
+    assert_selector("tool-tip", text: "Star", visible: :all)
   end
 end
