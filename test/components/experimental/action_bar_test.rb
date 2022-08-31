2d792689ea81b19decfcb48cad7ebf1b084a404a
Jon Rohan <yes@jonrohan.codes>
2022-08-31 19:45:51 +0000

    Updating render test

--- test/components/experimental/action_bar_test.rb
+++ test/components/experimental/action_bar_test.rb
@@ -8,12 +8,14 @@ class PrimerExperimentalActionBarTest < Minitest::Test
   def test_renders
     render_inline(Primer::Experimental::ActionBar.new) do |component|
       component.with_item_icon_button(icon: :heading, "aria-label": "Heading")
+      component.with_item_divider
     end
 
     assert_selector("action-bar.ActionBar") do
-      assert_selector(".ActionBar-item[data-targets=\"action-bar.items\"]") do
+      assert_selector("button[data-targets=\"action-bar.items\"]", count: 1) do
         assert_selector("tool-tip", text: "Heading", visible: :all)
       end
+      assert_selector("hr.ActionBar-divider[data-targets=\"action-bar.items\"]", count: 1)
     end
   end
 end
