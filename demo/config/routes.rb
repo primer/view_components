31d791e07e2ed25ba42188c344d16a62f4ac6e35
Jon Rohan <yes@jonrohan.codes>
2022-09-02 02:52:01 +0000

    Fix urls for prod

--- demo/config/routes.rb
+++ demo/config/routes.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount Lookbook::Engine, at: "/lookbook"
-  get '/view-components', to: redirect('/view-components/stories/')
-  get '/', to: redirect('/lookbook/')
-
-  get '/auto_complete', to: 'auto_complete_test#index'
   resources :toggle_switch, only: [:create]
+  if Rails.env.production?
+    mount Lookbook::Engine, at: "/view-components/lookbook"
+    get '/view-components', to: redirect('/view-components/stories/')
+    get '/', to: redirect('/view-components/stories/')
+
+    get '/view-components/auto_complete', to: 'auto_complete_test#index'
+  else
+    mount Lookbook::Engine, at: "/"
+
+    get '/auto_complete', to: 'auto_complete_test#index'
+  end
 end
