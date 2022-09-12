ec7084888ecdb0aa1ed983fd163df7882dff26e2
Jon Rohan <yes@jonrohan.codes>
2022-09-12 22:04:30 +0000

    Converting MenuComponent

---
+++ test/previews/primer/menu_component_preview.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Primer
+  # @label MenuComponent
+  class MenuComponentPreview < ViewComponent::Preview
+    # @label Default Options
+    def default; end
+  end
+end
