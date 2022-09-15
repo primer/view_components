4bb23c495c7268ada21c75202f26cd3980e39b93
Jon Rohan <yes@jonrohan.codes>
2022-09-15 00:56:01 +0000

    connect to pvc css

--- demo/app/assets/config/manifest.js
+++ demo/app/assets/config/manifest.js
@@ -1,3 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link_directory ../../../node_modules .css
+//= link_directory ../../../../app/assets/styles .css
 //= link_directory ../../../../app/assets/javascripts .js
