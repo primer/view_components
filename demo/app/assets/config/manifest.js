45b33c177d8b3fbc5c431d5657ca417f7c838d45
Jon Rohan <yes@jonrohan.codes>
2022-09-15 01:37:42 +0000

    use application import

--- demo/app/assets/config/manifest.js
+++ demo/app/assets/config/manifest.js
@@ -1,5 +1,4 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link_directory ../../../node_modules .css
 //= link_directory ../../../../app/assets/styles .css
 //= link_directory ../../../../app/assets/javascripts .js
