0bc24ef6f47e25cb36bd1b35cf3585b95c0d5a81
Jon Rohan <yes@jonrohan.codes>
2022-09-16 01:54:18 +0000

    Running axe scan on written previews

--- test/system/doc_examples_axe_test.rb
+++ test/system/doc_examples_axe_test.rb
@@ -5,29 +5,28 @@ require "application_system_test_case"
 class IntegrationDocExamplesAxeTest < ApplicationSystemTestCase
   # Skip components that should be tested as part of a larger component.
   # Do not add to this list for any other reason!
-  NOT_STANDALONE = [:BetaAutoCompleteItemPreview, :NavigationTabComponentPreview, :ContentPreview].freeze
-  IGNORE = [:MarkdownPreview].freeze
+  IGNORED_PREVIEWS = %w[
+    Primer::MarkdownPreview
+  ].freeze
 
   def test_accessibility_of_doc_examples
     # Workaround to ensure that all component previews are loaded.
     visit("/rails/view_components")
 
-    raise "You must generate previews with `bundle exec rake docs:preview` before running this test" unless defined? Primer::Docs
-
     puts "\n============================================================================="
-    puts "Running axe-core checks on previews generated from documentation examples..."
+    puts "Running axe-core checks on previews..."
     puts "============================================================================="
 
     failure_urls = []
 
-    Primer::Docs.constants.each do |klass|
-      next if NOT_STANDALONE.include?(klass)
-      next if IGNORE.include?(klass)
+    components = ViewComponent::Preview.descendants
+    components.each do |klass|
+      next if IGNORED_PREVIEWS.include?(klass.to_s)
 
-      component_previews = Primer::Docs.const_get(klass).instance_methods(false)
+      component_previews = klass.instance_methods(false)
       component_uri = klass.to_s.underscore.gsub("_preview", "")
       component_previews.each do |preview|
-        visit("/rails/view_components/primer/docs/#{component_uri}/#{preview}")
+        visit("/rails/view_components/#{component_uri}/#{preview}")
         begin
           assert_accessible(page)
         rescue RuntimeError => e
