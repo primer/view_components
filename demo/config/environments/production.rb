0eac4c73255098275c4d6f302863f816d0b82a00
Jon Rohan <yes@jonrohan.codes>
2022-09-17 22:36:58 +0000

    lint fix

--- demo/config/environments/production.rb
+++ demo/config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  config.logger = Logger.new(STDOUT) if ENV["RAILS_LOG_TO_STDOUT"].present?
+  config.logger = Logger.new($stdout) if ENV["RAILS_LOG_TO_STDOUT"].present?
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
