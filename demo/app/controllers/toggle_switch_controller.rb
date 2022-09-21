5533f8e7dfffceba0e4b94bbc3bae1e17ebcff91
Cameron Dutro <camertron@gmail.com>
2022-09-21 10:05:32 -0700

    Add the Requested-With header to toggle switch requests (#1393)

--- demo/app/controllers/toggle_switch_controller.rb
+++ demo/app/controllers/toggle_switch_controller.rb
@@ -3,10 +3,22 @@
 # For toggle switch previews/tests
 # :nocov:
 class ToggleSwitchController < ApplicationController
+  class << self
+    attr_accessor :last_request
+  end
+
   skip_before_action :verify_authenticity_token
 
   def create
+    self.class.last_request = request
+
     sleep 1 unless Rails.env.test?
+
+    # this mimics dotcom behavior
+    if request.headers["HTTP_REQUESTED_WITH"] != "XMLHttpRequest"
+      head :unprocessable_entity
+      return
+    end
 
     if form_params[:authenticity_token] && form_params[:authenticity_token] != "let_me_in"
       head :unauthorized
