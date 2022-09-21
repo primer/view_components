27d823358e4abb9ba6c5c208c44aab300546c144
Jon Rohan <yes@jonrohan.codes>
2022-09-21 11:58:38 -0700

    Adding optional wrapper arguments to IconButton (#1394)
    * Adding optional wrapper arguments to IconButton
    
    * Create wicked-jokes-shave.md

--- test/components/primer/beta/icon_button_test.rb
+++ test/components/primer/beta/icon_button_test.rb
@@ -13,4 +13,10 @@ class PrimerBetaIconButtonTest < Minitest::Test
     end
     assert_selector("tool-tip", text: "Star", visible: :all)
   end
+
+  def test_adds_wrapper_arguments
+    render_inline(Primer::Beta::IconButton.new(icon: :star, "aria-label": "Star", wrapper_arguments: { id: "foo" }))
+
+    assert_selector(".Button-withTooltip#foo")
+  end
 end
