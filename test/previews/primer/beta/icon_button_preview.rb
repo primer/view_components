ae48b9fe9d7ef3a908ea9f84f5fd25cf9bf40e50
Katie Langerman <langermank@github.com>
2022-09-23 15:15:46 -0700

    Add Lookbook previews for Beta Button and IconButton (#1400)
    * add preview
    
    * add previews for button + css fixes
    
    * docs: build docs
    
    * add icon button previews
    
    * delete old icon button preview
    
    * update tooltip previews with new button
    
    * lint
    
    * lint is confused
    
    * lint..
    
    * no comment
    
    * space
    
    * Create friendly-walls-rest.md
    
    * have your link button
    
    * docs: build docs
    
    * rename
    
    * remove comment
    
    Co-authored-by: Actions Auto Build <actions@github.com>

--- test/previews/primer/beta/icon_button_preview.rb
+++ test/previews/primer/beta/icon_button_preview.rb
@@ -4,14 +4,41 @@ module Primer
   module Beta
     # @label IconButton
     class IconButtonPreview < ViewComponent::Preview
-      # @label Default
+      # @label Playground
       # @param scheme select [default, danger, invisible]
       # @param size select [small, medium, large]
       # @param aria_label text
       # @param disabled toggle
       # @param tag select [a, summary, button]
-      def default(
+      # @param icon [Symbol] octicon
+      def playground(
         scheme: :default,
+        size: :medium,
+        id: "button-preview",
+        tag: :button,
+        disabled: false,
+        icon: :plus,
+        aria_label: "Button"
+      )
+        render(Primer::Beta::IconButton.new(
+                 scheme: scheme,
+                 size: size,
+                 id: id,
+                 tag: tag,
+                 disabled: disabled,
+                 icon: icon,
+                 "aria-label": aria_label
+               )) do |_c|
+          "Button"
+        end
+      end
+
+      # @label Default
+      # @param size select [small, medium, large]
+      # @param aria_label text
+      # @param disabled toggle
+      # @param tag select [a, summary, button]
+      def default(
         size: :medium,
         id: "button-preview",
         tag: :button,
@@ -20,7 +47,59 @@ module Primer
         aria_label: "Button"
       )
         render(Primer::Beta::IconButton.new(
-                 scheme: scheme,
+                 scheme: :default,
+                 size: size,
+                 id: id,
+                 tag: tag,
+                 disabled: disabled,
+                 icon: icon,
+                 "aria-label": aria_label
+               )) do |_c|
+          "Button"
+        end
+      end
+
+      # @label Invisible
+      # @param size select [small, medium, large]
+      # @param aria_label text
+      # @param disabled toggle
+      # @param tag select [a, summary, button]
+      def invisible(
+        size: :medium,
+        id: "button-preview",
+        tag: :button,
+        disabled: false,
+        icon: :x,
+        aria_label: "Button"
+      )
+        render(Primer::Beta::IconButton.new(
+                 scheme: :invisible,
+                 size: size,
+                 id: id,
+                 tag: tag,
+                 disabled: disabled,
+                 icon: icon,
+                 "aria-label": aria_label
+               )) do |_c|
+          "Button"
+        end
+      end
+
+      # @label Danger
+      # @param size select [small, medium, large]
+      # @param aria_label text
+      # @param disabled toggle
+      # @param tag select [a, summary, button]
+      def danger(
+        size: :medium,
+        id: "button-preview",
+        tag: :button,
+        disabled: false,
+        icon: :trash,
+        aria_label: "Button"
+      )
+        render(Primer::Beta::IconButton.new(
+                 scheme: :danger,
                  size: size,
                  id: id,
                  tag: tag,
