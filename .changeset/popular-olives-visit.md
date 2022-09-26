1fec4bf5097d3f002208840694d05dd4401c7a83
Jon Rohan <yes@jonrohan.codes>
2022-09-26 13:04:43 -0700

    Raise on usage of deprecated arguments in Primer::Beta::Button (#1406)
    * Raising error when the button is using an old argument
    
    * Fixing IconButton content areas
    
    * Create popular-olives-visit.md

---
+++ .changeset/popular-olives-visit.md
@@ -0,0 +1,5 @@
+---
+"@primer/view-components": patch
+---
+
+Raise on usage of deprecated arguments in Primer::Beta::Button
--- test/components/primer/beta/button_test.rb
+++ test/components/primer/beta/button_test.rb
@@ -10,4 +10,20 @@ class PrimerBetaButtonTest < Minitest::Test
 
     assert_selector(".Button", text: "Button")
   end
+
+  def test_warns_on_uses_of_variant
+    err = assert_raises ArgumentError do
+      render_inline(Primer::Beta::Button.new(variant: :small)) { "Button" }
+    end
+
+    assert_equal "The `variant:` argument is no longer supported on Primer::Beta::Button. Consider `scheme:` or `size:`.", err.message
+  end
+
+  def test_warns_on_uses_of_dropdown
+    err = assert_raises ArgumentError do
+      render_inline(Primer::Beta::Button.new(dropdown: true)) { "Button" }
+    end
+
+    assert_equal "The `dropdown:` argument is no longer supported on Primer::Beta::Button. Use the `trailing_action` slot instead.", err.message
+  end
 end
