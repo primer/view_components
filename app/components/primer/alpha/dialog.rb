78b644f4974bc11dd53c8eb4c0271f36ed5c8b37
Keith Cirkel <keithamus@users.noreply.github.com>
2022-09-27 17:59:35 +0100

    add iconbutton mode for Alpha Dialog

--- app/components/primer/alpha/dialog.rb
+++ app/components/primer/alpha/dialog.rb
@@ -49,14 +49,25 @@ module Primer
       # Optional button to open the dialog.
       #
       # @param system_arguments [Hash] The same arguments as <%= link_to_component(Primer::ButtonComponent) %>.
-      renders_one :show_button, lambda { |**system_arguments|
-        system_arguments[:classes] = class_names(
-          system_arguments[:classes]
-        )
-        system_arguments[:id] = "dialog-show-#{@system_arguments[:id]}"
-        system_arguments["data-show-dialog-id"] = @system_arguments[:id]
-        system_arguments[:data] = (system_arguments[:data] || {}).merge({ "show-dialog-id": @system_arguments[:id] })
-        Primer::ButtonComponent.new(**system_arguments)
+      renders_one :show, types: {
+        button: lambda { |**system_arguments|
+          system_arguments[:classes] = class_names(
+            system_arguments[:classes]
+          )
+          system_arguments[:id] = "dialog-show-#{@system_arguments[:id]}"
+          system_arguments["data-show-dialog-id"] = @system_arguments[:id]
+          system_arguments[:data] = (system_arguments[:data] || {}).merge({ "show-dialog-id": @system_arguments[:id] })
+          Primer::Beta::ButtonComponent.new(**system_arguments)
+        },
+        iconbutton: lambda { |**system_arguments|
+          system_arguments[:classes] = class_names(
+            system_arguments[:classes]
+          )
+          system_arguments[:id] = "dialog-show-#{@system_arguments[:id]}"
+          system_arguments["data-show-dialog-id"] = @system_arguments[:id]
+          system_arguments[:data] = (system_arguments[:data] || {}).merge({ "show-dialog-id": @system_arguments[:id] })
+          Primer::Beta::IconButton.new(**system_arguments)
+        }
       }
 
       # Header content.
