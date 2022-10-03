f3029b4a568dec980c83bac0c13f3110ade7814a
Jon Rohan <yes@jonrohan.codes>
2022-10-03 19:26:55 +0000

    adding padding around preview

--- test/snapshots_test.rb
+++ test/snapshots_test.rb
@@ -15,7 +15,7 @@ class SnapshotsTest < ApplicationSystemTestCase
         visit("/rails/view_components/#{component_uri}/#{preview}")
         begin
           page.driver.zoom_factor = 1
-          page.save_screenshot("#{component_uri}/#{preview}.png", selector: "#component-preview")
+          page.save_screenshot("#{component_uri}/#{preview}.png", selector: "#component-preview > *")
         rescue Ferrum::BrowserError
           next
         else
