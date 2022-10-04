64c618efaa863de83656e6a832352c2e3707a743
Jon Rohan <yes@jonrohan.codes>
2022-10-04 23:21:30 +0000

    Save actions

--- test/snapshots_test.rb
+++ test/snapshots_test.rb
@@ -12,36 +12,32 @@ class SnapshotsTest < ApplicationSystemTestCase
       component_previews = klass.instance_methods(false)
       component_uri = klass.to_s.underscore.gsub("_preview", "")
       component_previews.each do |preview|
-        visit("/rails/view_components/#{component_uri}/#{preview}")
-        page.driver.zoom_factor = 1
-        page.driver.resize_window(1024, 1400)
+        page_url = "#{component_uri}/#{preview}"
 
-        puts "Saving #{component_uri}##{preview} snapshots."
+        puts "Saving #{page_url} snapshots."
 
-        save_themes(component_uri, preview)
-        page.driver.browser.refresh
-        save_actions(component_uri, preview)
+        themes = %w[
+          light
+        ]
+
+        themes.each do |theme|
+          visit("/rails/view_components/#{page_url}?theme=#{theme}")
+          page.driver.zoom_factor = 1
+          page.driver.resize_window(1024, 1400)
+
+          page.save_screenshot(
+            "#{page_url}/#{theme}/initial.png",
+            selector: "#component-preview"
+          )
+          save_actions("#{page_url}/#{theme}")
+        end
       end
     end
   end
 
   private
 
-  def save_themes(component_uri, preview)
-    themes = ["light", "dark"]
-
-    page.driver.browser.execute("document.querySelector(\"html\").setAttribute(\"data-color-mode\", \"light\")")
-
-    themes.each do |theme|
-      page.driver.browser.execute("document.querySelector(\"html\").setAttribute(\"data-light-theme\", \"#{theme}\")")
-      page.save_screenshot(
-        "#{component_uri}/#{preview}/themes/#{theme}.png",
-        selector: "#component-preview"
-      )
-    end
-  end
-
-  def save_actions(component_uri, preview)
+  def save_actions(page_url)
     # focus first element
     page.driver.browser.keyboard.type(:tab)
     element = page.driver.browser.evaluate("document.activeElement")
@@ -51,7 +47,7 @@ class SnapshotsTest < ApplicationSystemTestCase
 
     begin
       page.save_screenshot(
-        "#{component_uri}/#{preview}/actions/focus.png",
+        "#{page_url}/focus.png",
         selector: "#component-preview"
       )
 
@@ -61,7 +57,7 @@ class SnapshotsTest < ApplicationSystemTestCase
       page.driver.resize_window(1024, 375)
 
       page.driver.browser.keyboard.type(:enter)
-      page.save_screenshot("#{component_uri}/#{preview}/actions/enter.png")
+      page.save_screenshot("#{page_url}/enter.png")
     rescue Ferrum::BrowserError => e
       puts "Error: #{e.message}"
     end
