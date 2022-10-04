84e98f2d27fc52ae3d0d0de0960d9f73a56603ac
Jon Rohan <yes@jonrohan.codes>
2022-10-04 20:02:56 +0000

    Adding click action

--- test/snapshots_test.rb
+++ test/snapshots_test.rb
@@ -13,19 +13,30 @@ class SnapshotsTest < ApplicationSystemTestCase
       component_uri = klass.to_s.underscore.gsub("_preview", "")
       component_previews.each do |preview|
         visit("/rails/view_components/#{component_uri}/#{preview}")
-        begin
-          page.driver.zoom_factor = 1
-          page.driver.resize_window(1024, 1400)
-          page.save_screenshot(
-            "#{component_uri}/#{preview}.png",
-            selector: "#component-preview"
-          )
-        rescue Ferrum::BrowserError
-          next
-        else
-          puts "Saving #{component_uri}##{preview} snapshot."
-        end
+        page.driver.zoom_factor = 1
+        page.driver.resize_window(1024, 1400)
+
+        save_default(component_uri, preview)
+        save_actions(component_uri, preview)
       end
     end
+  end
+
+  private
+
+  def save_default(component_uri, preview)
+    page.save_screenshot(
+      "#{component_uri}/#{preview}.png",
+      selector: "#component-preview"
+    )
+    puts "Saving #{component_uri}##{preview} snapshot."
+  end
+
+  def save_actions(component_uri, preview)
+    page.driver.browser.css("button").each(&:click)
+    page.save_screenshot(
+      "#{component_uri}/actions/#{preview}/click.png",
+      selector: "#component-preview"
+    )
   end
 end
