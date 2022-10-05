592a7bc0364474394d8afc0f44ec493c760503ca
Jon Rohan <yes@jonrohan.codes>
2022-10-05 13:24:31 -0700

    Actions snapshots (#1449)
    * Adding click action
    
    * Saving focus and enter states
    
    * Generating component snapshots
    
    * resize window
    
    * Generating component snapshots
    
    * Adding themes
    
    * Generating component snapshots
    
    * Adding color theme attributes
    
    * Clear snapshot folder before running
    
    * Save actions
    
    * Generating component snapshots
    
    * try dark now
    
    * Generating component snapshots
    
    * Remove dark theme for followup
    
    * Generating component snapshots
    
    * Also run forms tests (#1447)
    
    * Allow NavLists to define sub-items as well as a trailing visual (#1446)
    
    * Adding theme param to render preview with theme (#1451)
    
    * Adding theme param to render preview with theme
    
    * lint and simplify logic
    
    * Generating component snapshots
    
    * Generating component snapshots
    
    Co-authored-by: jonrohan <jonrohan@users.noreply.github.com>
    Co-authored-by: Cameron Dutro <camertron@gmail.com>

--- test/snapshots_test.rb
+++ test/snapshots_test.rb
@@ -12,20 +12,54 @@ class SnapshotsTest < ApplicationSystemTestCase
       component_previews = klass.instance_methods(false)
       component_uri = klass.to_s.underscore.gsub("_preview", "")
       component_previews.each do |preview|
-        visit("/rails/view_components/#{component_uri}/#{preview}")
-        begin
+        page_url = "#{component_uri}/#{preview}"
+
+        puts "Saving #{page_url} snapshots."
+
+        themes = %w[
+          light
+        ]
+
+        themes.each do |theme|
+          visit("/rails/view_components/#{page_url}?theme=#{theme}")
           page.driver.zoom_factor = 1
           page.driver.resize_window(1024, 1400)
+
           page.save_screenshot(
-            "#{component_uri}/#{preview}.png",
+            "#{page_url}/#{theme}/initial.png",
             selector: "#component-preview"
           )
-        rescue Ferrum::BrowserError
-          next
-        else
-          puts "Saving #{component_uri}##{preview} snapshot."
+          save_actions("#{page_url}/#{theme}")
         end
       end
+    end
+  end
+
+  private
+
+  def save_actions(page_url)
+    # focus first element
+    page.driver.browser.keyboard.type(:tab)
+    element = page.driver.browser.evaluate("document.activeElement")
+
+    # When nothing is active, the element will be the body
+    return if ["body"].include? element.tag_name
+
+    begin
+      page.save_screenshot(
+        "#{page_url}/focus.png",
+        selector: "#component-preview"
+      )
+
+      # We only want to press enter on buttons
+      return unless ["button"].include? element.tag_name
+
+      page.driver.resize_window(1024, 375)
+
+      page.driver.browser.keyboard.type(:enter)
+      page.save_screenshot("#{page_url}/enter.png")
+    rescue Ferrum::BrowserError => e
+      puts "Error: #{e.message}"
     end
   end
 end
