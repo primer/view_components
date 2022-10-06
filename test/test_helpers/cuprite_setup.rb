c70dd33267bf2672a3bf3a9f445f4ca26575a392
Jon Rohan <yes@jonrohan.codes>
2022-10-06 01:05:38 +0000

    turn on slomo

--- test/test_helpers/cuprite_setup.rb
+++ test/test_helpers/cuprite_setup.rb
@@ -13,7 +13,8 @@ Capybara.register_driver(:cuprite) do |app|
       inspector: true,
       # Allow running Chrome in a headful mode by setting HEADLESS env
       # var to a falsey value
-      headless: !ENV["HEADLESS"].in?(%w[n 0 no false])
+      headless: !ENV["HEADLESS"].in?(%w[n 0 no false]),
+      slowmo: 1
     }
   )
 end
