738972e4d621f99169426bbfba6b06bfd465099e
River Lynn Parrhesia Bailey <river.bailey@testdouble.com>
2022-10-12 09:23:57 -0500

    added the utilities scss files, and got postcss to build it

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,16 @@
 const path = require('path')
+const scss = require('postcss-scss')
 
 module.exports = {
+  customSyntax: scss,
+  parser: scss,
   map: {
     annotation: false
   },
   plugins: [
     require('postcss-import'),
     require('postcss-mixins')({
-        mixinsDir: path.join(__dirname, './lib/postcss_mixins/')
+      mixinsDir: path.join(__dirname, './lib/postcss_mixins/')
     }),
     require('postcss-preset-env')({
       stage: 2,
