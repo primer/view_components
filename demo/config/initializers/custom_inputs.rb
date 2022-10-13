daffa31caf9c1b1a9b4c326bf29e89a836c0246e
Cameron Dutro <camertron@gmail.com>
2022-10-12 21:50:02 -0700

    Rescue correct error

--- demo/config/initializers/custom_inputs.rb
+++ demo/config/initializers/custom_inputs.rb
@@ -4,5 +4,9 @@ begin
   require "lookbook"
 
   Lookbook.define_param_input(:octicon, "lookbook/previews/inputs/octicon")
-rescue NameError
+rescue LoadError
+  # Happens during docs:build, which runs in the context of the
+  # PVC gem's bundle (i.e. not the demo app's bundle). Lookbook
+  # may not be available in this bundle because it targets Rails
+  # version >= 7.0.
 end
