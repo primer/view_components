b3668d3836c35e52a301544de9955b09c023e82a
Neal Lindsay <neal.lindsay@gmail.com>
2022-10-17 18:53:38 -0400

    Add nested forms for check boxes (#1498)

--- lib/primer/forms/check_box.rb
+++ lib/primer/forms/check_box.rb
@@ -17,6 +17,19 @@ module Primer
         @input.label_arguments[:value] = checked_value
       end
 
+      def nested_form_arguments
+        return @nested_form_arguments if defined?(@nested_form_arguments)
+
+        @nested_form_arguments = { **@input.nested_form_arguments }
+        @nested_form_arguments[:class] = class_names(
+          @nested_form_arguments[:class],
+          @nested_form_arguments.delete(:classes),
+          "ml-4"
+        )
+
+        @nested_form_arguments
+      end
+
       private
 
       def checked_value
