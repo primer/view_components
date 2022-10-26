303617ab57cd39b2be19c7fd3cc723465999c38c
Jon Rohan <yes@jonrohan.codes>
2022-10-26 19:18:17 +0000

    drop ts part of test

--- test/components/asset_naming_test.rb
+++ test/components/asset_naming_test.rb
@@ -14,22 +14,4 @@ class AssetNamingTest < Minitest::Test
       assert File.exist?(component_file), "CSS file #{file} does not have a corresponding component file. The file should be named to match the component."
     end
   end
-
-  # TODO: Remove this when we fix the naming of these files
-  FIX_ME = %w[
-    app/components/primer/alpha/tool_tip.ts
-    app/components/primer/alpha/modal_dialog.ts
-    app/components/primer/beta/auto_complete/auto_complete.ts
-    app/components/primer/beta/x_banner.ts
-  ].freeze
-
-  def test_js_naming_matches_component
-    Dir["app/components/**/*.ts"].each do |file|
-      next if file.include?("primer.ts") || file.include?(".d.ts") || FIX_ME.include?(file)
-
-      component_file = file.sub(".ts", ".rb")
-
-      assert File.exist?(component_file), "TS file #{file} does not have a corresponding component file. The file should be named to match the component."
-    end
-  end
 end
