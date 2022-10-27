3850161852e43583ace11386f2f3a83d9eaa8084
Jon Rohan <yes@jonrohan.codes>
2022-10-27 22:53:56 +0000

    Expand primer/css import in demo app

--- demo/app/assets/config/manifest.js
+++ demo/app/assets/config/manifest.js
@@ -1,3 +1,4 @@
 //= link_directory ../stylesheets .css
 //= link_directory ../../../../app/assets/styles .css
 //= link_directory ../../../../app/assets/javascripts .js
+//= link_directory ../../../../node_modules .css
