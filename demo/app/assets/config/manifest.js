aa64946eae7037299ab1fe15b1c9434ca85d49a2
Jon Rohan <yes@jonrohan.codes>
2022-10-27 23:28:00 +0000

    include root node_modules in case we didn't npm i

--- demo/app/assets/config/manifest.js
+++ demo/app/assets/config/manifest.js
@@ -1,4 +1,3 @@
 //= link_directory ../stylesheets .css
 //= link_directory ../../../../app/assets/styles .css
 //= link_directory ../../../../app/assets/javascripts .js
-//= link_directory ../../../../node_modules .css
