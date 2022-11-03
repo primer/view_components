fee5bf621eed9d27eba4c52a51413cb39b84cfe8
River Lynn Parrhesia Bailey <river.bailey@testdouble.com>
2022-11-03 10:30:55 -0500

    loop through all the components with previews, and test the selectors for each

--- test/components/css/css_selector_test.rb
+++ test/components/css/css_selector_test.rb
@@ -1,47 +1,61 @@
 # frozen_string_literal: true
 
 require "components/css/test_helper"
+Dir["app/components/**/*.rb"].each {|file| require_relative "../../../#{file}" }
 
 class CssSelectorTest < Minitest::Test
   include Primer::ComponentTestHelpers
   include Primer::RenderPreview
 
-  IGNORED_SELECTORS = [":root", ":before", ":hover", ":active", ":disabled", ":focus"]
+  IGNORED_SELECTORS = [/^\d/, ":is", ":root", ":before", ":after", ":hover", ":active", ":disabled", ":focus"]
 
-  def test_all_selectors_are_previewed
-    css = JSON.parse(File.read("app/components/primer/beta/button.css.json"))
-    require_relative "../../../previews/primer/beta/button_preview"
-    preview_class = Primer::Beta::ButtonPreview
+  Primer::Component.descendants.each do |component_class|
+    class_test_name = component_class.name.camelize
+    define_method("test_all_selectors_are_previewed_for_#{class_test_name}") do
+      preview_class = get_preview_class(component_class)
+      next unless preview_class
 
-    previews = preview_class.instance_methods(false)
-    filtered_selectors = filter_selectors(css["selectors"])
+      selectors = get_component_selectors(component_class)
+      previews = preview_class.instance_methods(false)
 
-    matched_selectors = []
-    previews.each do |preview|
-      preview_page = render_preview(preview, preview_klass: preview_class)
+      matched_selectors = []
+      previews.each do |preview|
+        preview_page = render_preview(preview, preview_klass: preview_class)
 
-      filtered_selectors.each do |selector|
-        result = preview_page.css(selector)
-        matched_selectors << selector unless result.empty?
+        selectors.each do |selector|
+          result = preview_page.css(selector)
+          matched_selectors << selector unless result.empty?
+        end
       end
+
+      remaining_selectors = (selectors - matched_selectors.flatten.uniq).uniq || []
+      assert_empty(remaining_selectors, no_preview_for_selectors_message(preview_class, remaining_selectors))
     end
-
-    filtered_selectors = filtered_selectors.flatten.uniq
-    matched_selectors = matched_selectors.flatten.uniq
-    remaining_selectors = (filtered_selectors - matched_selectors).uniq
-
-    assert_empty(remaining_selectors, no_preview_for_selectors_message(preview_class, remaining_selectors))
   end
 
   private
+
+  def get_preview_class(component_class)
+    name = component_class.name.gsub("Component", "")
+    prevew_name = "#{name}Preview"
+    Object.const_defined?(prevew_name) ? prevew_name.constantize : nil
+  end
+
+  def get_component_selectors(component_class)
+    css_file = Object.const_source_location(component_class.to_s)[0].gsub(".rb", ".css.json")
+    css_data = File.exist?(css_file) ? JSON.parse(File.read(css_file)) : {}
+    filter_selectors(css_data["selectors"])
+  end
 
   def no_preview_for_selectors_message(preview_class, selectors)
     "PVC Preview Class `#{preview_class.name}` does not render a preview for these selectors. Selectors without a preview may be ignored by updating `IGNORED_SELECTORS` in #{__FILE__}\n#{selectors.join("\n")}"
   end
 
   def filter_selectors(selectors)
-    selectors.select do |selector|
+    filtered = (selectors || []).select do |selector|
       !IGNORED_SELECTORS.any?{ |pattern| selector.match(pattern) }
     end
+
+    filtered.flatten.uniq
   end
 end
