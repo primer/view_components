2cb6ecaa1c6640db0b62a66b4aeddfca56c41e43
River Lynn Parrhesia Bailey <river.bailey@testdouble.com>
2022-11-03 09:44:09 -0500

    working on a bit more code organization and error message for failed test

--- test/components/css/css_selector_test.rb
+++ test/components/css/css_selector_test.rb
@@ -8,16 +8,15 @@ class CssSelectorTest < Minitest::Test
 
   IGNORED_SELECTORS = [":root", ":before", ":hover", ":active", ":disabled", ":focus"]
 
-  def test_selectors_match
+  def test_all_selectors_are_previewed
     css = JSON.parse(File.read("app/components/primer/beta/button.css.json"))
     require_relative "../../../previews/primer/beta/button_preview"
-
     preview_class = Primer::Beta::ButtonPreview
-    previews = Primer::Beta::ButtonPreview.instance_methods(false)
-    puts "Previews: #{previews}"
-    matched_selectors = []
+
+    previews = preview_class.instance_methods(false)
     filtered_selectors = filter_selectors(css["selectors"])
 
+    matched_selectors = []
     previews.each do |preview|
       preview_page = render_preview(preview, preview_klass: preview_class)
 
@@ -31,16 +30,14 @@ class CssSelectorTest < Minitest::Test
     matched_selectors = matched_selectors.flatten.uniq
     remaining_selectors = (filtered_selectors - matched_selectors).uniq
 
-    puts "unfiltered count: #{css["selectors"].count}"
-    puts "  filtered count: #{filtered_selectors.count}"
-    puts "   matched count: #{matched_selectors.count}"
-    puts " remaining count: #{remaining_selectors.count}"
-    puts "remaining selectors:"
-
-    assert_empty(remaining_selectors, "Component for #{preview_class.name} does not have previews for the following selectors: #{remaining_selectors}")
+    assert_empty(remaining_selectors, no_preview_for_selectors_message(preview_class, remaining_selectors))
   end
 
   private
+
+  def no_preview_for_selectors_message(preview_class, selectors)
+    "PVC Preview Class `#{preview_class.name}` does not render a preview for these selectors. Selectors without a preview may be ignored by updating `IGNORED_SELECTORS` in #{__FILE__}\n#{selectors.join("\n")}"
+  end
 
   def filter_selectors(selectors)
     selectors.select do |selector|
