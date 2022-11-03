5e4c78a3341212f000644c2d4f814f5fea5a9437
River Lynn Parrhesia Bailey <river.bailey@testdouble.com>
2022-11-03 14:20:42 -0500

    adding a few previews to banner, to help coverage of the selectors and options in preview

--- previews/primer/beta/banner_preview.rb
+++ previews/primer/beta/banner_preview.rb
@@ -24,6 +24,26 @@ module Primer
         render(Primer::Beta::Banner.new) { "This is a banner!" }
       end
 
+      # @label Warning
+      def as_warning
+        render(Primer::Beta::Banner.new(scheme: :warning) { "This is a warning banner!" })
+      end
+
+      # @label Success
+      def as_success
+        render(Primer::Beta::Banner.new(scheme: :success) { "This is a success banner!" })
+      end
+
+      # @label Danger
+      def as_danger
+        render(Primer::Beta::Banner.new(scheme: :danger) { "This is a danger banner!" })
+      end
+
+      # @label Full width
+      def full_width
+        render(Primer::Beta::Banner.new(full: true) { "This is a full width banner!" })
+      end
+
       # @label With action button
       #
       # @param full toggle
