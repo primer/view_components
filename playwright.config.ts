571086131b0a95366a8abd212a0a05e46ae1da27
Jon Rohan <yes@jonrohan.codes>
2022-11-09 19:20:50 +0000

    Check in screenshots

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,16 +4,10 @@ import type {PlaywrightTestConfig} from '@playwright/test'
 import {devices} from '@playwright/test'
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
  * See https://playwright.dev/docs/test-configuration.
  */
 const config: PlaywrightTestConfig = {
-  testDir: './test/playwright/',
+  testDir: path.join(__dirname, 'test', 'playwright'),
   testMatch: '**/*.test.ts',
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
@@ -35,73 +29,8 @@ const config: PlaywrightTestConfig = {
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
 
-  reporter: [
-    ['line'],
-    ['html', {open: 'never', outputFolder: path.join(__dirname, '.playwright/report')}],
-    [
-      'json',
-      {
-        outputFile: path.join(__dirname, '.playwright', 'results.json')
-      }
-    ]
-  ],
+  reporter: [['line']],
 
-  /* Configure projects for major browsers */
-  projects: [
-    {
-      name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome']
-      }
-    }
-
-    // {
-    //   name: 'firefox',
-    //   use: {
-    //     ...devices['Desktop Firefox']
-    //   }
-    // },
-
-    // {
-    //   name: 'webkit',
-    //   use: {
-    //     ...devices['Desktop Safari']
-    //   }
-    // }
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: {
-    //     ...devices['Pixel 5'],
-    //   },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: {
-    //     ...devices['iPhone 12'],
-    //   },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: {
-    //     channel: 'msedge',
-    //   },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: {
-    //     channel: 'chrome',
-    //   },
-    // },
-  ],
-
-  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
-
-  /* Run your local dev server before starting the tests */
   webServer: {
     command: 'cd demo; bin/rails s -p 4000',
     port: 4000
