3b321c109a9e470c3805c67dd8cc4ca2ece285ba
Jon Rohan <yes@jonrohan.codes>
2022-11-09 19:01:22 +0000

    Updating config folders

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line import/no-nodejs-modules
+import path from 'node:path'
 import type {PlaywrightTestConfig} from '@playwright/test'
 import {devices} from '@playwright/test'
 
@@ -11,36 +13,38 @@ import {devices} from '@playwright/test'
  * See https://playwright.dev/docs/test-configuration.
  */
 const config: PlaywrightTestConfig = {
-  testDir: './test/snapshots/',
+  testDir: './test/playwright/',
+  testMatch: '**/*.test.ts',
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
-  expect: {
-    /**
-     * Maximum time expect() should wait for the condition to be met.
-     * For example in `await expect(locator).toHaveText();`
-     */
-    timeout: 5000
-  },
+
+  // https://playwright.dev/docs/api/class-testconfig#test-config-output-dir
+  outputDir: path.join(__dirname, '.playwright', 'results'),
+  snapshotDir: path.join(__dirname, '.playwright', 'screenshots'),
+
   /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+  use: {
+    screenshot: 'only-on-failure'
+  },
+  expect: {
+    toHaveScreenshot: {
+      animations: 'disabled'
+    }
+  },
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-    actionTimeout: 0,
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry'
-  },
+  reporter: [
+    ['line'],
+    ['html', {open: 'never', outputFolder: path.join(__dirname, '.playwright/report')}],
+    [
+      'json',
+      {
+        outputFile: path.join(__dirname, '.playwright', 'results.json')
+      }
+    ]
+  ],
 
   /* Configure projects for major browsers */
   projects: [
