7fce4281d0b6762e6bbfe056b7a733b1eda38109
Jon Rohan <yes@jonrohan.codes>
2022-11-09 23:20:15 +0000

    screenshot naming updates

--- test/playwright/previews.test.ts
+++ test/playwright/previews.test.ts
@@ -1,10 +1,14 @@
 import {test, expect} from '@playwright/test'
 import {componentPreviews} from './helpers'
 
+test.beforeEach(async (page, testInfo) => {
+  testInfo.snapshotSuffix = ''
+})
+
 for (const {componentName, previewURL} of componentPreviews()) {
   test(`renders ${componentName} preview`, async ({page}) => {
     await page.goto(`/lookbook/preview/${previewURL}/default`)
     const screenshot = await page.locator('#component-preview').screenshot({animations: 'disabled'})
-    expect(screenshot).toMatchSnapshot(['previews', `${previewURL}.png`])
+    expect(screenshot).toMatchSnapshot([previewURL, 'default.png'])
   })
 }
