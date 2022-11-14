5f37bec035b9bbaecd9727fd3f1cafc75f7acdf6
simurai <simurai@github.com>
2022-11-14 02:11:45 +0000

    Switch Playground and Default Options

--- previews/primer/beta/label_preview.rb
+++ previews/primer/beta/label_preview.rb
@@ -4,21 +4,17 @@ module Primer
   module Beta
     # @label Label
     class LabelPreview < ViewComponent::Preview
-      # @label Default Options
-      #
-      # @param size [Symbol] select [medium, large]
-      # @param tag [Symbol] select [span, summary, a, div]
-      # @param inline [Boolean] toggle
-      def default(size: :medium, tag: :span, inline: false)
-        render(Primer::Beta::Label.new(tag: tag, size: size, inline: inline)) { "Label" }
-      end
-
       # @label Playground
       #
       # @param size [Symbol] select [medium, large]
       # @param tag [Symbol] select [span, summary, a, div]
       # @param inline [Boolean] toggle
       def playground(size: :medium, tag: :span, inline: false)
+        render(Primer::Beta::Label.new(tag: tag, size: size, inline: inline)) { "Label" }
+      end
+
+      # @label Default Options
+      def default
         render(Primer::Beta::Label.new(tag: tag, size: size, inline: inline)) { "Label" }
       end
     end
