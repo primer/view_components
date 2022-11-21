daba6bd1aa7a8e816843c007ae622059421e0b8a
Andrew Porter <andrewrporter@github.com>
2022-11-21 20:41:52 +0000

    Added lookbook entries for list

--- previews/primer/alpha/ordered_list_preview.rb
+++ previews/primer/alpha/ordered_list_preview.rb
@@ -8,19 +8,22 @@ module Primer
     # @label OrderedList
     class OrderedListPreview < ViewComponent::Preview
       # @label Playground
-      # @param string_example text
-      # @param boolean_example toggle
-      # @param email_example email
-      # @param number_example number
-      # @param url_example url
-      # @param tel_example tel
-      # @param textarea_example textarea
-      # @param select_example select [one, two, three]
-      # @param select_custom_labels select [[One label, one], [Two label, two], [Three label, three]]
-      # With empty option (`~` in YAML)
-      # @param select_empty_option select [~, one, two, three]
-      def playground(string_example: "Some value", boolean_example: false, select_example: :one)
-        render(Primer::Alpha::OrderedList.new(string_example: string_example, boolean_example: boolean_example, select_example: select_example))
+      # @param type [Symbol] select [1, a, A, i, I]
+      def playground(type: :a)
+        render(Primer::Alpha::OrderedList.new(type: type)) do |c|
+          c.with_item { "Item 1" }
+          c.with_item { "Item 2" }
+          c.with_item { "Item 3" }
+        end
+      end
+
+      # @label Default options
+      def default
+        render(Primer::Alpha::OrderedList.new) do |c|
+          c.with_item { "Item 1" }
+          c.with_item { "Item 2" }
+          c.with_item { "Item 3" }
+        end
       end
     end
   end
