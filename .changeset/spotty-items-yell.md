cdc13a18d406e9b0b2dd3bb4ba7b058c72052b84
River Lynn Bailey <river.bailey@testdouble.com>
2022-11-23 12:55:08 -0600

    Add `severity` setting to ERBLint rules (#1634)
    Co-authored-by: Cameron Dutro <camertron@gmail.com>

---
+++ .changeset/spotty-items-yell.md
@@ -0,0 +1,5 @@
+---
+'@primer/view-components': patch
+---
+
+Adding a custom erblint schema to allow `severity` in linter configuration
--- lib/primer/view_components/linters/deprecated_components_counter.rb
+++ lib/primer/view_components/linters/deprecated_components_counter.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require_relative "helpers/deprecated_components_helpers"
+require_relative "severity_schema"
+
 require "erblint-github/linters/custom_helpers"
 
 module ERBLint
@@ -10,6 +12,8 @@ module ERBLint
       include CustomHelpers
       include ERBLint::LinterRegistry
       include Helpers::DeprecatedComponentsHelpers
+
+      self.config_schema = SeveritySchema
 
       def run(processed_source)
         processed_source.ast.descendants(:erb).each do |erb_node|
@@ -21,10 +25,7 @@ module ERBLint
           deprecated_components.each do |component|
             next unless code.include?(component)
 
-            add_offense(
-              erb_node.loc,
-              message(component)
-            )
+            add_offense(erb_node.loc, message(component))
           end
         end
 
@@ -43,6 +44,16 @@ module ERBLint
             corrector.insert_before(processed_source.source_buffer.source_range, "#{offense.context}\n")
           end
         end
+      end
+
+      # this override is necessary because of the github/erblint-github `CustomHelpers`
+      # module. `counter_correct?` is provided by this module, and calls `add_offense`
+      # directly. there is no simple way to modify this without updating the gem and
+      # creating what would likely be an API that is non-standard and/or difficult to use
+      #
+      # https://github.com/github/erblint-github/blob/main/lib/erblint-github/linters/custom_helpers.rb
+      def add_offense(source_range, message, context = nil, severity = nil)
+        super(source_range, message, context, severity || @config.severity)
       end
     end
   end
