02de2cfc6119d7e9075a9d579277adcfdb716d43
River Lynn Parrhesia Bailey <river.bailey@testdouble.com>
2022-11-25 12:31:22 -0600

    re-add basic nil test for guide, and add tests to ensure components marked as deprecated are valid components

--- test/lib/deprecations_test.rb
+++ test/lib/deprecations_test.rb
@@ -15,10 +15,10 @@ class DeprecationsTest < Minitest::Test
     assert Primer::Deprecations.deprecated?(component)
     assert Primer::Deprecations.correctable?(component)
     assert_equal Primer::Deprecations.replacement(component), replacement
+    assert_nil Primer::Deprecations.guide(component)
   end
 
-  # ensure all components that has `status: :deprecated` are listed in the
-  # component deprecations configuration file
+  # ensure all components that has `status: :deprecated` are listed in the component deprecations configuration file
   Primer::Component.descendants.each do |component_class|
     class_test_name = component_class.name.downcase.gsub("::", "_")
     define_method("test_ensure_#{class_test_name}_is_properly_deprecated") do
@@ -28,9 +28,36 @@ class DeprecationsTest < Minitest::Test
     end
   end
 
+  # ensure all components listed in the `deprecations.yml` configuration are valid PVC components
+  Primer::Deprecations.deprecated_components.each do |component_name|
+    class_test_name = component_name.downcase.gsub("::", "_")
+    define_method("test_ensure_configured_deprecation_for_#{class_test_name}_correlates_to_pvc_component") do
+      has_component = Object.const_defined?(component_name)
+      assert has_component, missing_component_message(component_name)
+
+      if has_component # rubocop:disable Style/IfUnlessModifier
+        component_class = component_name.constantize
+        assert component_class <= Primer::Component, non_component_message(component_name)
+        assert component_class.deprecated?, component_not_deprecated_message(component_class)
+      end
+    end
+  end
+
   private
 
   def missing_deprecation_message(component_class)
     "PVC Component '#{component_class.name}' has a `status` of `:deprected`, but is not listed in the deprecations.yml file. Please update the deprecations configuration in `lib/primer/deprecations.yml`"
+  end
+
+  def missing_component_message(component_name)
+    "A deprecation was found in `lib/deprecations/deprecations.yml` for '#{component_name}', but this component does not exist"
+  end
+
+  def non_component_message(component_name)
+    "A deprecation was found in `lib/deprecations/deprecations.yml` for '#{component_name}', but this is not a valid `Primer::Component`"
+  end
+
+  def component_not_deprecated_message(component_class)
+    "A deprecation was found in `lib/deprecations/deprecations.yml` for '#{component_class.name}', but this component is not deprecated. Current status: #{component_class.status}"
   end
 end
