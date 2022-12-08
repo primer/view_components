6143e3803a3ed6976fb01dda8a0cf4f1e9bc3717
Cameron Dutro <camertron@gmail.com>
2022-12-08 12:02:30 -0800

    Use URL helpers for toggle switch previews

--- previews/primer/alpha/toggle_switch_preview.rb
+++ previews/primer/alpha/toggle_switch_preview.rb
@@ -6,32 +6,35 @@ module Primer
     class ToggleSwitchPreview < ViewComponent::Preview
       include ActionView::Helpers::FormTagHelper
 
+      # use send to avoid yard warning
+      send :include, Rails.application.routes.url_helpers
+
       def playground
-        render(ToggleSwitch.new(src: "/toggle_switch"))
+        render(ToggleSwitch.new(src: toggle_switch_index_path))
       end
 
       def default
-        render(ToggleSwitch.new(src: "/toggle_switch"))
+        render(ToggleSwitch.new(src: toggle_switch_index_path))
       end
 
       def checked
-        render(ToggleSwitch.new(src: "/toggle_switch", checked: true))
+        render(ToggleSwitch.new(src: toggle_switch_index_path, checked: true))
       end
 
       def disabled
-        render(ToggleSwitch.new(src: "/toggle_switch", enabled: false))
+        render(ToggleSwitch.new(src: toggle_switch_index_path, enabled: false))
       end
 
       def checked_disabled
-        render(ToggleSwitch.new(src: "/toggle_switch", checked: true, enabled: false))
+        render(ToggleSwitch.new(src: toggle_switch_index_path, checked: true, enabled: false))
       end
 
       def small
-        render(ToggleSwitch.new(src: "/toggle_switch", size: :small))
+        render(ToggleSwitch.new(src: toggle_switch_index_path, size: :small))
       end
 
       def with_status_label_position_end
-        render(ToggleSwitch.new(src: "/toggle_switch", status_label_position: :end))
+        render(ToggleSwitch.new(src: toggle_switch_index_path, status_label_position: :end))
       end
 
       def with_a_bad_src
@@ -43,11 +46,17 @@ module Primer
       end
 
       def with_csrf_token
-        render(ToggleSwitch.new(src: "/toggle_switch", csrf_token: "let_me_in"))
+        render(ToggleSwitch.new(src: toggle_switch_index_path, csrf_token: "let_me_in"))
       end
 
       def with_bad_csrf_token
-        render(ToggleSwitch.new(src: "/toggle_switch", csrf_token: "i_am_a_criminal"))
+        render(ToggleSwitch.new(src: toggle_switch_index_path, csrf_token: "i_am_a_criminal"))
+      end
+
+      private
+
+      # necessary to support URL helpers
+      def controller
       end
     end
   end
