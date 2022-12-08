80c02275a8e19a2a398dd029b2c28e22fc6cdaf5
Cameron Dutro <camertron@gmail.com>
2022-12-08 12:01:46 -0800

    Fix routes

--- app/forms/immediate_validation_form.rb
+++ app/forms/immediate_validation_form.rb
@@ -8,14 +8,14 @@ class ImmediateValidationForm < ApplicationForm
       name: :has_error,
       label: "Will have error",
       caption: "Every time this checks with the server, it returns an error",
-      auto_check_src: "/example_check/error"
+      auto_check_src: @view_context.example_check_error_path
     )
 
     validation_form.text_field(
       name: :no_error,
       label: "Will not error",
       caption: "Will not have an error when it checks the server",
-      auto_check_src: "/example_check/ok",
+      auto_check_src: @view_context.example_check_ok_path,
       validation_message: "This message will go away once you type something"
     )
 
@@ -23,7 +23,7 @@ class ImmediateValidationForm < ApplicationForm
       name: :random_error,
       label: "Random error",
       caption: "Server checks will randomly respond with errors",
-      auto_check_src: "/example_check/random"
+      auto_check_src: @view_context.example_check_random_path
     )
   end
 end
