d709bae13ad0ff907b20b0471cbcb847daf3724a
River Lynn Bailey <mxriverlynn@github.com>
2022-12-09 12:45:30 -0600

    Move `Primer::OcticonSymbolComponent` to `Primer::Alpha::OcticonSymbols` (#1696)

--- test/components/component_test.rb
+++ test/components/component_test.rb
@@ -111,6 +111,7 @@ class PrimerComponentTest < Minitest::Test
 
   def test_registered_components
     ignored_components = [
+      "Primer::OcticonSymbolsComponent",
       "Primer::OcticonComponent",
       "Primer::Markdown",
       "Primer::MenuComponent",
