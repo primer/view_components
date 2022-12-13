f54ee3f7a5cf13efafee6e0b6980f8607ced38dd
Cameron Dutro <camertron@gmail.com>
2022-12-13 11:17:13 -0800

    Add option to skip adding model scope to form inputs (#1704)

--- test/lib/primer/forms/input_test.rb
+++ test/lib/primer/forms/input_test.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "lib/test_helper"
+require_relative "models/deep_thought"
 
 class Primer::Forms::InputTest < Minitest::Test
   include Primer::ComponentTestHelpers
@@ -75,5 +76,45 @@ class Primer::Forms::InputTest < Minitest::Test
   def test_focusable
     assert @text_field.focusable?
     refute @hidden_field.focusable?
+  end
+
+  class NoModelScopeForm < ApplicationForm
+    form do |no_model_scope_form|
+      no_model_scope_form.text_field(
+        name: :ultimate_answer,
+        label: "Ultimate answer",
+        scope_name_to_model: false,
+        scope_id_to_model: false,
+        **(@id ? { id: @id } : {})
+      )
+    end
+
+    def initialize(id: nil)
+      @id = id
+    end
+  end
+
+  def test_removes_model_scope_from_name_and_id
+    model = DeepThought.new(42)
+
+    render_in_view_context do
+      primer_form_with(model: model, url: "/foo") do |f|
+        render(NoModelScopeForm.new(f))
+      end
+    end
+
+    assert_selector "input#ultimate_answer[name=ultimate_answer]"
+  end
+
+  def test_uses_given_id
+    model = DeepThought.new(42)
+
+    render_in_view_context do
+      primer_form_with(model: model, url: "/foo") do |f|
+        render(NoModelScopeForm.new(f, id: "foobar"))
+      end
+    end
+
+    assert_selector "input#foobar[name=ultimate_answer]"
   end
 end
