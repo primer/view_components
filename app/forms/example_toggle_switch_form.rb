cd9095f40c1d453ee3bcc7ef7d5c0aafb6f15979
Neal Lindsay <neal.lindsay@gmail.com>
2022-12-19 14:46:06 -0500

    Exclude ExampleToggleSwitchForm from docs
    This is an example form and should not be documented.

--- app/forms/example_toggle_switch_form.rb
+++ app/forms/example_toggle_switch_form.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# :nodoc:
 class ExampleToggleSwitchForm < Primer::Forms::ToggleSwitchForm
   def initialize(**system_arguments)
     super(name: :example_field, label: "Example", **system_arguments)
