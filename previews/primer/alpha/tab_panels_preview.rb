9ea547b8ba606cdb87865015ae2332a927e4bba2
Joel Hawksley <joel@hawksley.org>
2022-12-20 09:58:22 -0700

    fix missed Slots API changes

--- previews/primer/alpha/tab_panels_preview.rb
+++ previews/primer/alpha/tab_panels_preview.rb
@@ -11,7 +11,7 @@ module Primer
       def playground(number_of_panels: 3, align: :left)
         render(Primer::Alpha::TabPanels.new(label: "label", align: align)) do |component|
           Array.new(number_of_panels || 3) do |i|
-            component.tab(selected: i.zero?, id: "tab-#{i + 1}") do |tab|
+            component.with_tab(selected: i.zero?, id: "tab-#{i + 1}") do |tab|
               tab.with_panel { "Panel #{i + 1}" }
               tab.with_text { "Tab #{i + 1}" }
             end
@@ -26,7 +26,7 @@ module Primer
       def default(number_of_panels: 3, align: :left)
         render(Primer::Alpha::TabPanels.new(label: "label", align: align)) do |component|
           Array.new(number_of_panels || 3) do |i|
-            component.tab(selected: i.zero?, id: "tab-#{i + 1}") do |tab|
+            component.with_tab(selected: i.zero?, id: "tab-#{i + 1}") do |tab|
               tab.with_panel { "Panel #{i + 1}" }
               tab.with_text { "Tab #{i + 1}" }
             end
