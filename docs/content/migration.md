2bbe3d40c2c37a3b75103b96f293323904ff25f6
River Lynn Parrhesia Bailey <river.bailey@testdouble.com>
2022-12-20 14:00:32 -0600

    initial migration

--- docs/content/migration.md
+++ docs/content/migration.md
@@ -15,7 +15,7 @@ stable status.
 | [`State`](https://primer.style/css/components/labels#states)             | [`Primer::Beta::State`](https://primer.style/view-components/components/state)              |
 | [`breadcrumb-item`](https://primer.style/css/components/breadcrumb)      | [`Primer::Beta::Breadcrumbs`](https://primer.style/view-components/components/beta/breadcrumbs)    |
 | [`Counter`](https://primer.style/css/stickersheet/labels#counters)       | [`Primer::Beta::Counter`](https://primer.style/view-components/components/counter)          |
-| [`Subhead`](https://primer.style/css/components/subhead)                 | [`Primer::SubheadComponent`](https://primer.style/view-components/components/subhead)          |
+| [`Subhead`](https://primer.style/css/components/subhead)                 | [`Primer::Beta::Subhead`](https://primer.style/view-components/components/subhead)          |
 | [`blankslate`](https://primer.style/css/components/blankslate)           | [`Primer::Beta::Blankslate`](https://primer.style/view-components/components/beta/blankslate)    |
 
 ## Upgrading Deprecated Components
---
+++ previews/primer/beta/subhead_preview.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+module Primer
+  module Beta
+    # @label Subhead
+    class SubheadComponentPreview < ViewComponent::Preview
+      # @label Playground
+      #
+      # @param spacious [Boolean]
+      # @param hide_border [Boolean]
+      # @param heading_danger [Boolean]
+      # @param heading_tag [Symbol] select [div, h1, h2, h3, h4, h5, h6]
+      def playground(spacious: false, hide_border: false, heading_tag: :div, heading_danger: false)
+        render(Primer::Beta::Subhead.new(spacious: spacious, hide_border: hide_border)) do |component|
+          component.with_heading(tag: heading_tag, danger: heading_danger) do
+            "My Heading"
+          end
+          component.with_description do
+            "My Description"
+          end
+        end
+      end
+
+      # @label Default Options
+      #
+      # @param spacious [Boolean]
+      # @param hide_border [Boolean]
+      # @param heading_danger [Boolean]
+      # @param heading_tag [Symbol] select [div, h1, h2, h3, h4, h5, h6]
+      def default(spacious: false, hide_border: false, heading_tag: :div, heading_danger: false)
+        render(Primer::Beta::Subhead.new(spacious: spacious, hide_border: hide_border)) do |component|
+          component.with_heading(tag: heading_tag, danger: heading_danger) do
+            "My Heading"
+          end
+          component.with_description do
+            "My Description"
+          end
+        end
+      end
+    end
+  end
+end
