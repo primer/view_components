93ae24c57723c669e359c88fe9aa18dd0e2523c9
River Lynn Parrhesia Bailey <river.bailey@testdouble.com>
2022-12-20 11:37:48 -0600

    added missing previews, and adjusted selector test ignores

--- previews/primer/beta/state_preview.rb
+++ previews/primer/beta/state_preview.rb
@@ -23,6 +23,44 @@ module Primer
       def default(title: "State", scheme: :default, size: :default, tag: :span)
         render(Primer::Beta::State.new(title: title, scheme: scheme, size: size, tag: tag)) { "State" }
       end
+
+      # @!group States
+      #
+      # @label Default
+      def states_default
+        render(Primer::Beta::State.new(title: "Default")) { "Default state" }
+      end
+
+      # @label Open
+      def states_open
+        render(Primer::Beta::State.new(title: "Open", scheme: :open)) { "Open state" }
+      end
+
+      # @label Closed
+      def states_closed
+        render(Primer::Beta::State.new(title: "Closed", scheme: :closed)) { "Closed state" }
+      end
+
+      # @label Merged
+      def states_merged
+        render(Primer::Beta::State.new(title: "Merged", scheme: :merged)) { "Merged state" }
+      end
+      #
+      # @!endgroup
+
+      # @!group Sizes
+      #
+      # @label Default
+      def sizes_default
+        render(Primer::Beta::State.new(title: "Default")) { "Default size" }
+      end
+
+      # @label Small
+      def sizes_small
+        render(Primer::Beta::State.new(title: "Default", size: :small)) { "Small size" }
+      end
+      #
+      # @!endgroup
     end
   end
 end
