f96b7edd1af06170c4dea3a418630a8bd12bc092
Joel Hawksley <joel@hawksley.org>
2022-12-20 11:49:19 -0700

    fix missed Slots API changes (#1718)
    Co-authored-by: joelhawksley <joelhawksley@users.noreply.github.com>

--- previews/primer/alpha/underline_panels_preview.rb
+++ previews/primer/alpha/underline_panels_preview.rb
@@ -11,9 +11,9 @@ module Primer
       def playground(number_of_panels: 3, align: :left)
         render(Primer::Alpha::UnderlinePanels.new(label: "Test navigation", align: align)) do |component|
           Array.new(number_of_panels || 3) do |i|
-            component.tab(selected: i.zero?, id: "tab-#{i + 1}") do |tab|
-              tab.panel { "Panel #{i + 1}" }
-              tab.text { "Tab #{i + 1}" }
+            component.with_tab(selected: i.zero?, id: "tab-#{i + 1}") do |tab|
+              tab.with_panel { "Panel #{i + 1}" }
+              tab.with_text { "Tab #{i + 1}" }
             end
           end
         end
@@ -26,9 +26,9 @@ module Primer
       def default(number_of_panels: 3, align: :left)
         render(Primer::Alpha::UnderlinePanels.new(label: "Test navigation", align: align)) do |component|
           Array.new(number_of_panels || 3) do |i|
-            component.tab(selected: i.zero?, id: "tab-#{i + 1}") do |tab|
-              tab.panel { "Panel #{i + 1}" }
-              tab.text { "Tab #{i + 1}" }
+            component.with_tab(selected: i.zero?, id: "tab-#{i + 1}") do |tab|
+              tab.with_panel { "Panel #{i + 1}" }
+              tab.with_text { "Tab #{i + 1}" }
             end
           end
         end
