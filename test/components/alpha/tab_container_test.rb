bed4f6a4eb0dc532398cc79847e6a4aa38698e1f
River Lynn Parrhesia Bailey <river.bailey@testdouble.com>
2022-12-21 10:36:33 -0600

    initial migration

--- test/components/alpha/tab_container_test.rb
+++ test/components/alpha/tab_container_test.rb
@@ -2,17 +2,17 @@
 
 require "components/test_helper"
 
-class PrimerTabContainerComponentTest < Minitest::Test
+class PrimerAlphaTabContainerTest < Minitest::Test
   include Primer::ComponentTestHelpers
 
   def test_does_not_render_without_content
-    render_inline(Primer::TabContainerComponent.new)
+    render_inline(Primer::Alpha::TabContainer.new)
 
     refute_selector("tab-container")
   end
 
   def test_renders_content
-    render_inline(Primer::TabContainerComponent.new) { "content" }
+    render_inline(Primer::Alpha::TabContainer.new) { "content" }
 
     assert_selector("tab-container[data-view-component]", text: "content")
   end
