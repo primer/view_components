b3620e7bd8561efcaf6ab21c6fedc3aeaa71da2b
simurai <simurai@github.com>
2022-12-22 01:26:42 +0000

    Add buttons import back
    still used for lots of examples

--- docs/src/@primer/gatsby-theme-doctocat/components/head.js
+++ docs/src/@primer/gatsby-theme-doctocat/components/head.js
@@ -5,6 +5,7 @@ import useSiteMetadata from '@primer/gatsby-theme-doctocat/src/use-site-metadata
 import '@primer/css/dist/primitives.css'
 import '@primer/css/dist/color-modes.css'
 import '@primer/css/dist/base.css'
+import '@primer/css/dist/buttons.css' // still needed for ButtonGroup, CloseButton, Details, etc.
 import '@primer/css/dist/utilities.css'
 import '@primer/css/dist/markdown.css'
 import '../../../../static/primer_view_components.css'
