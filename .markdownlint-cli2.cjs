693fd5138cf7da9a0e66c9e19e8b9c636b97ac77
Kate Higa <khiga8@github.com>
2022-12-27 10:39:47 -0500

    Swap config files

--- .markdownlint-cli2.cjs
+++ .markdownlint-cli2.cjs
@@ -1,9 +1,12 @@
-{
-  "default": true,
+const options = require('@github/markdownlint-github').init({
   "line_length": false,
   "ol-prefix": false,
   "no-trailing-punctuation": false,
   "no-inline-html": false,
   "first-line-heading": false,
   "no-duplicate-heading": false
+})
+module.exports = {
+    config: options,
+    customRules: ["@github/markdownlint-github"],
 }
