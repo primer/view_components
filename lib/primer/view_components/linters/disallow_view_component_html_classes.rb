f1ed752f191415800071ac416047d439e25c47a9
Neal Lindsay <neal.lindsay@gmail.com>
2023-02-03 15:18:29 -0500

    Linter to disallow use of private HTML classes
    In order to preserve our ability to add, remove, and change styles for
    the HTML classes we create for Primer ViewComponents, this linter will
    complain if it sees those classes being used in ERB templates.

---
+++ lib/primer/view_components/linters/disallow_view_component_html_classes.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'better_html'
+
+require_relative "helpers/rubocop_helpers"
+
+module ERBLint
+  module Linters
+    # Finds usages of ViewComponent-reserved HTML classes
+    class DisallowViewComponentHtmlClasses < Linter
+      include LinterRegistry
+
+      RESERVED_HTML_CLASSES = %w(
+        ActionListItem-visual
+      ).freeze
+
+      def run(processed_source)
+        processed_source
+          .parser
+          .nodes_with_type(:tag)
+          .each do |node|
+            tag = BetterHtml::Tree::Tag.from_node(node)
+
+            tag.attributes["class"]&.value&.split(/\s+/)&.each do |class_name|
+              if RESERVED_HTML_CLASSES.include? class_name
+                add_offense(
+                  processed_source.to_source_range(tag.loc),
+                  "HTML class \"#{class_name}\" is reserved for Primer ViewComponents. It might disappear or have different styles in the future."
+                )
+              end
+            end
+          end
+      end
+    end
+  end
+end
