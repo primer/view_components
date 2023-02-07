e8a8a5f08ac836e79295487e3b2163db885c6598
Cameron Dutro <camertron@gmail.com>
2023-02-07 11:54:52 -0800

    Refactor the component manifest
    Also renames YARD to Yard to make Zeitwerk happy and autoloads the Yard namespace.

--- lib/primer/yard/backend.rb
+++ lib/primer/yard/backend.rb
@@ -2,26 +2,12 @@
 
 # :nocov:
 module Primer
-  module YARD
+  module Yard
     # Shared functionality for generating documentation from YARD comments.
     class Backend
       include DocsHelper
 
       private
-
-      def pretty_default_value(tag, component)
-        params = tag.object.parameters.find { |param| [tag.name.to_s, "#{tag.name}:"].include?(param[0]) }
-        default = tag.defaults&.first || params&.second
-
-        return "N/A" unless default
-
-        constant_name = "#{component.name}::#{default}"
-        constant_value = default.safe_constantize || constant_name.safe_constantize
-
-        return pretty_value(default) if constant_value.nil?
-
-        pretty_value(constant_value)
-      end
 
       def view_context
         @view_context ||= begin
