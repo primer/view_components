d485bd202b65228df629de9b1458c3d892a8c6b6
Keith Cirkel <keithamus@users.noreply.github.com>
2023-03-03 16:34:25 +0000

    remove blank line

--- test/components/primer/alpha/overlay_test.rb
+++ test/components/primer/alpha/overlay_test.rb
@@ -42,7 +42,6 @@ class PrimerAlphaOverlayTest < Minitest::Test
     assert_selector("[popovertoggletarget]")
   end
 
-
   def test_raises_on_missing_title
     error = assert_raises(ArgumentError) do
       render_inline(Primer::Alpha::Overlay.new(role: :dialog))
