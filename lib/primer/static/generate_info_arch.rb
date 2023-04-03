601ef90dfc8e1a6b864ba2eb108d084a1562e4ac
Cameron Dutro <camertron@gmail.com>
2023-04-03 14:25:15 -0700

    Fix coverage

--- lib/primer/static/generate_info_arch.rb
+++ lib/primer/static/generate_info_arch.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# :nocov:
+
 require "json"
 
 module Primer
--- lib/primer/static/generate_statuses.rb
+++ lib/primer/static/generate_statuses.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# :nocov:
 module Primer
   module Static
     # :nodoc:
