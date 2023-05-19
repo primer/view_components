8963d7c67f0355acb891290f2623889a402e49f8
Cameron Dutro <camertron@gmail.com>
2023-05-19 14:21:53 -0700

    Bump popover-polyfill to latest version (#2023)

--- test/accessibility_test.rb
+++ test/accessibility_test.rb
@@ -10,15 +10,12 @@ class AccessibilityTest < System::TestCase
   IGNORED_PREVIEWS = %w[
     Primer::Beta::MarkdownPreview
     Primer::Beta::AutoCompleteItemPreview
-    Docs::AutoCompleteItemPreview
-    Docs::BetaAutoCompleteItemPreview
-    Docs::NavigationTabPreview
-    Docs::NavigationTabComponentPreview
   ].freeze
 
   EXCLUDES = {}.freeze
 
   ViewComponent::Preview.all.each do |klass|
+    next if klass.name.start_with?("Docs::")
     next if IGNORED_PREVIEWS.include?(klass.to_s)
 
     component_previews = klass.instance_methods(false)
