0cedaa530fc9ffd6c78eb48b0cb2bebdbed515e6
Jon Rohan <yes@jonrohan.codes>
2023-06-01 21:20:03 +0000

    Fixing fallback plugin imports for multi files
    Co-authored-by: Katie Langerman <langermank@users.noreply.github.com>

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const importedJSONFromPrimitives = require('./node_modules/@primer/primitives/tokens-next-private/fallbacks/color-fallbacks.json')
+const fs = require('fs')
 
 module.exports = {
   map: {
@@ -8,27 +8,50 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-mixins')({
-        mixinsDir: path.join(__dirname, './lib/postcss_mixins/')
+      mixinsDir: path.join(__dirname, './lib/postcss_mixins/')
     }),
     require('postcss-custom-properties-fallback')({
-      importFrom: {
-        customProperties: importedJSONFromPrimitives
-      },
+      importFrom: [
+        () => {
+          const primitiveFallbacks = [
+            'color-fallbacks.json',
+            'base/size/size.json',
+            'base/typography/typography.json',
+            'functional/size/border.json',
+            'functional/size/breakpoints.json',
+            'functional/size/size-coarse.json',
+            'functional/size/size-fine.json',
+            'functional/size/size.json',
+            'functional/size/viewport.json',
+            'functional/typography/typography.json',
+          ]
+          let customProperties = {}
+          for (const filePath of primitiveFallbacks) {
+            const fileData = fs.readFileSync(path.join(__dirname, './node_modules/@primer/primitives/tokens-next-private/fallbacks/', filePath), 'utf8')
+            customProperties = {...customProperties, ...JSON.parse(fileData)}
+          }
+
+          return { customProperties: customProperties };
+        }
+      ]
     }),
     require('postcss-preset-env')({
       stage: 2,
       // https://preset-env.cssdb.org/features/#stage-2
       features: {
-        'nesting-rules': { noIsPseudoSelector: true },
+        'nesting-rules': {noIsPseudoSelector: true},
         'focus-visible-pseudo-class': false,
         'logical-properties-and-values': false,
-        'custom-media-queries':  {
+        'custom-media-queries': {
           importFrom: [
-            path.join(__dirname, './node_modules/@primer/primitives/tokens-next-private/css/functional/size/viewport.css')
+            path.join(
+              __dirname,
+              './node_modules/@primer/primitives/tokens-next-private/css/functional/size/viewport.css'
+            )
           ]
-        },
+        }
       }
     }),
     process.env.CI === 'true' ? require('cssnano') : null
-  ],
-};
+  ]
+}
