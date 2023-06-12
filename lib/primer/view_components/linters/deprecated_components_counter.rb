e1bd7a2e549595695e773cb4488cccef3fb3939e
Kate Higa <16447748+khiga8@users.noreply.github.com>
2023-06-12 09:34:00 -0400

    Clean up

--- lib/primer/view_components/linters/deprecated_components_counter.rb
+++ lib/primer/view_components/linters/deprecated_components_counter.rb
@@ -6,7 +6,7 @@ require_relative "severity_schema"
 module ERBLint
   module Linters
     # Lints against deprecated components
-    class DeprecatedComponentsCounter < Linter
+    class DeprecatedComponentsCounter < BaseLinter
       include ERBLint::LinterRegistry
       include Helpers::DeprecatedComponentsHelpers
 
@@ -22,7 +22,7 @@ module ERBLint
           deprecated_components.each do |component|
             next unless code.include?(component)
 
-            add_offense(erb_node.loc, message(component))
+            add_offense_with_severity(erb_node.loc, message(component))
           end
         end
 
@@ -62,28 +62,22 @@ module ERBLint
 
         if offenses_count.zero?
           # have to adjust to get `\n` so we delete the whole line
-          add_offense(processed_source.to_source_range(comment_node.loc.adjust(end_pos: 1)), "Unused erblint:counter comment for #{rule_name}", "") if comment_node
+          add_offense_with_severity(processed_source.to_source_range(comment_node.loc.adjust(end_pos: 1)), "Unused erblint:counter comment for #{rule_name}", "") if comment_node
           return
         end
 
         first_offense = @offenses[0]
 
         if comment_node.nil?
-          add_offense(processed_source.to_source_range(first_offense.source_range), "#{rule_name}: If you must, add <%# erblint:counter #{rule_name} #{offenses_count} %> to bypass this check.", "<%# erblint:counter #{rule_name} #{offenses_count} %>")
+          add_offense_with_severity(processed_source.to_source_range(first_offense.source_range), "#{rule_name}: If you must, add <%# erblint:counter #{rule_name} #{offenses_count} %> to bypass this check.", "<%# erblint:counter #{rule_name} #{offenses_count} %>")
         else
           clear_offenses
-          add_offense(processed_source.to_source_range(comment_node.loc), "Incorrect erblint:counter number for #{rule_name}. Expected: #{expected_count}, actual: #{offenses_count}.", "<%# erblint:counter #{rule_name} #{offenses_count} %>") if expected_count != offenses_count
+          add_offense_with_severity(processed_source.to_source_range(comment_node.loc), "Incorrect erblint:counter number for #{rule_name}. Expected: #{expected_count}, actual: #{offenses_count}.", "<%# erblint:counter #{rule_name} #{offenses_count} %>") if expected_count != offenses_count
         end
       end
 
-      # this override is necessary because of the github/erblint-github `CustomHelpers`
-      # module. `counter_correct?` is provided by this module, and calls `add_offense`
-      # directly. there is no simple way to modify this without updating the gem and
-      # creating what would likely be an API that is non-standard and/or difficult to use
-      #
-      # https://github.com/github/erblint-github/blob/main/lib/erblint-github/linters/custom_helpers.rb
-      def add_offense(source_range, message, context = nil, severity = nil)
-        super(source_range, message, context, severity || @config.severity)
+      def add_offense_with_severity(source_range, message, context = nil, severity = nil)
+        add_offense(source_range, message, context, severity || @config.severity)
       end
     end
   end
