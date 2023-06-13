23577c4db2a97f1bb3b9a2c4dff22b0c133f0eb5
Cameron Dutro <camertron@gmail.com>
2023-06-13 15:00:20 -0700

    Document the disabled: arg

--- app/components/primer/beta/details.rb
+++ app/components/primer/beta/details.rb
@@ -58,6 +58,7 @@ module Primer
       #
       # @param overlay [Symbol] Dictates the type of overlay to render with. <%= one_of(Primer::Beta::Details::OVERLAY_MAPPINGS.keys) %>
       # @param reset [Boolean] Defaults to false. If set to true, it will remove the default caret and remove style from the summary element
+      # @param disabled [Boolean] Whether or not to disable the summary button.
       # @param system_arguments [Hash] <%= link_to_system_arguments_docs %>
       def initialize(overlay: NO_OVERLAY, reset: false, disabled: false, **system_arguments)
         @system_arguments = deny_tag_argument(**system_arguments)
