a1f1beec01381e3e48378bf246cce9e510c102af
Kate Higa <16447748+khiga8@users.noreply.github.com>
2023-06-20 14:35:40 -0400

    Update test

--- test/lib/erblint/tooltipped_migration_test.rb
+++ test/lib/erblint/tooltipped_migration_test.rb
@@ -8,22 +8,30 @@ class TooltippedMigrationTest < ErblintTestCase
   end
 
   def test_warns_if_tooltipped_class_is_used_on_html_tag
-    @file ="<li aria-label='Some text' class='tooltipped'>List item</li>"
+    @file = "<li class='tooltipped'>List item</li>"
     @linter.run(processed_source)
     assert_equal 1, @linter.offenses.count
     assert_match(/.tooltipped has been deprecated./, @linter.offenses.first.message)
   end
 
   def test_warns_if_tooltipped_class_is_used_on_ruby_component
-    @file ='<%= render SomeComponent.new(classes: "tooltipped tooltipped-multiline tooltipped-s", "aria-label": "Text") do %>'
+    @file = '<%= render SomeComponent.new(classes: "tooltipped tooltipped-multiline tooltipped-s") do %>'
     @linter.run(processed_source)
     assert_equal 1, @linter.offenses.count
     assert_match(/.tooltipped has been deprecated./, @linter.offenses.first.message)
   end
 
   def test_does_not_warn_if_tooltipped_is_not_used
-    @file ='<%= render SomeComponent.new("aria-label": "Text") do %>'
+    @file = "<%= render SomeComponent.new do %>"
     @linter.run(processed_source)
+    assert_equal 0, @linter.offenses.count
+  end
+
+  def test_does_not_warn_if_inline_disable_comment
+    @file = <<~HTML
+      <%= render SomeComponent.new(classes: "tooltipped") do %><%# erblint:disable Primer::Accessibility::TooltippedMigration %>
+    HTML
+    @linter.run_and_update_offense_status(processed_source)
     assert_equal 0, @linter.offenses.count
   end
 end
