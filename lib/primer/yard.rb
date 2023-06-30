0653b64cff3e3897f89a3f8470258d8475dfaf8f
Jon Rohan <yes@jonrohan.codes>
2023-06-30 20:38:58 +0000

    Move these changes to new pr

--- lib/primer/yard.rb
+++ lib/primer/yard.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
-require "yard"
-
 module Primer
-  # :nodoc:
   module Yard
     autoload :Backend,              "primer/yard/backend"
     autoload :ComponentManifest,    "primer/yard/component_manifest"
@@ -14,7 +11,5 @@ module Primer
     autoload :Registry,             "primer/yard/registry"
     autoload :RendersManyHandler,   "primer/yard/renders_many_handler"
     autoload :RendersOneHandler,    "primer/yard/renders_one_handler"
-
-    ::YARD::Tags::Library.define_tag("Snapshot preview", :snapshot)
   end
 end
