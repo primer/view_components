19bf3fa438839a179647b123447cfd42e6d3a24b
Jon Rohan <yes@jonrohan.codes>
2023-06-30 19:59:43 +0000

    Adding tests

--- test/system/alpha/action_bar_test.rb
+++ test/system/alpha/action_bar_test.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
-require "application_system_test_case"
+require "system/test_case"
 
-class IntegrationAlphaActionBarTest < ApplicationSystemTestCase
-  def test_renders_component
+class IntegrationAlphaActionBarTest < System::TestCase
+  def test_resizing_hides_items
     visit_preview(:default)
 
-    assert_selector(".action-bar")
+    assert_selector("action-bar") do
+      assert_selector("[data-targets=\"action-bar.items\"]", count: 9)
+      refute_selector("[data-target=\"action-bar.moreMenu\"]")
+    end
+
+    page.driver.browser.resize(width: 183, height: 350)
+
+    assert_selector("action-bar") do
+      assert_selector("[data-targets=\"action-bar.items\"]", count: 4)
+      assert_selector("[data-target=\"action-bar.moreMenu\"]")
+    end
   end
 end
