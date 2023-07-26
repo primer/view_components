0a5aaccb3e100a279bb9be73c9f58331144e61ae
Mike Perrotti <mperrotti@github.com>
2023-07-26 10:41:57 -0400

    generates a system arguments page in Lookbook

--- lib/primer/yard/lookbook_pages_backend.rb
+++ lib/primer/yard/lookbook_pages_backend.rb
@@ -176,6 +176,7 @@ module Primer
         each_component do |component_ref|
           page_for(component_ref).generate
         end
+        generate_system_args_docs
       end
 
       def page_for(component_ref)
@@ -193,6 +194,29 @@ module Primer
 
       def each_component(&block)
         manifest.each(&block)
+      end
+
+      def generate_system_args_docs()
+        docs = registry.find(Primer::BaseComponent)
+
+        path = File.expand_path(
+          File.join(
+            *%w[.. .. .. previews pages system-arguments.md.erb]
+          ), __dir__
+        )
+
+        data = {
+          "description_md" => docs.base_docstring.to_s,
+          "args_md" => view_context.render(inline: docs.constructor.base_docstring)
+        }
+
+        frontmatter = {
+          "title" => "System arguments",
+          "id" => "system_arguments",
+          "data" => data
+        }
+
+        File.write(path, YAML.dump(frontmatter) + "\n---\n<%= @page.data[:description_md] %>\n<%= @page.data[:args_md] %>")
       end
     end
   end
