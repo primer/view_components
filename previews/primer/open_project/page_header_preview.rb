3daff8801a96a264a488c3c7e5cbf4cf260a7224
Oliver Günther <mail@oliverguenther.de>
2023-09-05 07:46:55 +0200

    Add OpenProject status for custom components and generate PageHeader

---
+++ previews/primer/open_project/page_header_preview.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Primer
+  module OpenProject
+    # @logical_path primer/OpenProject
+    # @component Primer::Beta::AutoComplete::Item
+    # @label Page Header
+    class PageHeaderPreview < ViewComponent::Preview
+      # @label Default
+      # @snapshot
+      def default
+        render(Primer::OpenProject::PageHeader.new(tag: :h1)) do |header|
+          header.with_title { 'Hello' }
+          header.with_description { "Last updated 5 minutes ago by XYZ." }
+        end
+      end
+
+      # @label With actions
+      def actions
+        render_with_template(locals: {})
+      end
+    end
+  end
+end
