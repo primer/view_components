93992b8a35a8c217e2e5eb58f197a7d3c2ae242e
Tyler Jones <tylerjdev@github.com>
2023-10-06 20:14:24 +0000

    Add system test cases to `action_bar`

--- test/system/alpha/action_bar_test.rb
+++ test/system/alpha/action_bar_test.rb
@@ -18,4 +18,59 @@ class IntegrationAlphaActionBarTest < System::TestCase
       assert_selector("[data-target=\"action-bar.moreMenu\"]")
     end
   end
+
+  def test_focus_set_on_first_item
+    visit_preview(:default)
+    page.driver.browser.keyboard.type(:tab)
+
+    assert_selector("action-bar") do
+      # focus should be set on the first item
+      assert_equal page.evaluate_script("document.activeElement.classList.contains('Button--iconOnly')"), true
+    end
+  end
+
+  def test_focus_set_within_overflow_menu
+    visit_preview(:default)
+
+    page.driver.browser.resize(width: 145, height: 350)
+    assert_selector("[data-targets=\"action-bar.items\"]", count: 2)
+
+    page.driver.browser.keyboard.type(:tab, :left)
+
+    assert_equal page.evaluate_script("document.activeElement.classList.contains('Button--iconOnly')"), true
+
+    # We want to ensure that we're within the ActionMenu assert_selector("action-bar") do
+    page.driver.browser.keyboard.type(:enter)
+    assert_equal page.evaluate_script("document.activeElement.classList.contains('ActionListContent')"), true
+  end
+
+  def test_escape_in_overflow_menu_sets_focus_back
+    visit_preview(:default)
+
+    page.driver.browser.resize(width: 145, height: 350)
+    page.driver.browser.keyboard.type(:tab, :left)
+
+    page.driver.browser.keyboard.type(:enter)
+    assert_equal page.evaluate_script("document.activeElement.classList.contains('ActionListContent')"), true
+
+    page.driver.browser.keyboard.type(:escape)
+    assert_equal page.evaluate_script("document.activeElement.classList.contains('Button--iconOnly')"), true
+  end
+
+  def test_click_outside_of_menu_sets_tabindex_back
+    visit_preview(:default)
+
+    page.driver.browser.resize(width: 145, height: 350)
+    page.driver.browser.keyboard.type(:tab, :left)
+
+    page.driver.browser.keyboard.type(:enter)
+    assert_equal page.evaluate_script("document.activeElement.classList.contains('ActionListContent')"), true
+
+    page.driver.browser.mouse.click(x: 0, y: 0)
+    page.driver.browser.keyboard.type(:tab, :left)
+    assert_equal page.evaluate_script("document.activeElement.classList.contains('Button--iconOnly')"), true
+
+    # Ensures that ActionMenu trigger is still focusable
+    assert_equal page.evaluate_script("document.activeElement.closest('action-menu')"), true
+  end
 end
