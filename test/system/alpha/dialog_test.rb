a108bcad1981d85a89c0ac6ac1af0c2f59fdfc91
Tyler Jones <tylerjdev@github.com>
2023-10-18 00:08:36 +0000

    Add more test cases

--- test/system/alpha/dialog_test.rb
+++ test/system/alpha/dialog_test.rb
@@ -4,6 +4,18 @@ require "system/test_case"
 
 module Alpha
   class IntegrationDialogTest < System::TestCase
+    def click_on_initial_dialog_close_button
+      find("button[data-close-dialog-id='dialog-one']").trigger("click")
+    end
+
+    def click_on_nested_dialog_close_button
+      find("button[data-close-dialog-id='dialog-two']").click
+    end
+
+    def click_on_nested_dialog_button
+      find("#dialog-show-dialog-two").click
+    end
+
     def test_modal_has_accessible_name
       visit_preview(:default)
 
@@ -19,6 +31,33 @@ module Alpha
       click_button("Show Dialog")
 
       assert_equal page.evaluate_script("document.activeElement")["aria-label"], "Close"
+    end
+
+    def test_closes_top_level_dialog
+      visit_preview(:nested_dialog)
+
+      click_button("Show Dialog")
+      click_on_nested_dialog_button
+
+      assert_equal(find("modal-dialog#dialog-two")["open"], true)
+
+      click_on_nested_dialog_close_button
+
+      assert_selector "modal-dialog#dialog-two", visible: :hidden
+      assert_selector "modal-dialog#dialog-one"
+    end
+
+    def test_closes_dialog_that_is_not_top_level
+      visit_preview(:nested_dialog)
+
+      click_button("Show Dialog")
+      click_on_nested_dialog_button
+
+      assert_equal(find("modal-dialog#dialog-two")["open"], true)
+
+      click_on_initial_dialog_close_button
+
+      assert_selector "modal-dialog#dialog-one", visible: :hidden
     end
   end
 end
