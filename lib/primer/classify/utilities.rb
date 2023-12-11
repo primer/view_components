7923e91afd4fe7498a1f84373071d6a70cf17b86
Cameron Dutro <camertron@gmail.com>
2023-12-11 14:35:33 -0800

    Always convert CSS classes to system arguments in linters

--- lib/primer/classify/utilities.rb
+++ lib/primer/classify/utilities.rb
@@ -90,10 +90,7 @@ module Primer
         end
 
         # Extract hash from classes ie. "mr-1 mb-2 foo" => { mr: 1, mb: 2, classes: "foo" }
-        def classes_to_hash(classes)
-          # This method is too slow to run in production
-          return { classes: classes } unless validate_class_names?
-
+        def classes_to_hash(classes, force: true)
           obj = {}
           classes = classes.split
           # Loop through all classes supplied and reject ones we find a match for
