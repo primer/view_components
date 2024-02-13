6d55c3e56554468996e57322adea2eafe0112f48
Katie Langerman <18661030+langermank@users.noreply.github.com>
2024-02-13 00:47:59 +0000

    remove caret

--- app/components/primer/alpha/tool_tip.ts
+++ app/components/primer/alpha/tool_tip.ts
@@ -23,7 +23,7 @@ const isPopoverOpen = (() => {
 
 const TOOLTIP_ARROW_EDGE_OFFSET = 6
 const TOOLTIP_SR_ONLY_CLASS = 'sr-only'
-const TOOLTIP_OFFSET = 10
+const TOOLTIP_OFFSET = 4
 
 type Direction = 'n' | 's' | 'e' | 'w' | 'ne' | 'se' | 'nw' | 'sw'
 
@@ -92,15 +92,6 @@ class ToolTipElement extends HTMLElement {
         text-wrap: balance;
       }
 
-      :host:before{
-        position: absolute;
-        z-index: 1000001;
-        color: var(--bgColor-emphasis, var(--color-neutral-emphasis-plus));
-        content: "";
-        border: 6px solid transparent;
-        opacity: 0;
-      }
-
       @keyframes tooltip-appear {
         from {
           opacity: 0;
@@ -108,15 +99,6 @@ class ToolTipElement extends HTMLElement {
         to {
           opacity: 1;
         }
-      }
-
-      :host:after{
-        position: absolute;
-        display: block;
-        right: 0;
-        left: 0;
-        height: 12px;
-        content: "";
       }
 
       :host(:popover-open),
@@ -127,65 +109,11 @@ class ToolTipElement extends HTMLElement {
         animation-timing-function: ease-in;
       }
 
-      :host(.\\:popover-open),
-      :host(.\\:popover-open):before {
+      :host(.\\:popover-open) {
         animation-name: tooltip-appear;
         animation-duration: .1s;
         animation-fill-mode: forwards;
         animation-timing-function: ease-in;
-        animation-delay: .4s;
-      }
-
-      :host(.tooltip-s):before,
-      :host(.tooltip-n):before {
-        right: 50%;
-        margin-right: -${TOOLTIP_ARROW_EDGE_OFFSET}px;
-      }
-      :host(.tooltip-s):before,
-      :host(.tooltip-se):before,
-      :host(.tooltip-sw):before {
-        bottom: 100%;
-        border-bottom-color: var(--bgColor-emphasis, var(--color-neutral-emphasis-plus));
-      }
-      :host(.tooltip-s):after,
-      :host(.tooltip-se):after,
-      :host(.tooltip-sw):after {
-        bottom: 100%
-      }
-      :host(.tooltip-n):before,
-      :host(.tooltip-ne):before,
-      :host(.tooltip-nw):before {
-        top: 100%;
-        border-top-color: var(--bgColor-emphasis, var(--color-neutral-emphasis-plus));
-      }
-      :host(.tooltip-n):after,
-      :host(.tooltip-ne):after,
-      :host(.tooltip-nw):after {
-        top: 100%;
-      }
-      :host(.tooltip-se):before,
-      :host(.tooltip-ne):before {
-        left: 0;
-        margin-left: ${TOOLTIP_ARROW_EDGE_OFFSET}px;
-      }
-      :host(.tooltip-sw):before,
-      :host(.tooltip-nw):before {
-        right: 0;
-        margin-right: ${TOOLTIP_ARROW_EDGE_OFFSET}px;
-      }
-      :host(.tooltip-w):before {
-        top: 50%;
-        bottom: 50%;
-        left: 100%;
-        margin-top: -6px;
-        border-left-color: var(--bgColor-emphasis, var(--color-neutral-emphasis-plus));
-      }
-      :host(.tooltip-e):before {
-        top: 50%;
-        right: 100%;
-        bottom: 50%;
-        margin-top: -6px;
-        border-right-color: var(--bgColor-emphasis, var(--color-neutral-emphasis-plus));
       }
 
       @media (forced-colors: active) {
