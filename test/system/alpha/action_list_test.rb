2ccdd3329fd1c9318c6e66dc67ff6f5970ffcd79
Cameron Dutro <camertron@gmail.com>
2024-02-21 14:38:42 -0800

    Test for visible tooltip when window shrinks

--- test/system/alpha/action_list_test.rb
+++ test/system/alpha/action_list_test.rb
@@ -8,29 +8,35 @@ module Alpha
 
     def test_js_truncate_label_shows_tooltip
       visit_preview(:long_label_with_tooltip)
+      page.driver.resize_window(200, 200)
+
       assert_selector "li.ActionListItem span.ActionListItem-label--truncate"
-      assert_selector "tool-tip[data-view-component][role='tooltip']", text: "Really really long label that may wrap, truncate, or appear as a tooltip", visible: :hidden
+      assert_selector "tool-tip", text: "Really really long label that may wrap, truncate, or appear as a tooltip", visible: :hidden
 
       find("button").send_keys("tab") # sends focus to button
-      assert_selector "tool-tip[data-view-component][role='tooltip']", text: "Really really long label that may wrap, truncate, or appear as a tooltip", visible: :hidden
+      assert_selector "tool-tip", text: "Really really long label that may wrap, truncate, or appear as a tooltip", visible: :visible
     end
 
     def test_js_truncate_label_no_tooltip
       visit_preview(:long_label_wrap)
+      page.driver.resize_window(200, 200)
+
       assert_selector "li.ActionListItem span.ActionListItem-label"
-      refute_selector "tool-tip[data-view-component][role='tooltip']", text: "Really really long label that may wrap, truncate, or appear as a tooltip", visible: :hidden
+      refute_selector "tool-tip"
 
       find("button").send_keys("tab") # sends focus to button
-      refute_selector "tool-tip[data-view-component][role='tooltip']", text: "Really really long label that may wrap, truncate, or appear as a tooltip", visible: :hidden
+      refute_selector "tool-tip"
     end
 
     def test_js_truncate_label_wraps
       visit_preview(:long_label_truncate_no_tooltip)
+      page.driver.resize_window(200, 200)
+
       assert_selector "li.ActionListItem span.ActionListItem-label--truncate"
-      refute_selector "tool-tip[data-view-component][role='tooltip']", text: "Really really long label that may wrap, truncate, or appear as a tooltip", visible: :hidden
+      refute_selector "tool-tip"
 
       find("button").send_keys("tab") # sends focus to button
-      refute_selector "tool-tip[data-view-component][role='tooltip']", text: "Really really long label that may wrap, truncate, or appear as a tooltip", visible: :hidden
+      refute_selector "tool-tip"
     end
   end
 end
