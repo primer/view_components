a42532e86c16e479d28d8cb6c28e1c6efbc3a3dd
Keith Cirkel <keithamus@users.noreply.github.com>
2024-03-28 13:14:50 +0000

    add more underline previews, snapshot tests (#2736)

--- previews/primer/alpha/underline_panels_preview.rb
+++ previews/primer/alpha/underline_panels_preview.rb
@@ -21,6 +21,7 @@ module Primer
 
       # @label Default options
       #
+      # @snapshot
       # @param number_of_panels [Integer] number
       # @param align [Symbol] select [left, right]
       def default(number_of_panels: 3, align: :left)
@@ -30,6 +31,43 @@ module Primer
               tab.with_panel { "Panel #{i + 1}" }
               tab.with_text { "Tab #{i + 1}" }
             end
+          end
+        end
+      end
+
+      # @label With Icons and Counters
+      #
+      # @snapshot
+      # @param number_of_panels [Integer] number
+      # @param align [Symbol] select [left, right]
+      def with_icons_and_counters(number_of_panels: 3, align: :left)
+        render(Primer::Alpha::UnderlinePanels.new(label: "Test navigation", align: align)) do |component|
+          Array.new(number_of_panels || 3) do |i|
+            component.with_tab(selected: i.zero?, id: "tab-#{i + 1}") do |tab|
+              tab.with_panel { "Panel #{i + 1}" }
+              tab.with_text { "Tab #{i + 1}" }
+              tab.with_icon(icon: :star)
+              tab.with_counter(count: (i + 1) * 5)
+            end
+          end
+        end
+      end
+
+      # @label With Actions
+      #
+      # @snapshot
+      # @param number_of_panels [Integer] number
+      # @param align [Symbol] select [left, right]
+      def with_actions(number_of_panels: 3, align: :right)
+        render(Primer::Alpha::UnderlinePanels.new(label: "Test navigation", align: align)) do |component|
+          Array.new(number_of_panels || 3) do |i|
+            component.with_tab(selected: i.zero?, id: "tab-#{i + 1}") do |tab|
+              tab.with_panel { "Panel #{i + 1}" }
+              tab.with_text { "Tab #{i + 1}" }
+            end
+          end
+          component.with_actions do
+            "Actions Content"
           end
         end
       end
