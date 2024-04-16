a4ab1393ef25f4f6f1f0a9bf3ef6e32cb301c5ec
Henriette Darge <h.darge@openproject.com>
2024-04-16 13:19:13 +0200

    Make the mobile button use the smaller variant

--- app/components/primer/open_project/page_header.rb
+++ app/components/primer/open_project/page_header.rb
@@ -47,6 +47,7 @@ module Primer
       # Optional description below the title row
       renders_one :description, lambda { |**system_arguments|
         deny_tag_argument(**system_arguments)
+
         system_arguments[:tag] = :div
         system_arguments[:classes] = class_names(system_arguments[:classes], "PageHeader-description")
 
@@ -59,21 +60,27 @@ module Primer
       renders_many :actions, types: {
         icon_button: lambda { |icon:, mobile_icon:, label:, scheme: DEFAULT_ACTION_SCHEME, **system_arguments|
           deny_tag_argument(**system_arguments)
-          system_arguments = set_action_arguments(system_arguments, scheme: scheme)
+
+          system_arguments = set_action_arguments(system_arguments, scheme: scheme, button_action: true)
+
           add_option_to_mobile_menu(system_arguments, mobile_icon, label, scheme)
 
           Primer::Beta::IconButton.new(icon: icon, "aria-label": label, **system_arguments)
         },
         button: lambda { |mobile_icon:, mobile_label:, scheme: DEFAULT_ACTION_SCHEME, **system_arguments|
           deny_tag_argument(**system_arguments)
-          system_arguments = set_action_arguments(system_arguments, scheme: scheme)
+
+          system_arguments = set_action_arguments(system_arguments, scheme: scheme, button_action: true)
+
           add_option_to_mobile_menu(system_arguments, mobile_icon, mobile_label, scheme)
 
           Primer::Beta::Button.new(**system_arguments)
         },
         zen_mode_button: lambda { |mobile_icon: Primer::OpenProject::ZenModeButton::ZEN_MODE_BUTTON_ICON, mobile_label: Primer::OpenProject::ZenModeButton::ZEN_MODE_BUTTON_LABEL, **system_arguments|
           deny_tag_argument(**system_arguments)
-          system_arguments = set_action_arguments(system_arguments, scheme: DEFAULT_ACTION_SCHEME)
+
+          system_arguments = set_action_arguments(system_arguments, scheme: DEFAULT_ACTION_SCHEME, button_action: true)
+
           add_option_to_mobile_menu(system_arguments, mobile_icon, mobile_label, DEFAULT_ACTION_SCHEME)
 
           Primer::OpenProject::ZenModeButton.new(**system_arguments)
@@ -81,9 +88,10 @@ module Primer
 
         link: lambda { |mobile_icon:, mobile_label:, scheme: DEFAULT_ACTION_SCHEME, **system_arguments|
           deny_tag_argument(**system_arguments)
-          system_arguments[:target] ||= "_top"
 
+          system_arguments[:target] ||= "_top"
           system_arguments = set_action_arguments(system_arguments, scheme: scheme)
+
           add_option_to_mobile_menu(system_arguments, mobile_icon, mobile_label, scheme)
 
           Primer::Beta::Link.new(**system_arguments)
@@ -91,9 +99,7 @@ module Primer
         # Should only be used rarely on a per-need basis
         text: lambda { |**system_arguments|
           system_arguments = set_action_arguments(system_arguments)
-
           system_arguments[:color] ||= :muted
-
           # Enforce that texts are hidden on mobile
           system_arguments[:display] = [:none, :flex]
 
@@ -102,7 +108,11 @@ module Primer
         menu: {
           renders: lambda { |**system_arguments, &block|
             deny_tag_argument(**system_arguments)
+
             system_arguments[:menu_arguments] = set_action_arguments(system_arguments[:menu_arguments])
+            system_arguments[:button_arguments] ||= {}
+            system_arguments[:button_arguments][:data] ||= {}
+            system_arguments[:button_arguments][:data][:targets] = "page-header.actionItems"
 
             # Add the options individually to the mobile menu in the template
             @desktop_menu_block = block
@@ -115,9 +125,10 @@ module Primer
             deny_tag_argument(**system_arguments)
 
             # The id will be automatically calculated for the trigger button, so we have to behave the same, for the mobile click to work
+            system_arguments[:button_arguments] ||= {}
             system_arguments[:button_arguments][:id] = "dialog-show-#{system_arguments[:dialog_arguments][:id]}"
+            system_arguments[:button_arguments] = set_action_arguments(system_arguments[:button_arguments], button_action: true)
 
-            system_arguments[:button_arguments] = set_action_arguments(system_arguments[:button_arguments])
             add_option_to_mobile_menu(system_arguments[:button_arguments], mobile_icon, mobile_label, :default)
 
             Primer::OpenProject::PageHeader::Dialog.new(**system_arguments)
@@ -180,7 +191,7 @@ module Primer
 
             if item.is_a?(String)
               # Unless it is defined differently, make the last item of the breadcrumb bold
-              font_weight = item.include?('<b>') || item.include?('text-bold') ? :normal : :bold
+              font_weight = item.include?("<b>") || item.include?("text-bold") ? :normal : :bold
               breadcrumbs.with_item(href: "#", font_weight: font_weight) { item }
             else
               breadcrumbs.with_item(href: item[:href], target: "_top") { item[:text] }
@@ -230,7 +241,7 @@ module Primer
 
       private
 
-      def set_action_arguments(system_arguments, scheme: nil)
+      def set_action_arguments(system_arguments, scheme: nil, button_action: false)
         system_arguments[:ml] ||= 2
         system_arguments[:display] = [:none, :flex]
         system_arguments[:scheme] = scheme unless scheme.nil?
@@ -238,6 +249,10 @@ module Primer
           system_arguments[:classes],
           "PageHeader-action",
         )
+        if button_action
+          system_arguments[:data] ||= {}
+          system_arguments[:data][:targets] = "page-header.actionItems"
+        end
 
         system_arguments[:id] ||= self.class.generate_id
         system_arguments
--- app/components/primer/open_project/page_header_element.ts
+++ app/components/primer/open_project/page_header_element.ts
@@ -1,7 +1,22 @@
-import {controller} from '@github/catalyst'
+import {controller, targets} from '@github/catalyst'
 
 @controller
 class PageHeaderElement extends HTMLElement {
+  @targets actionItems: HTMLElement[]
+
+  connectedCallback() {
+    for (const item of this.actionItems) {
+      /*
+      If there is only one action to be shown, we show that instead of the mobile action menu. However, the buttons should be the smaller button variant.
+      Unfortunately, the `size` attribute does not support responsive attributes and the .pcss syntax does not support inheritance between classes.
+      So we have to add the class manually here.
+      */
+      if (window.innerWidth <= 544) {
+        item.classList.add('Button--small')
+      }
+    }
+  }
+
   menuItemClick(event: Event) {
     const currentTarget = event.currentTarget as HTMLButtonElement
 
