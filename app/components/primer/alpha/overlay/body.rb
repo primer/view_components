500448dae45be941563ac213cd548eec064fa462
Jon Rohan <yes@jonrohan.codes>
2024-04-29 20:56:30 +0000

    Revert "Add multiple body options to Overlay body slot (#2781)"
    This reverts commit 2972a8f667c30fb84af59d9dad53cf217bfca756.

--- app/components/primer/alpha/overlay/body.rb
+++ app/components/primer/alpha/overlay/body.rb
@@ -6,12 +6,8 @@ module Primer
       # A `Overlay::Body` is a compositional component, used to render the
       # Body of an overlay. See <%= link_to_component(Primer::Alpha::Overlay) %>.
       class Body < Primer::Component
-
-        attr_reader :tab_arguments, :tab_label
-
-        # @param tab_arguments [Hash] Arguments passed to TabPanels component
         # @param system_arguments [Hash] <%= link_to_system_arguments_docs %>
-        def initialize(padding: DEFAULT_PADDING, tab_label: nil, tab_arguments: {}, **system_arguments)
+        def initialize(padding: DEFAULT_PADDING, **system_arguments)
           @system_arguments = deny_tag_argument(**system_arguments)
           @system_arguments[:tag] = :div
           @system_arguments[:classes] = class_names(
@@ -19,8 +15,6 @@ module Primer
             PADDING_MAPPINGS[fetch_or_fallback(PADDING_OPTIONS, padding, DEFAULT_PADDING)],
             system_arguments[:classes]
           )
-          @tab_arguments = tab_arguments
-          @tab_label = tab_label
         end
 
         def call
