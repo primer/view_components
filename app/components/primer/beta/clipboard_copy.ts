dc6d2c737b358183c7cb9d75e6d08894782267f5
Lindsey Wild <35239154+lindseywild@users.noreply.github.com>
2024-05-14 18:58:39 +0000

    Adds live region to clipboard copy

--- app/components/primer/beta/clipboard_copy.ts
+++ app/components/primer/beta/clipboard_copy.ts
@@ -37,18 +37,35 @@ document.addEventListener('clipboard-copy', ({target}) => {
   if (!target.hasAttribute('data-view-component')) return
 
   const currentTimeout = clipboardCopyElementTimers.get(target)
+  const clipboardCopyLiveRegion = document.querySelector<HTMLDivElement>('[data-clipboard-copy-live-region]')
+  const copiedAnnouncement = 'Copied!'
 
   if (currentTimeout) {
     clearTimeout(currentTimeout)
     clipboardCopyElementTimers.delete(target)
   } else {
     showCheck(target)
+    if (clipboardCopyLiveRegion) {
+      if (clipboardCopyLiveRegion.textContent === copiedAnnouncement) {
+        /* This is a hack due to the way the aria live API works.
+    A screen reader will not read a live region again
+    if the text is the same. Adding a space character tells
+    the browser that the live region has updated,
+    which will cause it to read again, but with no audible difference. */
+        clipboardCopyLiveRegion.textContent = `${copiedAnnouncement}\u00A0`
+      } else {
+        clipboardCopyLiveRegion.textContent = copiedAnnouncement
+      }
+    }
   }
 
   clipboardCopyElementTimers.set(
     target,
     setTimeout(() => {
       showCopy(target)
+      // if (clipboardCopyLiveRegion) {
+      //   clipboardCopyLiveRegion.textContent = ''
+      // }
       clipboardCopyElementTimers.delete(target)
     }, CLIPBOARD_COPY_TIMER_DURATION),
   )
