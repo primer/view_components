b0096785e32246c5583208212d26897b612ad677
Lindsey Wild <35239154+lindseywild@users.noreply.github.com>
2024-05-16 15:07:12 +0000

    Moves click test

--- test/system/beta/clipboard_copy_button_test.rb
+++ test/system/beta/clipboard_copy_button_test.rb
@@ -6,16 +6,6 @@ module Beta
   class IntegrationClipboardCopyButtonTest < System::TestCase
     include Primer::ClipboardTestHelpers
 
-    def test_copies_text
-      visit_preview(:playground)
-
-      clipboard_text = capture_clipboard do
-        find("#clipboard-button").click
-      end
-
-      assert_equal "Text to copy", clipboard_text
-    end
-
     def test_populates_live_region
       visit_preview(:playground)
 
@@ -24,6 +14,16 @@ module Beta
       assert_selector("[data-clipboard-copy-feedback]") do |node|
         assert_equal(node.text.strip, "Copied!")
       end
+    end
+
+    def test_copies_text
+      visit_preview(:playground)
+
+      clipboard_text = capture_clipboard do
+        find("#clipboard-button").click
+      end
+
+      assert_equal "Text to copy", clipboard_text
     end
 
     def test_includes_tooltip
