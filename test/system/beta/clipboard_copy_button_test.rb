6121eeb564a905b9628f41718cc2d93bfc55772c
Lindsey Wild <35239154+lindseywild@users.noreply.github.com>
2024-05-16 10:27:10 -0400

    Apply suggestions from code review
    Co-authored-by: Cameron Dutro <camertron@gmail.com>

--- test/system/beta/clipboard_copy_button_test.rb
+++ test/system/beta/clipboard_copy_button_test.rb
@@ -19,9 +19,7 @@ module Beta
     def test_populates_live_region
       visit_preview(:playground)
 
-      clipboard_text = capture_clipboard do
-        find("#clipboard-button").click
-      end
+      find("#clipboard-button").click
 
       assert_selector("[data-clipboard-copy-feedback]") do |node|
         assert_equal(node.text.strip, "Copied!")
