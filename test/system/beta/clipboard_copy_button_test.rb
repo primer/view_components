3eccd4e293658ea846fa97659c76861d4f3404ea
Lindsey Wild <35239154+lindseywild@users.noreply.github.com>
2024-05-16 17:21:36 +0000

    Add live region to announce "Copied!" on ClipboardCopy component (#2843)
    Co-authored-by: Cameron Dutro <camertron@gmail.com>

--- test/system/beta/clipboard_copy_button_test.rb
+++ test/system/beta/clipboard_copy_button_test.rb
@@ -9,17 +9,20 @@ module Beta
     def test_copies_text
       visit_preview(:playground)
 
+      assert_selector("[data-clipboard-copy-feedback]", text: "", visible: :false)
+
       clipboard_text = capture_clipboard do
         find("#clipboard-button").click
       end
 
       assert_equal "Text to copy", clipboard_text
+      assert_selector("[data-clipboard-copy-feedback]", text: "Copied!", visible: :false)
     end
 
     def test_includes_tooltip
       visit_preview(:with_tooltip)
 
-      assert_selector ".Button + tool-tip", visible: :all
+      assert_selector "tool-tip[for='clipboard-button']", visible: :all
     end
   end
 end
