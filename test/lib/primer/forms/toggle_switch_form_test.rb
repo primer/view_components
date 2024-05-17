105c86ea587008802430b34ae5d97a5d81c14822
Cameron Dutro <camertron@gmail.com>
2024-05-17 11:50:55 -0700

    Fix linting and test failures

--- test/lib/primer/forms/toggle_switch_form_test.rb
+++ test/lib/primer/forms/toggle_switch_form_test.rb
@@ -5,12 +5,17 @@ require "lib/test_helper"
 class Primer::Forms::ToggleSwitchFormTest < Minitest::Test
   include Primer::ComponentTestHelpers
 
-  def test_it_renders_with_a_name
+  def test_it_renders
+    render_inline(ExampleToggleSwitchForm.new(src: "/toggle_switch"))
+
+    assert_selector "toggle-switch[src='/toggle_switch']"
+    assert_selector "em", text: "favorite"
+  end
+
+  def test_accepts_custom_csrf_token
     bogus_csrf = "let me in"
     render_inline(ExampleToggleSwitchForm.new(csrf: bogus_csrf, src: "/toggle_switch"))
-
-    assert_selector "toggle-switch[src='/toggle_switch'][csrf='#{bogus_csrf}']"
-    assert_selector "em", text: "favorite"
+    assert_selector "toggle-switch[data-csrf='#{bogus_csrf}']"
   end
 
   def test_can_render_without_subclass
