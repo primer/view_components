4375234320c136b5babe569e954a19387a666b7d
Cameron Dutro <camertron@gmail.com>
2024-06-25 10:40:01 -0700

    Revert allocation counts (I think redefining the class was messing with things)

--- test/performance/bench_utilities.rb
+++ test/performance/bench_utilities.rb
@@ -9,13 +9,13 @@ class BenchUtilities < Minitest::Benchmark
   include Primer::AssertAllocationsHelper
 
   def bench_allocations_supported_selector
-    assert_allocations "3.0" => 4, "3.1" => 6, "3.2" => 6, "3.3" => 6 do
+    assert_allocations "3.0" => 4, "3.1" => 6, "3.2" => 4, "3.3" => 6 do
       Primer::Classify::Utilities.supported_selector?("m-1")
     end
   end
 
   def bench_allocations_non_supported_selector
-    assert_allocations "3.0" => 2, "3.1" => 0, "3.2" => 0, "3.3" => 0 do
+    assert_allocations "3.0" => 2, "3.1" => 0, "3.2" => 2, "3.3" => 0 do
       Primer::Classify::Utilities.supported_selector?("foo")
     end
   end
