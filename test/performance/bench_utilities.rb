8c66a7a4798d9c65b4a62020ec3545b4c7e0080f
Cameron Dutro <camertron@gmail.com>
2024-06-25 10:33:31 -0700

    Adjust allocations; rename test class

--- test/performance/bench_utilities.rb
+++ test/performance/bench_utilities.rb
@@ -5,17 +5,17 @@ require "minitest/benchmark"
 require "test_helper"
 require "test_helpers/assert_allocations_helper"
 
-class BenchClassify < Minitest::Benchmark
+class BenchUtilities < Minitest::Benchmark
   include Primer::AssertAllocationsHelper
 
   def bench_allocations_supported_selector
-    assert_allocations "3.0" => 4, "3.1" => 6, "3.2" => 4, "3.3" => 6 do
+    assert_allocations "3.0" => 4, "3.1" => 6, "3.2" => 6, "3.3" => 6 do
       Primer::Classify::Utilities.supported_selector?("m-1")
     end
   end
 
   def bench_allocations_non_supported_selector
-    assert_allocations "3.0" => 2, "3.1" => 0, "3.2" => 2, "3.3" => 0 do
+    assert_allocations "3.0" => 2, "3.1" => 0, "3.2" => 0, "3.3" => 0 do
       Primer::Classify::Utilities.supported_selector?("foo")
     end
   end
