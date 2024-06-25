e919482d0ebe2c5d8e74bd461979518d92921b76
Cameron Dutro <camertron@gmail.com>
2024-06-25 10:58:12 -0700

    Warm up first

--- test/performance/bench_utilities.rb
+++ test/performance/bench_utilities.rb
@@ -9,12 +9,18 @@ class BenchUtilities < Minitest::Benchmark
   include Primer::AssertAllocationsHelper
 
   def bench_allocations_supported_selector
-    assert_allocations "3.0" => 6, "3.1" => 6, "3.2" => 6, "3.3" => 6 do
+    # warm up
+    Primer::Classify::Utilities.supported_selector?("m-1")
+
+    assert_allocations "3.0" => 4, "3.1" => 4, "3.2" => 4, "3.3" => 4 do
       Primer::Classify::Utilities.supported_selector?("m-1")
     end
   end
 
   def bench_allocations_non_supported_selector
+    # warm up
+    Primer::Classify::Utilities.supported_selector?("foo")
+
     assert_allocations "3.0" => 0, "3.1" => 0, "3.2" => 0, "3.3" => 0 do
       Primer::Classify::Utilities.supported_selector?("foo")
     end
