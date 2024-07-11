0546589cc3eae600bebfa6da858d7136788566d9
Cameron Dutro <camertron@gmail.com>
2024-07-11 16:05:28 -0700

    Add playground preview

--- previews/primer/alpha/select_panel_preview.rb
+++ previews/primer/alpha/select_panel_preview.rb
@@ -6,7 +6,48 @@ module Primer
     class SelectPanelPreview < ViewComponent::Preview
       # @label Playground
       #
-      def playground(open_on_load: false)
+      # @param title text
+      # @param size [Symbol] select [auto, small, medium, medium_portrait, large, xlarge]
+      # @param simulate_failure toggle
+      # @param simulate_no_results toggle
+      # @param no_results_label text
+      # @param dynamic_label toggle
+      # @param dynamic_label_prefix text
+      # @param dynamic_aria_label_prefix text
+      # @param show_filter toggle
+      # @param open_on_load toggle
+      # @param anchor_align [Symbol] select [start, center, end]
+      # @param anchor_side [Symbol] select [outside_bottom, outside_top, outside_left, outside_right]
+      def playground(
+        title: "Sci-fi equipment",
+        size: :auto,
+        simulate_failure: false,
+        simulate_no_results: false,
+        no_results_label: "No results found",
+        dynamic_label: false,
+        dynamic_label_prefix: nil,
+        dynamic_aria_label_prefix: nil,
+        show_filter: true,
+        open_on_load: false,
+        anchor_align: :start,
+        anchor_side: :outside_bottom
+      )
+        render_with_template(locals: {
+          system_arguments: {
+            title: title,
+            size: size,
+            simulate_failure: simulate_failure,
+            simulate_no_results: simulate_no_results,
+            no_results_label: no_results_label,
+            dynamic_label: dynamic_label,
+            dynamic_label_prefix: dynamic_label_prefix,
+            dynamic_aria_label_prefix: dynamic_aria_label_prefix,
+            show_filter: show_filter,
+            open_on_load: open_on_load,
+            anchor_align: anchor_align,
+            anchor_side: anchor_side
+          }
+        })
       end
 
       # @label Default
