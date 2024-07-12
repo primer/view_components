1f213ffdd038ca75d5b277fbc5ac93c5ba449c31
Cameron Dutro <camertron@gmail.com>
2024-07-11 21:15:00 -0700

    Add additional test to improve coverage

--- test/components/alpha/select_panel_test.rb
+++ test/components/alpha/select_panel_test.rb
@@ -16,6 +16,16 @@ module Primer
           end
         end
       end
+
+      # This test is only here for coverage purposes and should be refactored
+      # when a full test suite is implemented.
+      def test_adds_src_attribute
+        render_preview(:remote_fetch)
+
+        assert_selector("select-panel") do
+          assert_selector("remote-input[src]")
+        end
+      end
     end
   end
 end
