d3578f1a2bb04ce961a205b97d1fcd430913bee5
Sam Partington <sampart@github.com>
2024-07-26 16:04:19 +0100

    Give it the right classes

--- app/components/primer/alpha/action_list/item.rb
+++ app/components/primer/alpha/action_list/item.rb
@@ -41,7 +41,7 @@ module Primer
         # Description content that complements the item's label. See `ActionList`'s `description_scheme` argument
         # for layout options.
         renders_one :description, -> (test_selector: nil) do
-          Primer::BaseComponent.new(tag: "span", test_selector: test_selector) { content }
+          Primer::BaseComponent.new(tag: "span", classes: "ActionListItem-description", test_selector: test_selector) { content }
         end
 
         # An icon, avatar, SVG, or custom content that will render to the left of the label.
