2458b07e26931d7bdbd644fb35e8a2596b21794b
Jamie Shark <5520141+jamieshark@users.noreply.github.com>
2024-07-31 10:08:33 -0500

    Add custom erb matching config to details menu.

--- lib/primer/view_components/linters/details_menu_migration.rb
+++ lib/primer/view_components/linters/details_menu_migration.rb
@@ -14,6 +14,12 @@ module ERBLint
             " https://primer.style/design/components/action-menu/rails/alpha"
           DETAILS_MENU_RUBY_PATTERN = /tag:?\s+:"details-menu"/.freeze
 
+          class ConfigSchema < LinterConfig
+            property :custom_erb_pattern, accepts: array_of?(String),
+              default: -> { [] }
+          end
+          self.config_schema = ConfigSchema
+
           def run(processed_source)
             # HTML tags
             tags(processed_source).each do |tag|
@@ -25,7 +31,7 @@ module ERBLint
             # ERB nodes
             erb_nodes(processed_source).each do |node|
               code = extract_ruby_from_erb_node(node)
-              generate_node_offense(self.class, processed_source, node, MIGRATE_FROM_DETAILS_MENU) if code.match?(DETAILS_MENU_RUBY_PATTERN)
+              generate_node_offense(self.class, processed_source, node, MIGRATE_FROM_DETAILS_MENU) if (code.match?(DETAILS_MENU_RUBY_PATTERN) || code.match?(Regexp.new(@config.custom_erb_pattern.join("|"), true)))
             end
           end
         end
