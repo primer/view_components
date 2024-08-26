5be0a0f1e60e02cb5140e159fd6b68dfd25194fa
Cameron Dutro <camertron@gmail.com>
2024-08-26 10:48:16 -0700

    Fire an event when banners are dismissed (#3026)

--- test/system/alpha/banner_test.rb
+++ test/system/alpha/banner_test.rb
@@ -4,6 +4,8 @@ require "system/test_case"
 
 module Alpha
   class IntegrationBannerTest < System::TestCase
+    include Primer::JsTestHelpers
+
     def test_dismiss_button_remove
       visit_preview(:playground, dismiss_scheme: :remove)
       assert_selector(".Banner")
@@ -20,6 +22,22 @@ module Alpha
       find(".Banner-close button").click
 
       assert_selector(".Banner", visible: :hidden)
+    end
+
+    def test_dismiss_fires_event
+      visit_preview(:playground, dismiss_scheme: :hide)
+
+      evaluate_multiline_script(<<~JS)
+        window.bannerDismissed = false
+
+        document.querySelector('x-banner').addEventListener('banner:dismiss', () => {
+          window.bannerDismissed = true
+        })
+      JS
+
+      find(".Banner-close button").click
+
+      assert page.evaluate_script("window.bannerDismissed")
     end
   end
 end
