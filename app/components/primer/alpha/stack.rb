9bae3358381dc083754c1e2626e70f89407ce0ca
Marie Lucca <francinelucca@github.com>
2024-09-30 15:53:07 -0400

    feat(Stack): add properties options list

--- app/components/primer/alpha/stack.rb
+++ app/components/primer/alpha/stack.rb
@@ -5,10 +5,6 @@ module Primer
     # TODO: docs this
     class Stack < Primer::Component
       DEFAULT_JUSTIFY = :start
-      DEFAULT_DIRECTION = :vertical
-      DEFAULT_ALIGN = :stretch
-      DEFAULT_WRAP = :nowrap
-      DEFAULT_PADDING = :none
       JUSTIFY_MAPPING = {
         DEFAULT_JUSTIFY => "start",
         :center => "center",
@@ -16,6 +12,20 @@ module Primer
         :space_between => "space-between",
         :space_evenly => "space-evenly"
       }.freeze
+      DEFAULT_DIRECTION = :vertical
+      DIRECTION_OPTIONS = [DEFAULT_DIRECTION, :none, :condensed, :normal, :spacious].freeze
+      DEFAULT_ALIGN = :stretch
+      ALIGN_OPTIONS = [DEFAULT_ALIGN, :start, :center, :end, :baseline].freeze
+      DEFAULT_WRAP = :nowrap
+      WRAP_OPTIONS = [DEFAULT_WRAP, :wrap].freeze
+      DEFAULT_PADDING = :none
+      PADDING_OPTIONS = [DEFAULT_PADDING, :condensed, :normal, :spacious].freeze
+      DEFAULT_GAP = nil
+      GAP_OPTIONS = [DEFAULT_GAP, :condensed, :normal, :spacious].freeze
+
+      DEFAULT_TAG = :div
+      # TODO
+      TAG_OPTIONS = [DEFAULT_TAG].freeze
 
 
       def initialize(
@@ -25,18 +35,18 @@ module Primer
         align: DEFAULT_ALIGN,
         wrap: DEFAULT_WRAP,
         padding: DEFAULT_PADDING,
-        tag: :div,
+        tag: DEFAULT_TAG,
         **system_arguments
       )
         @system_arguments = system_arguments
         @system_arguments[:classes] = class_names(@system_arguments.delete(:classes), "Stack")
         @system_arguments[:"data-justify"] = fetch_or_fallback(JUSTIFY_MAPPING, justify, DEFAULT_JUSTIFY)
-        @system_arguments[:"data-gap"] = gap
-        @system_arguments[:"data-direction"] = direction
-        @system_arguments[:"data-align"] = align
-        @system_arguments[:"data-wrap"] = wrap
-        @system_arguments[:"data-padding"] = padding
-        @system_arguments[:"tag"] = tag
+        @system_arguments[:"data-gap"] = fetch_or_fallback(GAP_OPTIONS, gap, DEFAULT_GAP)
+        @system_arguments[:"data-direction"] = fetch_or_fallback(DIRECTION_OPTIONS, direction, DEFAULT_DIRECTION)
+        @system_arguments[:"data-align"] = fetch_or_fallback(ALIGN_OPTIONS, align, DEFAULT_ALIGN)
+        @system_arguments[:"data-wrap"] = fetch_or_fallback(WRAP_OPTIONS, wrap, DEFAULT_WRAP)
+        @system_arguments[:"data-padding"] = fetch_or_fallback(PADDING_OPTIONS, padding, DEFAULT_PADDING)
+        @system_arguments[:"tag"] = fetch_or_fallback(TAG_OPTIONS, tag, DEFAULT_TAG)
       end
     end
   end
