0233797f86d64db5daecac512222704824500452
Cameron Dutro <camertron@gmail.com>
2024-11-20 15:47:59 -0800

    Mark form previews as non-interactive so we don't accidentally submit forms

--- previews/primer/forms_preview.rb
+++ previews/primer/forms_preview.rb
@@ -3,69 +3,69 @@
 module Primer
   # :nodoc:
   class FormsPreview < ViewComponent::Preview
-    # @snapshot interactive
+    # @snapshot
     def single_text_field_form; end
 
-    # @snapshot interactive
+    # @snapshot
     def multi_text_field_form; end
 
-    # @snapshot interactive
+    # @snapshot
     def text_field_and_checkbox_form; end
 
-    # @snapshot interactive
+    # @snapshot
     def horizontal_form; end
 
-    # @snapshot interactive
+    # @snapshot
     def composed_form; end
 
-    # @snapshot interactive
+    # @snapshot
     def submit_button_form; end
 
-    # @snapshot interactive
+    # @snapshot
     def radio_button_group_form; end
 
-    # @snapshot interactive
+    # @snapshot
     def check_box_group_form; end
 
-    # @snapshot interactive
+    # @snapshot
     def array_check_box_group_form; end
 
-    # @snapshot interactive
+    # @snapshot
     def select_form; end
 
-    # @snapshot interactive
+    # @snapshot
     def action_menu_form(route_format: :html)
       render_with_template(locals: { route_format: route_format })
     end
 
-    # @snapshot interactive
+    # @snapshot
     def radio_button_with_nested_form; end
 
-    # @snapshot interactive
+    # @snapshot
     def check_box_with_nested_form; end
 
-    # @snapshot interactive
+    # @snapshot
     def caption_template_form; end
 
-    # @snapshot interactive
+    # @snapshot
     def after_content_form; end
 
-    # @snapshot interactive
+    # @snapshot
     def invalid_form; end
 
-    # @snapshot interactive
+    # @snapshot
     def multi_input_form; end
 
-    # @snapshot interactive
+    # @snapshot
     def name_with_question_mark_form; end
 
-    # @snapshot interactive
+    # @snapshot
     def immediate_validation_form; end
 
-    # @snapshot interactive
+    # @snapshot
     def example_toggle_switch_form; end
 
-    # @snapshot interactive
+    # @snapshot
     def auto_complete_form; end
   end
 end
