355733766645834cb55937580780fabcfea8c03a
Cameron Dutro <camertron@gmail.com>
2024-12-10 12:01:51 -0800

    Don't add test/lib to Zeitwerk autoloads

--- test/lib/primer/forms/utils_test.rb
+++ test/lib/primer/forms/utils_test.rb
@@ -7,9 +7,7 @@ class Primer::Forms::UtilsTest < Minitest::Test
   # https://github.com/ruby/ruby/pull/5646 and Rubies that do not. Ruby versions 2.7 to 3.1
   # (inclusive) are affected. See also https://bugs.ruby-lang.org/issues/18624.
   def test_const_source_location
-    # Load the constant so const_source_location will return the correct path instead of
-    # the path to the internal Zeitwerk file that defines the autoload.
-    Primer::Forms.const_get(:ConstSrc)
+    ensure_autoload!
 
     actual_loc = Primer::Forms::Utils.const_source_location("Primer::Forms::ConstSrc")
     expected_loc = File.join(File.dirname(__FILE__), "const_src.rb")
@@ -18,6 +16,11 @@ class Primer::Forms::UtilsTest < Minitest::Test
   end
 
   def test_const_source_location_falls_back_to_enumerating_load_path
+    # Normally this would be dangerous since test/lib isn't Zeitwerk-friendly, but because
+    # we're doing it after Zeitwerk setup, it will essentially have no effect.
+    Rails.autoloaders.main.push_dir(Rails.root.join("..", "test", "lib"))
+    ensure_autoload!
+
     called = false
 
     Object.stub(:const_source_location, [false, 0]) do
@@ -28,5 +31,18 @@ class Primer::Forms::UtilsTest < Minitest::Test
     end
 
     assert called, "Object.const_source_location was not called"
+  end
+
+  private
+
+  def ensure_autoload!
+    # Load the constant so const_source_location will return the correct path instead of
+    # the path to the file that defines the autoload (which is this file, see below).
+    Primer::Forms.const_get(:ConstSrc)
+  rescue NameError
+    # We unfortunately can't add test/lib to the load path because it's not Zeitwerk-friendly,
+    # so we fake it by setting up the autoload to our test file manually.
+    Primer::Forms.autoload :ConstSrc, File.join(File.dirname(__FILE__), "const_src")
+    Primer::Forms.const_get(:ConstSrc)
   end
 end
