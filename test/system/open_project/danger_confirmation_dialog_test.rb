da9aa2a57dd25906c9e096afdd7991dba8e8eec2
Alexander Brandon Coles <a.coles@openproject.com>
2024-12-12 00:07:39 +0000

    Flesh out Danger Confirmation Dialog system test

--- test/system/open_project/danger_confirmation_dialog_test.rb
+++ test/system/open_project/danger_confirmation_dialog_test.rb
@@ -10,4 +10,26 @@ class IntegrationOpenProjectDangerConfirmationDialogTest < System::TestCase
 
     assert_selector(".DangerConfirmationDialog")
   end
+
+  def test_submit_button_disabled_on_dialog_open
+    visit_preview(:default)
+
+    click_button("Click me")
+
+    assert_selector(".DangerConfirmationDialog") do
+      refute_selector("input[type='checkbox']:checked")
+      refute_selector("button[data-submit-dialog-id]:enabled")
+    end
+  end
+
+  def test_submit_button_enabled_when_confirmation_checkbox_checked
+    visit_preview(:default)
+
+    click_button("Click me")
+
+    assert_selector(".DangerConfirmationDialog") do
+      check("I understand that this deletion cannot be reversed")
+      assert_selector("button[data-submit-dialog-id]:enabled")
+    end
+  end
 end
