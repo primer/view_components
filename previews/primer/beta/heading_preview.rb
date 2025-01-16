a56bc9523131164449780b7d5b102b9f8348769b
Kate Higa <16447748+khiga8@users.noreply.github.com>
2025-01-16 19:34:38 +0000

    Temp

--- previews/primer/beta/heading_preview.rb
+++ previews/primer/beta/heading_preview.rb
@@ -20,6 +20,15 @@ module Primer
       def default(tag: :h2, content: "Heading")
         render(Primer::Beta::Heading.new(tag: tag)) { content }
       end
+
+            # @label Default options
+      #
+      # @param tag [Symbol] select [h1, h2, h3, h4, h5, h6]
+      # @param content [String] text
+      # @snapshot
+      def temp(tag: :h2, content: "Blah blah test")
+        render(Primer::Beta::Heading.new(tag: :h3)) { content }
+      end
     end
   end
 end
