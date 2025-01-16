fe49c490fc504b4b8fea9fc0978f6ac064c23199
Kate Higa <16447748+khiga8@users.noreply.github.com>
2025-01-16 16:36:09 +0000

    Only generate snapshot for one theme, and add snapshot for interacted state
    * The markup shouldn't change based on the theme so let's just generate the snapshot for one component on default theme.
    * The markup may change based on interaction so add support for that.

--- test/playwright/snapshots.test.ts
+++ test/playwright/snapshots.test.ts
@@ -60,13 +60,44 @@ test.describe('generate snapshots', () => {
         }
 
         test(`${example.preview_path}-aria-snapshots`, async ({page}) => {
-          await page.goto(`/rails/view_components/${example.preview_path}?theme=all`)
+          await page.goto(`/rails/view_components/${example.preview_path}`)
+
+          const defaultScreenshot = await page.locator('#component-preview').ariaSnapshot()
+          expect(defaultScreenshot).toMatchSnapshot([example.preview_path, 'default.yml'])
+        })
+
+        if (example.snapshot === 'interactive') {
+          test(`${example.preview_path}-aria-snapshots-interactive`, async ({page}) => {
+            await page.goto(`/rails/view_components/${example.preview_path}`)
+
+            // Focus state
+            await page.keyboard.press('Tab')
+
+            await new Promise(resolve => setTimeout(resolve, 100))
+            await page.keyboard.press('Enter')
+
+            const subject = await page.evaluate(() => document.querySelector('[data-interaction-subject]'))
+            if (subject) {
+              await page.waitForSelector('[data-interaction-subject][data-ready=true]')
+            }
+
+            // Wait a bit for animations etc to resolve
+            await new Promise(resolve => setTimeout(resolve, 100))
+
+            const focusedScreenshot = await page.screenshot({animations: 'disabled'})
+            expect(focusedScreenshot).toMatchSnapshot([example.preview_path, `default-interacted.yml`])
+          })
+        }
+
+        test(`${example.preview_path}-aria-snapshots`, async ({page}) => {
+          await page.goto(`/rails/view_components/${example.preview_path}`)
+
           const defaultScreenshot = await page.locator('#component-preview').ariaSnapshot()
           expect(defaultScreenshot).toMatchSnapshot([example.preview_path, 'default.yml'])
         })
 
         test(example.preview_path, async ({page}) => {
-          await page.goto(`/rails/view_components/${example.preview_path}?theme=all`)
+          await page.goto(`/rails/view_components/${example.preview_path}`)
           const defaultScreenshot = await page.locator('#component-preview').screenshot({animations: 'disabled'})
           expect(defaultScreenshot).toMatchSnapshot([example.preview_path, 'default.png'])
 
