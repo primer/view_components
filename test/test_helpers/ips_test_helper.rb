413f9545c518dbe18c3671449481e832b1738586
Jon Rohan <yes@jonrohan.codes>
2025-03-11 19:20:27 +0000

    Fix lint error

--- test/test_helpers/ips_test_helper.rb
+++ test/test_helpers/ips_test_helper.rb
@@ -22,8 +22,8 @@ module Primer
     def suppress_output
       original_stderr = $stderr.clone
       original_stdout = $stdout.clone
-      $stderr.reopen(File.new("/dev/null", "w"))
-      $stdout.reopen(File.new("/dev/null", "w"))
+      $stderr.reopen(File::NULL)
+      $stdout.reopen(File::NULL)
       yield
     ensure
       $stdout.reopen(original_stdout)
