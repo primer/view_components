510a3a5f533c1a873c94c19cdef3f71e94928272
Manuel Puyol <manuelpuyol@github.com>
2025-04-08 14:33:27 +0000

    update tests

--- test/components/primer/alpha/include_fragment_test.rb
+++ test/components/primer/alpha/include_fragment_test.rb
@@ -6,8 +6,20 @@ class PrimerAlphaIncludeFragmentTest < Minitest::Test
   include Primer::ComponentTestHelpers
 
   def test_renders
-    render_inline(Primer::IncludeFragment.new)
+    render_inline(Primer::Alpha::IncludeFragment.new)
 
-    assert_text("Add a test here")
+    assert_selector("include-fragment[loading='eager']")
+  end
+
+  def test_renders_lazy
+    render_inline(Primer::Alpha::IncludeFragment.new(loading: :lazy))
+
+    assert_selector("include-fragment[loading='lazy']")
+  end
+
+  def test_renders_with_src
+    render_inline(Primer::Alpha::IncludeFragment.new(src: "/some/path"))
+
+    assert_selector("include-fragment[src='/some/path']")
   end
 end
