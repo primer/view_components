3ca407cc9f49ed72ea5ba569f52a8cbe423d499b
Manuel Puyol <manuelpuyol@github.com>
2025-04-08 14:28:05 +0000

    update lookbook

--- app/components/primer/alpha/include_fragment.rb
+++ app/components/primer/alpha/include_fragment.rb
@@ -2,16 +2,20 @@
 
 module Primer
   module Alpha
-    # Add a general description of component here
+    # Use `IncludeFragment` to load HTML elements from the server.
     # Add additional usage considerations or best practices that may aid the user to use the component correctly.
     # @accessibility Add any accessibility considerations
     class IncludeFragment < Primer::Component
       status :alpha
 
+      # @param src [String] The URL  from which to retrieve an HTML element fragment.
+      # @param loading [Symbol] <%= one_of([:lazy, :eager]) %>
       # @param system_arguments [Hash] <%= link_to_system_arguments_docs %>
-      def initialize(**system_arguments)
+      def initialize(src: nil, loading: :eager, **system_arguments)
         @system_arguments = system_arguments
         @system_arguments[:tag] = "include-fragment"
+        @system_arguments[:loading] = loading
+        @system_arguments[:src] = src
       end
 
       def call
--- previews/primer/alpha/include_fragment_preview.rb
+++ previews/primer/alpha/include_fragment_preview.rb
@@ -8,19 +8,9 @@ module Primer
     # @label IncludeFragment
     class IncludeFragmentPreview < ViewComponent::Preview
       # @label Playground
-      # @param string_example text
-      # @param boolean_example toggle
-      # @param email_example email
-      # @param number_example number
-      # @param url_example url
-      # @param tel_example tel
-      # @param textarea_example textarea
-      # @param select_example select [one, two, three]
-      # @param select_custom_labels select [[One label, one], [Two label, two], [Three label, three]]
-      # With empty option (`~` in YAML)
-      # @param select_empty_option select [~, one, two, three]
-      def playground(string_example: "Some value", boolean_example: false, select_example: :one)
-        render(Primer::Alpha::IncludeFragment.new(string_example: string_example, boolean_example: boolean_example, select_example: select_example))
+      # @param loading select [eager, lazy]
+      def playground(loading: :eager)
+        render(Primer::Alpha::IncludeFragment.new(loading: loading, src: UrlHelpers.include_fragment_deferred_path)) { "Loading..." }
       end
     end
   end
