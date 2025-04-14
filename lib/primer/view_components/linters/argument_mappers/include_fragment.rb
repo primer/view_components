f70c73a4190ede2ade134c15e844ed7180575147
Manuel Puyol <manuelpuyol@github.com>
2025-04-14 20:09:09 +0000

    add more acceptable attrs

--- lib/primer/view_components/linters/argument_mappers/include_fragment.rb
+++ lib/primer/view_components/linters/argument_mappers/include_fragment.rb
@@ -8,7 +8,7 @@ module ERBLint
       # Maps attributes in the include-fragment element to arguments for the IncludeFragment component.
       class IncludeFragment < Base
         DEFAULT_TAG = "include-fragment"
-        ATTRIBUTES = %w[loading src accept].freeze
+        ATTRIBUTES = %w[loading src accept role preload tabindex autofocus hidden].freeze
 
         def attribute_to_args(attribute)
           attr_name = attribute.name
