57f6c41025de70dfcd6880dab7e881d182e3cbd9
Manuel Puyol <manuelpuyol@github.com>
2025-04-28 08:45:36 -0700

    Update IncludeFragment to add data-nonce when a current nonce is present (#3466)

--- app/components/primer/alpha/include_fragment.rb
+++ app/components/primer/alpha/include_fragment.rb
@@ -8,14 +8,27 @@ module Primer
     class IncludeFragment < Primer::Component
       status :alpha
 
+      ALLOWED_LOADING_VALUES = [:lazy, :eager].freeze
+      DEFAULT_LOADING = :eager
+
       # @param src [String] The URL  from which to retrieve an HTML element fragment.
       # @param loading [Symbol] <%= one_of([:lazy, :eager]) %>
+      # @param accept [String] What to send as the Accept header.
       # @param system_arguments [Hash] <%= link_to_system_arguments_docs %>
-      def initialize(src: nil, loading: :eager, **system_arguments)
+      def initialize(src: nil, loading: nil, accept: nil, **system_arguments)
         @system_arguments = system_arguments
         @system_arguments[:tag] = "include-fragment"
         @system_arguments[:loading] = loading
         @system_arguments[:src] = src
+        @system_arguments[:accept] = accept if accept
+
+        if loading
+          @system_arguments[:loading] = fetch_or_fallback(ALLOWED_LOADING_VALUES, loading.to_sym, DEFAULT_LOADING)
+        end
+
+        if Primer::CurrentAttributes.nonce
+          @system_arguments[:"data-nonce"] = Primer::CurrentAttributes.nonce
+        end
       end
 
       def call
