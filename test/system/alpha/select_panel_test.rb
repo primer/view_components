db676f058f1764dd93163b35968e2cf711b4a58d
Tyler Jones <tylerjdev@github.com>
2025-09-29 20:09:51 +0000

    Add test

--- test/system/alpha/select_panel_test.rb
+++ test/system/alpha/select_panel_test.rb
@@ -1365,5 +1365,33 @@ module Alpha
         end
       end
     end
+
+    def test_prevents_events_from_bubbling
+      visit_preview(:default)
+
+      evaluate_multiline_script(<<~JS)
+        document.body.addEventListener('keydown', (event) => {
+          window.bodyKeydownFired = true
+        }, { once: true })
+      JS
+
+      click_on_invoker_button
+      
+      keyboard.type(:tab)
+
+      assert_includes active_element.text, "Item 1"
+
+      keyboard.type(:a)  # type an alphabetic key, which should be stopped from propagating
+
+      refute page.evaluate_script("window.bodyKeydownFired")
+
+      keyboard.type(:escape)  # close the panel
+
+      refute_selector "select-panel dialog[open]"
+
+      keyboard.type(:a)  
+
+      assert page.evaluate_script("window.bodyKeydownFired")
+    end
   end
 end
