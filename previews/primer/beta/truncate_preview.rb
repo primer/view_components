26f29b486fa3405826cf202e5325f1280f61b85d
LiuLiu <liuliu-dev@github.com>
2026-02-27 08:39:10 -0800

    Update Truncate lookbook previews with tooltip + link for accessibility (#3921)

--- previews/primer/beta/truncate_preview.rb
+++ previews/primer/beta/truncate_preview.rb
@@ -19,37 +19,14 @@ module Primer
       end
 
       # @label Multiple items
-      def multiple_items
-        render(Primer::Beta::Truncate.new) do |component|
-          component.with_item do
-            "really-long-repository-owner-name"
-          end
-          component.with_item(font_weight: :bold) do
-            "really-long-repository-name"
-          end
-        end
-      end
+      def multiple_items; end
 
       # @label Advanced multiple items
-      def advanced_multiple_items
-        render(Primer::Beta::Truncate.new(tag: :ol)) do |component|
-          component.with_item(tag: :li) { "primer" }
-          component.with_item(tag: :li, priority: true) { "/ css" }
-          component.with_item(tag: :li) { "/ Issues" }
-          component.with_item(tag: :li) { "/ #123" }
-          component.with_item(tag: :li, priority: true) { "Visual bug on primer.style found in lists" }
-        end
-      end
+      def advanced_multiple_items; end
 
       # @label Max widths
       # @snapshot
-      def max_widths
-        render(Primer::Beta::Truncate.new) do |component|
-          component.with_item(max_width: 300) { "branch-name-that-is-really-long-branch-name-that-is-really-long-branch-name-that-is-really-long" }
-          component.with_item(max_width: 200) { "branch-name-that-is-really-long-branch-name-that-is-really-long-branch-name-that-is-really-long" }
-          component.with_item(max_width: 100) { "branch-name-that-is-really-long-branch-name-that-is-really-long-branch-name-that-is-really-long" }
-        end
-      end
+      def max_widths; end
     end
   end
 end
